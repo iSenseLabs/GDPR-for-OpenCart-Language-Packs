ca4cc005165b73e93f7d36e4d6e50d093aa8cce3
Aleksandar Banderov <banderov@isenselabs.com>
2018-05-23 15:11:52 +0300

    Add Swedish catalog translation

---
+++ swedish/src_2.3/upload/catalog/language/sv-sv/extension/module/isenselabs_gdpr.php
@@ -0,0 +1,92 @@
+<?php
+$_['text_account'] = 'Konto';
+$_['text_gdpr'] = 'GDPR verktyg';
+
+$_['button_back'] = 'Tillbaka';
+$_['button_continue'] = 'Fortsätt';
+$_['entry_email'] = 'Epost adress';
+$_['text_your_email'] = 'Din epostadress';
+
+$_['text_data_rectification'] = 'Ändra personuppgifter';
+$_['text_data_rectification_helper'] = 'Använd länkarna nedan för att ändra och uppdatera dina personuppgifter.';
+$_['text_edit'] = 'Ändra kontoinformation';
+$_['text_password'] = 'Ändra ditt lösenord';
+$_['text_address'] = 'Ändra fakturerings och leveransadresser';
+$_['text_newsletter'] = 'Prenumerera / avregistrera dig från nyhetsbrev';
+
+$_['text_data_portability'] = 'Dataflyttbarhet';
+$_['text_data_portability_helper'] = 'Använd länkarna nedan för att ladda ner de uppgifter och historik vi har sparat om dig.';
+$_['text_personal_information'] = 'Personuppgifter';
+$_['text_personal_addresses'] = 'Sparade fakturerings och leveransadresser';
+$_['text_orders'] = 'Orderhistorik';
+$_['text_gdpr_requests'] = 'GDPR förfrågningar';
+
+$_['text_access_to_personal_data'] = 'Tillgång till personuppgifter';
+$_['text_access_to_personal_data_helper'] = 'Använd länken nedan för att begära en rapport som innehåller de personuppgifter vi har sparat om dig.';
+$_['text_personal_data_helper'] = 'Använd formuläret nedan för att begära en rapport som innehåller de personuppgifter vi har sparat om dig.';
+$_['text_request_a_report'] = 'Begär en rapport';
+$_['text_enter_your_email'] = 'Ange din epostadress';
+$_['text_enter_your_email_data_request_helper'] = 'Du kommer att få ett mail med en länk. Klicka på länken för att se all information som vi har lagrat om dig.Länken är aktiv i %s% timmar.';
+$_['text_how_this_works_data_request'] = 'Hur fungerar detta?';
+$_['text_successful_request'] = 'Genomfört!';
+$_['text_successful_data_request_helper'] = 'Tack för din förfrågan. Om epostadressen du angivit är korrekt och finns i vår databas kommer du få ett mail med en länk. Klicka på länken för att se alla personuppgifter vi har sparade om dig. Länken är aktiv i %s% timmar.';
+
+$_['text_personal_data_subject'] = 'Här är information om de personuppgifter vi har sparat om dig';
+$_['text_greeting'] = 'Hejsan! Du har begärt att se vilka personuppgifter vi har sparat om dig i vår webshop. Klicka på länken så får du en rapport:';
+$_['text_button_view_data'] = 'Visa personuppgifter';
+$_['text_actions'] = 'Åtgärder';
+$_['text_active_link_helper'] = '* Kom ihåg att länken bara är aktiv i %s% timmar.';
+
+$_['text_view_personal_data'] = 'Här visas den data vi har sparat om dig';
+$_['text_firstname'] = 'Förnamn';
+$_['text_lastname'] = 'Efternamn';
+$_['text_telephone'] = 'Telefonnummer';
+$_['text_email'] = 'Epostadress';
+$_['text_fax'] = 'Fax';
+$_['text_newsletter_subscription'] = 'Prenumererar på nyhetsbrev';
+$_['text_ip'] = 'IP';
+$_['text_customer_information'] = 'Kunduppgifter';
+$_['text_saved_addresses'] = 'Sparade adresser';
+$_['text_company'] = 'Företag';
+$_['text_address_1'] = 'Adress rad 1';
+$_['text_address_2'] = 'Adress rad 2';
+$_['text_country'] = 'Land';
+$_['text_city'] = 'Ort';
+$_['text_postcode'] = 'Postnummer';
+$_['text_zone'] = 'Län';
+$_['text_address'] = 'Adress';
+$_['text_data_third_party'] = 'Data skickad till tredje-part';
+$_['text_data_third_party_helper'] = 'Ibland skickas viss data vidare till tredje part. Det här är för att du ska få den bästa upplevelsen i vår butik och våra tjänster. Exempel på tredje part kan vara företag som förmedlar frakter';
+$_['text_third_party_services'] = 'Tjänster';
+$_['text_other'] = 'Annat';
+
+$_['text_hash_error'] = 'Ingen data kan visas. Länken kan inaktiv eller ogiltig. Vill du se begärd data behöver du göra en ny begäran.';
+$_['text_no_data_third_party'] = 'Vi skickar ingen data vidare till tredje part.';
+$_['text_no_data'] = 'Det finns ingen data sparad!';
+
+$_['text_right_to_be_forgotten'] = 'Rätt att bli glömd';
+$_['text_right_to_be_forgotten_helper'] = 'Använd det här alternativet om du vill ta bort dina personliga och andra data från denna webshop. Kom ihåg att <strong>detta kommer att radera ditt konto så att du inte längre kan komma åt eller använda det</strong>.';
+$_['text_request_personal_data_deletion'] = 'Begär att bli glömd';
+$_['text_enter_your_email_data_deletion_helper'] = 'Du kommer att få ett mail med en länk. Klicka på länken för att radera all information som vi har lagrat om dig. Om det finns data som inte kan raderas kommer den istället att bli anonymiserad. Länken är aktiv i %s% timmar.';
+$_['text_successful_data_deletion_helper'] = 'Tack för din förfrågan! Om e-postadressen är korrekt och den finns vår databas får du ett mail med en länk. Genom att klicka på länken raderas alla de uppgifter vi har lagrade om dig. Tänk på att länken bara är aktiv i %s% timmar.';
+
+$_['text_delete_data_subject'] = 'Begäran om att bli glömd';
+$_['text_button_delete_data'] = 'Radera alla mina uppgifter';
+$_['text_greeting_delete'] = 'Hejsan! Du har begärt att vi skall radera alla uppgifter som finns sparat i webshopen. Kom ihåg att detta kommer att radera ditt konto, och du kommer inte kunna använda det längre.<br /><br />Om du inte begärt detta mailet kan du ignorera det. Om inte klicka på knappen nedan för att radera all data:';
+
+$_['text_personal_data_deletion'] = 'Personlig dataradering';
+$_['text_hash_error_delete'] = 'Åtgärden kan inte slutföras. Länken kan inaktiv eller ogiltig. Om du vill utföra denna åtgärd behöver du göra en ny förfrågan.';
+$_['text_confirm_the_action'] = 'Bekräftelse av kund krävs';
+$_['text_confirm_the_action_helper'] = '<strong>Denna åtgärd kan inte ångras!</strong> Om du bekräftar åtgärden kommer ditt konto på vår webbplats att raderas och du kommer inte längre kunna logga in med det. Du kommer inte heller ha tillgång till din orderhistorik, adresser osv. Vänligen gör ditt val nedan:';
+$_['text_button_confirm_deletion'] = 'JA, radera all min data';
+$_['text_button_cancel_deletion'] = 'NEJ, lämna min data orörd';
+
+// GDPR Compliance 1.1/2.1./3.1
+$_['text_order'] = 'Order';
+$_['text_personal_data'] = 'Gäst personuppgifter';
+$_['text_personal_data_helper'] = 'Även om kunden inte är registrerad kan det ändå finnas sparad data kopplad till denna mailadressen. Här kan du se data som finns sparat från vår orderhistorik.';
+$_['text_payment_view_data'] = 'Betalningsinformation';
+$_['text_shipping_view_data'] = 'Leveransinformation';
+$_['text_order_view_data'] = 'Orderinformation';
+$_['text_user_agent'] = 'Användaragent';
+$_['text_accept_language'] = 'Acceptera språk';
