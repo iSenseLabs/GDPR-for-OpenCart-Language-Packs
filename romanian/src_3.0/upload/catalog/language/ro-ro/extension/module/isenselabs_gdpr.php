ea80e70a9ce1291f8f387043d65c6aa2fa92a67e
Aleksandar Banderov <banderov@isenselabs.com>
2018-05-23 14:22:14 +0300

    Add Romanian

---
+++ romanian/src_3.0/upload/catalog/language/ro-ro/extension/module/isenselabs_gdpr.php
@@ -0,0 +1,82 @@
+<?php
+$_['text_account'] = 'Contul meu';
+$_['text_gdpr'] = 'GDPR - Regulamentul General Pentru Protectia Datelor Personale';
+
+$_['button_back'] = 'Inapoi';
+$_['button_continue'] = 'Continua';
+$_['entry_email'] = 'Adresa de e-mail';
+$_['text_your_email'] = 'Adresa mea de e-mail';
+
+$_['text_data_rectification'] = 'Dreptul la rectificare';
+$_['text_data_rectification_helper'] = 'Poti folosi link-urile de mai jos pentru a actualiza datele contului tau daca datele nu sunt corecte';
+$_['text_edit'] = 'Modifica datele contului';
+$_['text_password'] = 'Schimba parola';
+$_['text_address'] = 'Modifica adresele';
+$_['text_newsletter'] = 'Aboneaza-te / Dezaboneaza-te la buletinul de stiri';
+
+$_['text_data_portability'] = 'Portabilitatea Datelor';
+$_['text_data_portability_helper'] = 'Poti folosi link-urile de mai jos pentru a descarca toate datele personale inregistrate in vederea prelucrarii comenzilor plasate in acest magazin online';
+$_['text_personal_information'] = 'Informatii personale';
+$_['text_personal_addresses'] = 'Adrese salvate';
+$_['text_orders'] = 'Comenzile mele';
+$_['text_gdpr_requests'] = 'Cereri GDPR';
+
+$_['text_access_to_personal_data'] = 'Accesul la date personale';
+$_['text_access_to_personal_data_helper'] = 'Poti folosi link-ul de mai jos pentru a solicita un raport care va contine toate datele personale stocate pentru tine';
+$_['text_personal_data_helper'] = 'Foloseste formularul de mai jos pentru a solicita un raport care va contine toate datele personale stocate pentru tine.';
+$_['text_request_a_report'] = 'Solicita raport';
+$_['text_enter_your_email'] = 'Introdu adresa de e-mail';
+$_['text_enter_your_email_data_request_helper'] = 'Vei primi pe e-mail un link care odata accesat iti va arata toate informatiile pe care le stocam pentru tine. Acest link va fi activ %s% ore.';
+$_['text_how_this_works_data_request'] = 'Cum functioneaza?';
+$_['text_successful_request'] = 'Succes!';
+$_['text_successful_data_request_helper'] = 'Multumim pentru solicitare! Daca adresa de e-mail este corecta si prezenta in baza noastra de date, vei primi pe e-mail un link. Accesand acest link vei afla toate informatiile pe care le stocam pentru tine. Acest link va fi activ %s% ore.';
+
+$_['text_personal_data_subject'] = 'Vezi datele personale';
+$_['text_greeting'] = 'Salut! Ai solicitat vizualizarea datelor personale stocate in acest magazin online. Acesseaza link-ul de mai jos:';
+$_['text_button_view_data'] = 'Vizualizeaza datele tale personale';
+$_['text_actions'] = 'Actiuni';
+$_['text_active_link_helper'] = '* Acest link va fi activ %s% ore.';
+
+$_['text_view_personal_data'] = 'Vizualizeaza datele personale';
+$_['text_firstname'] = 'Prenume';
+$_['text_lastname'] = 'Nume';
+$_['text_telephone'] = 'Telefon';
+$_['text_email'] = 'E-mail';
+$_['text_fax'] = 'Fax';
+$_['text_newsletter_subscription'] = 'Abonare la buletinul de stiri';
+$_['text_ip'] = 'IP';
+$_['text_customer_information'] = 'Informatii client';
+$_['text_saved_addresses'] = 'Adrese salvate';
+$_['text_company'] = 'Compania';
+$_['text_address_1'] = 'Adresa 1';
+$_['text_address_2'] = 'Adresa 2';
+$_['text_country'] = 'Tara';
+$_['text_city'] = 'Orasul';
+$_['text_postcode'] = 'Codul postal';
+$_['text_zone'] = 'Zona';
+$_['text_address'] = 'Adresa';
+$_['text_data_third_party'] = 'Informatii transmise tertilor';
+$_['text_data_third_party_helper'] = 'Uneori este posibil sa transmitem o parte a datelor personale catre serviciile de mai jos pentru a ne asigura ca beneficiezi de cea mai bune servicii.';
+$_['text_third_party_services'] = 'Servicii';
+$_['text_other'] = 'Altele';
+
+$_['text_hash_error'] = 'Nu exista date care sa poata fi afisate. Link-ul este expirat sau invalid. Este necesar sa depui o noua cerere GDPR.';
+$_['text_no_data_third_party'] = 'Nu transmitem informatiile tale personale unor terte parti.';
+$_['text_no_data'] = 'Nu sunt informatii despre acest client!';
+
+$_['text_right_to_be_forgotten'] = 'Dreptul de a fi uitat';
+$_['text_right_to_be_forgotten_helper'] = 'Foloseste aceasta optiune daca doresti sa stergi toate datele personale din magazinul noastru. <strong>Aceasta actiune va duce la stergerea contului tau si nu vei mai putea plasa comenzi sau accesa istoricul acestora</strong>.';
+$_['text_request_personal_data_deletion'] = 'Solicita stergerea datelor personale';
+$_['text_enter_your_email_data_deletion_helper'] = 'Vei primi pe e-mail un link. Acesarea acestuia va sterge toate datele personale pe care le stocam pentru tine. Daca ceva nu poate fi sters, va fi anonimizat. Link-ul va fi activ %s% ore.';
+$_['text_successful_data_deletion_helper'] = 'Multumim pentru solicitare! Daca adresa de e-mail este corecta si prezenta in baza noastra de date, vei primi pe e-mail un link. Acesarea acestuia va sterge toate datele personale pe care le stocam pentru tine. Acest link va fi disponibil %s% ore.';
+
+$_['text_delete_data_subject'] = 'Cerere pentru stergerea datelor personale';
+$_['text_button_delete_data'] = 'Sterge datele mele personale';
+$_['text_greeting_delete'] = 'Salut! Ai solicitat stergerea datele personale din acest magazin online. Aceasta actiune va duce la stergerea contului tau si nu vei mai putea plasa comenzi sau accesa istoricul acestora.<br /><br />Daca nu ai solicitat asta, ignora acest mesaj. Daca ai solicitat, apasa butonul de mai jos:';
+
+$_['text_personal_data_deletion'] = 'Stergerea Datelor Personale';
+$_['text_hash_error_delete'] = 'Actiunea nu poate fi finalizata. Link-ul este expirat sau invalid. Daca doresti stergerea Datelor Personale va fi necesar sa depui o noua cerere GDPR.';
+$_['text_confirm_the_action'] = 'Este necesara confirmarea clientului';
+$_['text_confirm_the_action_helper'] = '<strong>ACTIUNEA ESTE IREVERSIBILA!</strong> Daca vei confirma actiunea, contul tau va fi sters si autentificarea nu va mai fi posibila. Nu va mai fi posibila accesarea istoricului comenzilor, adreselor, etc. Te rugam sa iei decizia mai jos:';
+$_['text_button_confirm_deletion'] = 'DA, sterge toate datele mele personale';
+$_['text_button_cancel_deletion'] = 'NU, lasati datele personale intacte';
