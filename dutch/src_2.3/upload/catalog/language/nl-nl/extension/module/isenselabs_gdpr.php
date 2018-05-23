b20bb5cb69167bed6a97e0393d48225a363c117b
Aleksandar Banderov <banderov@isenselabs.com>
2018-05-23 16:32:32 +0300

    Add Dutch catalog translation and edit wrong folder structure for OC 1.5.x to 2.2.x

---
+++ dutch/src_2.3/upload/catalog/language/nl-nl/extension/module/isenselabs_gdpr.php
@@ -0,0 +1,103 @@
+<?php
+$_['text_account'] = 'Account';
+$_['text_gdpr'] = 'Beheer al uw gegevens';
+
+$_['button_back'] = 'Terug';
+$_['button_continue'] = 'Ga door';
+$_['entry_email'] = 'Email adres';
+$_['text_your_email'] = 'Uw e-mail adres';
+
+$_['text_data_rectification'] = 'Data beheer';
+$_['text_data_rectification_helper'] = 'Met onderstaande links kan alle data aanpassen die wij over u bezitten.<br />* Voor deze opties moet u ingelogd zijn of inloggen in uw account.';
+$_['text_edit'] = 'Pas uw algemene accountgegevens aan';
+$_['text_password'] = 'Wijzig uw wachtwoord';
+$_['text_address'] = 'Wijzig uw adres';
+$_['text_newsletter'] = 'Schrijf in of uit bij onze nieuwsbrief';
+
+$_['text_data_portability'] = 'Data Overdraagbaarheid';
+$_['text_data_portability_helper'] = 'Via onderstaande links kan u alle data downloaden die wij over u bewaren voor een goede werking van onze webwinkel.<br />* Voor deze opties moet u ingelogd zijn of inloggen in uw account.';
+$_['text_personal_information'] = 'Persoonlijke informatie';
+$_['text_personal_addresses'] = 'Bewaarde adressen';
+$_['text_orders'] = 'Bestellingen';
+$_['text_gdpr_requests'] = 'Uw eerdere GDPR/AVG aanvragen';
+
+$_['text_access_to_personal_data'] = 'Toegang tot uw persoonlijke data';
+$_['text_access_to_personal_data_helper'] = 'Via onderstaande link kan u een rapport aanvragen dat alle data bevat die wij van u bewaren.';
+$_['text_personal_data_helper'] = 'Gebruik het onderstaande formulier om het rapport op te vragen dat alle data bevat die wij van u bewaren.';
+$_['text_request_a_report'] = 'Rapport aanvragen';
+$_['text_enter_your_email'] = 'Vul uw e-mail adres in';
+$_['text_enter_your_email_data_request_helper'] = 'U zal een e-mail ontvangen met een link. Als u op de link klikt zal u een overzicht krijgen van al uw gegevens die wij over u bewaren.Deze link zal %s% uren actief blijven.';
+$_['text_how_this_works_data_request'] = 'Hoe werkt dit?';
+$_['text_successful_request'] = 'Succesvol!';
+$_['text_successful_data_request_helper'] = 'Dank voor uw aanvraag! Als het e-mail adres correct is, en het is aanwezig in onze databank, dan zult u een e-mail ontvangen met een link. Het aanklikken van die link, zal u helpen om alle persoonlijke gegevens die we over u bezitten te bekijken. Hou er rekening mee dat omwille van uw privacy, deze link na %s% uur zal vervallen.';
+
+$_['text_personal_data_subject'] = 'Aanvraag overzicht persoonlijke data';
+$_['text_greeting'] = 'Beste klant, U diende een aanvraag in om uw persoonlijke gegevens in onze webwinkel in te kijken. Klik op onderstaande link om een overzicht van uw persoonlijke gegevens te bekijken:';
+$_['text_button_view_data'] = 'Bekijk uw persoonlijke gegevens';
+$_['text_actions'] = 'Acties';
+$_['text_active_link_helper'] = '* Hou er rekening mee dat deze link slechts %s% uren geldig blijft.';
+
+$_['text_view_personal_data'] = 'Bekijk persoonlijke gegevens';
+$_['text_firstname'] = 'Voornaam';
+$_['text_lastname'] = 'Achternaam';
+$_['text_telephone'] = 'Telefoon';
+$_['text_email'] = 'E-mail';
+$_['text_fax'] = 'Fax';
+$_['text_newsletter_subscription'] = 'Ingeschreven voor nieuwsbrief';
+$_['text_ip'] = 'IP';
+$_['text_customer_information'] = 'Klant informatie';
+$_['text_saved_addresses'] = 'Bewaarde adressen';
+$_['text_company'] = 'Bedrijf';
+$_['text_address_1'] = 'Adres lijn 1';
+$_['text_address_2'] = 'Adres lijn 2';
+$_['text_country'] = 'Land';
+$_['text_city'] = 'Stad/gemeente';
+$_['text_postcode'] = 'Postcode';
+$_['text_zone'] = 'Provincie';
+$_['text_address'] = 'Adres';
+$_['text_data_third_party'] = 'Gedeelde data met externe diensten';
+$_['text_data_third_party_helper'] = 'In sommige gevallen delen wij uw gegevens met onderstaande externe diensten. Dit dient hoofdzakelijk om ervoor te zorgen dat u de beste ervaring hebt met onze webwinkel.';
+$_['text_third_party_services'] = 'Diensten';
+$_['text_other'] = 'Andere';
+
+$_['text_hash_error'] = 'There is no data to be shown. The link is invalid or it has expired. If you want to see the requested data, you have to make another GDPR request.';
+$_['text_no_data_third_party'] = 'Wij sturen geen persoonlijke gegevens naar third-party diensten.';
+$_['text_no_data'] = 'Er zijn geen gegevens voor deze klant.';
+
+$_['text_right_to_be_forgotten'] = 'Recht om vergeten te worden.';
+$_['text_right_to_be_forgotten_helper'] = 'Gebruik deze optie als u al uw persoonlijke gegevens van onze webwinkel wil verwijderen. Hou er rekening meer dat <strong>dat dit proces ook uw account zal verwijderen. U kan bijgevolg niet meer inloggen in uw account of de gegevens en bestellingen raadplegen</strong>.';
+$_['text_request_personal_data_deletion'] = 'Aanvraag tot verwijderen account en persoonlijke gegevens';
+$_['text_enter_your_email_data_deletion_helper'] = 'U zal een e-mail ontvangen met een link. Bij het aanklikken van de link wordt al uw persoonlijke data verwijderd. Als iets niet kan verwijderd worden, dan zal de data geanonimiseerd worden. Deze link blijft geldig voor %s% uur.';
+$_['text_successful_data_deletion_helper'] = 'Dank voor uw aanvraag! Als uw mailadres correct is en zich in onze databank bevindt, zal u een e-mail ontvangen met een link. Bij het aanklikken van de link wordt al uw persoonlijke data verwijderd. Hou er rekening mee dat deze link slechts geldig is voor %s% uur.';
+
+$_['text_delete_data_subject'] = 'Aanvraag verwijdering persoonlijke gegevens';
+$_['text_button_delete_data'] = 'Verwijder mijn persoonlijke gegevens';
+$_['text_greeting_delete'] = 'Beste klant! U deed een aanvraag om al uw persoonlijke gegevens te verwijderen van onze webwinkel. Hou er rekening mee dat dit proces ook uw account verwijderd en u deze later niet meer kunt raadplegen of heractiveren. U kan wel altijd een nieuwe account registreren.<br /><br />Als u deze aanvraag niet hebt gedaan, negeer dan deze e-mail. Indien u de aanvraag wel deed, klik dan op onderstaande link om door te gaan:';
+
+$_['text_personal_data_deletion'] = 'Verwijdering van persoonlijke gegevens';
+$_['text_hash_error_delete'] = 'Deze actie kan niet doorgaan. De link is ongeldig of reeds vervallen. Als u deze actie wil vervolledigen, maak dan een nieuwe aanvraag.';
+$_['text_confirm_the_action'] = 'Bevestiging klant vereist';
+$_['text_confirm_the_action_helper'] = '<strong>Deze actie kan niet ongedaan gemaakt worden!</strong> Als u deze actie bevestigt, zal uw account definitief van onze webwinkel verwijderd worden en kunt u deze niet meer raadplegen. U zal ook geen toegang meer hebben tot uw bestellingen, adressen, enz. Maak uw beslissing hieronder:';
+$_['text_button_confirm_deletion'] = 'JA, verwijder al mijn gegevens';
+$_['text_button_cancel_deletion'] = 'Nee, hou mijn gegevens intact';
+
+// GDPR Compliance 1.1/2.1./3.1
+$_['text_order'] = 'Bestelling';
+$_['text_personal_data'] = 'Persoonlijke gegeven gast';
+$_['text_personal_data_helper'] = 'Als u geen geregistreerde klant bent, kunnen er mogelijk toch bewaarde gegevens zijn voor dit specifieke mailadres. Hieronder kunt u eventueel bewaarde gegevens opvragen.';
+$_['text_payment_view_data'] = 'Betalingsinformatie';
+$_['text_shipping_view_data'] = 'Verzendingsinformatie';
+$_['text_order_view_data'] = 'Bestellingsinformatie';
+$_['text_user_agent'] = 'User Agent';
+$_['text_accept_language'] = 'Accepteer taal';
+
+// GDPR Compliance 1.2/2.2./3.2
+$_['text_cookie_settings_header'] = 'Kies welk type cookies u wenst te accepteren.';
+$_['text_btn_close'] = 'Sluiten';
+$_['text_btn_save'] = 'Bewaar';
+$_['text_general_cookies'] = 'Strikt noodzakelijke cookies';
+$_['text_general_cookies_helper'] = 'Deze cookies zijn nodig om onze webwinkel te laten werken en kunnen niet uitgeschakeld worden. Deze cookies worden enkel geplaatst bij door u genomen acties zoals het instellen van uw taal, valuta of inlogsessies of privacy instellingen. U kan uw browser instellen deze cookies te blokkeren, maar onze site kan dan mogelijk niet werken.';
+$_['text_general_analytics'] = 'Analytics and Statistieken';
+$_['text_general_analytics_helper'] = 'Deze cookies staan ons toe het bezoekersverkeer te meten verkeersbronnen te raadplegen door het verzamelen in data sets Ze helpen ons ook te begrijpen welke producten en acties populairder zijn dan andere.';
+$_['text_general_marketing'] = 'Marketing and Retargeting';
+$_['text_general_marketing_helper'] = 'Deze cookies zijn meestal ingesteld door onze marketing en adverteer partners. Ze kunnen gebruikt worden om een profiel van uw interesses op te bouwen en u later relevante advertenties te tonen. Als u deze cookies weigert zult u geen advertenties op uw maat of interesses te zien krijgen.';
