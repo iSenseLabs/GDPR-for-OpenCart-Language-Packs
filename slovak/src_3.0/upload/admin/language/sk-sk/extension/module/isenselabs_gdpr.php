84ca37933c3f256c1e13cb4b916d5098f113dcc3
Aleksandar Banderov <banderov@isenselabs.com>
2018-05-23 17:24:30 +0300

    Add Slovak translation

---
+++ slovak/src_3.0/upload/admin/language/sk-sk/extension/module/isenselabs_gdpr.php
@@ -0,0 +1,193 @@
+<?php
+// Heading
+$_['heading_title']    			= 'Súlad s GDPR';
+
+// Text
+$_['text_extension']   			= 'Rozšírenia';
+$_['text_success']     			= 'Úspech: Zmenili ste súlad so službou GDPR!';
+$_['text_edit']        			= 'Upraviť modul';
+$_['text_module']      			= 'Moduly';
+$_['text_complete']    			= 'Hotovo';
+$_['text_error']       			= 'Chyba';
+
+// Entry
+$_['entry_status']     			= 'Stav';
+$_['text_gdpr_status']			= 'Stav rozšírenia';
+$_['text_gdpr_status_helper']   = 'Povoliť alebo zakázať rozšírenie';
+
+// Errors
+$_['error_permission'] 			= 'Upozornenie: Nemáte oprávnenie na modifikáciu modulu!';
+$_['error_unexpected'] 			= 'Vyskytla sa neočakávaná chyba. Skúste to znova neskôr alebo sa obráťte na vývojárov modulu s nasledujúcim ID chyby: ';
+
+// Licensing
+$_['text_your_license']         = 'Vaša licencia';
+$_['text_please_enter_the_code']= 'Zadajte kód licencie:';
+$_['text_activate_license']     = 'Aktivovať licenciu';
+$_['text_not_having_a_license'] = 'Nemáte kód? Získajte ho.';
+$_['text_license_holder']       = 'Držiteľ licencie';
+$_['text_registered_domains']   = 'Registrované domény';
+$_['text_expires_on']           = 'Platnosť licencie uplynie';
+$_['text_valid_license']        = 'PLATNÁ LICENCIA';
+$_['text_manage']               = 'spravovať';
+$_['text_get_support']          = 'Získať podporu';
+$_['text_community']            = 'Komunita';
+$_['text_ask_our_community']    = 'Máme veľkú komunitu. Môžete sa opýtať o vašom probléme na fóre.';
+$_['text_browse_forums']        = 'Prezerať fóra';
+$_['text_tickets']              = 'Tikety';
+$_['text_open_a_ticket']        = 'Chcete spolupracovať s našou technickou podporou? Potom vytvorte tiket na podporu.';
+$_['text_open_ticket_for_real'] = 'Vytvoriť tiket';
+$_['text_pre_sale']             = 'Pre-sale';
+$_['text_pre_sale_text']        = 'Máte skvelý nápad pre váš webový obchod? Náš tím vývojárov to dokáže.';
+$_['text_bump_the_sales']       = 'Bump the sales';
+
+$_['tab_home']       			= 'Kontrolný zoznam';
+$_['tab_controlpanel'] 			= 'Nastavenia';
+$_['tab_support']       		= 'Podpora';
+$_['tab_requests']				= 'Žiadosti';
+$_['tab_acceptances']			= 'Prijímanie pravidiel';
+$_['tab_personaldata']			= 'Tretie strany';
+$_['tab_datasecurity']			= 'Bezpečnosť údajov';
+
+$_['text_loading_data']		 	= 'Načítam dáta, čakajte...';
+$_['text_no_results']			= 'V aktuálnom zobrazení nie sú žiadne výsledky.';
+
+$_['column_request_id']			= 'ID žiadosti';
+$_['column_type']				= 'Typ';
+$_['column_email']				= 'Email';
+$_['column_accept_language']	= 'Prijať jazyk';
+$_['column_user_agent']			= 'Agent používateľa';
+$_['column_server_ip']			= 'IP adresa servera';
+$_['column_client_ip']			= 'IP adresa užívateľa';
+$_['column_date']				= 'Dátum';
+$_['column_acceptance_id']		= 'ID súhlasu';
+$_['column_name']				= 'Názov';
+$_['column_content']			= 'Obsah súhlasu';
+$_['column_actions']			= 'Akcie';
+
+$_['text_date_start']			= 'Dátum od';
+$_['text_date_end']				= 'Dátum do';
+$_['text_filter']				= 'Filtrovať';
+$_['text_download']				= 'Stiahnuť';
+$_['text_view_current']			= 'Zobraziť súčasné pravidlá';
+
+$_['text_life_of_links']		= 'Vypršanie platnosti žiadostí o GDPR';
+$_['text_access_to_personal_data_link'] = 'Prístup k osobným údajom';
+$_['text_access_to_personal_data_link_helper'] = 'Čas vypršania platnosti (v hodinách) každej žiadosti o získanie osobných údajov. Ak zákazník v tejto lehote nepotvrdí, bude musieť podať ďalšiu žiadosť.';
+$_['text_access_default_hours'] = 'Predvolená hodnota: 5 hodín';
+
+$_['text_remove_personal_data_link'] = 'Odstránenie osobných údajov';
+$_['text_remove_personal_data_link_helper'] = 'Čas vypršania platnosti (v hodinách) na odstránenie / anonymizáciu osobných údajov zákazníka. Ak zákazník v tejto lehote nepotvrdí, bude musieť podať ďalšiu žiadosť.';
+$_['text_delete_default_hours'] = 'Predvolená hodnota: 2 hodiny';
+
+$_['text_access_to_personal_data'] = 'Prístup k osobným údajom';
+$_['text_access_to_personal_data_helper'] = 'Špecifikujte, ktorým službám odosielate osobné údaje zákazníka (napr. meno, email, customer_id, IP adresu, atď) do polí nižšie.';
+$_['text_third_party_services'] = 'Služby tretích strán';
+$_['text_third_party_services_helper'] = 'Tu uveďte služby, ktoré používate na sledovanie, analýzu alebo zlepšenie skúseností svojich zákazníkov. Príklad: Facebook reklamy, Facebook pixel, Google Analytics, Google AdWords, Google Retargeting, MailChimp atď.';
+$_['text_other_services'] = 'Ďalšie služby';
+$_['text_other_services_helper'] = 'Tu uveďte služby, ktoré nie sú verejné, alebo niečo, čo sa používa výhradne na interné použitie. Príklad: integrácia ERP, PIWIK, Matomo atď.';
+
+$_['text_general'] = 'Všeobecné nastavenia';
+$_['text_show_gdpr_menu_left'] = 'Zobraziť odkaz na rozšírenie v hlavnom menu administrácie';
+$_['text_show_gdpr_menu_left_helper'] = 'Ak chcete zobraziť odkaz na modul GDPR v hlavnom menu administrácie, povoľte ho.';
+$_['text_enabled'] = 'Zapnuté';
+$_['text_disabled'] = 'Vypnuté';
+$_['text_show_gdpr_menu_left_default'] = 'Predvolená hodnota: Zakázané';
+
+$_['tab_cookieconsent'] = 'Súhlas so súbormi cookie';
+$_['text_cc_enable'] = 'Zobraziť panel so súhlasom s cookies';
+$_['text_cc_enable_helper'] = 'Vyberte, či chcete povoliť alebo zakázať panel so súhlasom s cookies.';
+$_['text_cc_positioning'] = 'Umiestnenie';
+$_['text_cc_positioning_helper'] = 'Vyberte umiestnenie panelu so súhlasom.';
+$_['text_cc_banner_bottom'] = 'Banner v spodnej časti';
+$_['text_cc_banner_top'] = 'Banner v hornej časti';
+$_['text_cc_banner_top_pushdown'] = 'Banner v hornej časti (pushdown)';
+$_['text_cc_banner_float_left'] = 'Plávajúci vľavo';
+$_['text_cc_banner_float_right'] = 'Plávajúci vpravo';
+$_['text_cc_link_to_pp'] = 'Zobraziť odkaz na pravidlá ochrany osobných údajov';
+$_['text_cc_link_to_pp_helper'] = 'Ak je povolené, zobrazí sa na lište odkaz na zásady ochrany osobných údajov.';
+$_['text_cc_texts'] = 'Vlastné texty';
+$_['text_cc_texts_helper'] = 'Definujte texty, ktoré chcete zobraziť v paneli súhlasu so súbormi cookie.';
+$_['text_cc_main_message'] = 'Hlavná správa';
+$_['text_cc_main_message_helper'] = 'Tu zadajte hlavnú správu panela. Príklad je už zobrazený.';
+$_['text_cc_pp_message'] = 'Text odkazu na ochranu osobných údajov';
+$_['text_cc_pp_message_helper'] = 'Ak sa rozhodnete zobraziť odkaz na pravidlá, tu zadajte text odkazu.';
+$_['text_cc_accept_text'] = 'Text tlačidla súhlasu';
+$_['text_cc_accept_text_helper'] = 'Definujte text na tlačidlo so súhlasom.';
+$_['text_cc_dismiss_text'] = 'ZText tlačidla pre zrušenie';
+$_['text_cc_dismiss_text_helper'] = 'Definujte text na tlačidlo pre zrušenie.';
+$_['text_cc_track_pp_clicks'] = 'Sledovať kliknutia ako prijímanie zásad ochrany osobných údajov';
+$_['text_cc_track_pp_clicks_helper'] = 'Ak je povolené, kliknutia na tlačidlo Súhlasím sa budú považovať za prijatie zásad ochrany osobných údajov a budú zaznamenané v module.';
+$_['text_cc_bar_bg'] = 'Pozadie banneru';
+$_['text_cc_bar_text'] = 'Text banneru';
+$_['text_cc_button_bg'] = 'Pozadie tlačidla';
+$_['text_cc_button_text'] = 'Text tlačidla';
+$_['text_cc_palette'] = 'Farebná paleta';
+$_['text_cc_palette_styling'] = 'Zvoľte štýl vášho panela súhlasu so súbormi cookie.';
+
+// Dashboard
+$_['text_steps_gdpr_compliant'] = 'Kontrolný zoznam GDPR';
+$_['text_install_the_extension'] = 'Nainštalujte rozšírenie';
+$_['text_install_the_extension_helper'] = 'Uistite sa, že je modul nainštalovaný a všetky súbory sú nahrané.';
+$_['text_enable_the_extension'] = 'Povoľte rozšírenie';
+$_['text_enable_the_extension_helper'] = 'Prejdite na kartu Nastavenia a aktivujte modul.';
+$_['text_track_policy_acceptances'] = 'Začnite sledovať akceptovanie zásad';
+$_['text_track_policy_acceptances_helper'] = 'Toto je súčasť zákona GDPR, ktorý vyžaduje, aby ste presne vedeli, kedy a s akými pravidlami súhlasili vaši zákazníci.';
+$_['text_respont_gdpr_requests'] = 'Začať prijímať/odpovedať na požiadavky GDPR';
+$_['text_respont_gdpr_requests_helper'] = 'Toto je automaticky riešené rozšírením. Zákonom o GDPR sa vyžaduje, aby ste reagovali na všetky požiadavky GDPR do 72 hodín. Vďaka nášmu rozšíreniu budú okamžite zohľadnené.';
+$_['text_update_pp'] = 'Aktualizujte svoje pravidlá ochrany osobných údajov';
+$_['text_update_pp_helper'] = 'To je niečo, čo musíte robiť ručne. Uistite sa, že máte aktualizované svoje pravidlá ochrany osobných údajov a dodajte, že váš obchod je teraz v súlade so zákonom GDPR.<br /><br />GDPR hovorí, že informácie, ktoré poskytnete ľuďom o tom, ako spracovávať osobné údaje, musia byť:<ul><li>stručné, prehľadné, zrozumiteľné a ľahko dostupné;</li><li>písané v jasnom a zrozumiteľnom jazyku, najmä ak sú adresované dieťaťu;</li><li>bezplatne</li></ul>';
+$_['text_enable_cookie_consent_bar'] = 'Povolenie panelu súhlasu s cookies (voliteľné)';
+$_['text_enable_cookie_consent_bar_helper'] = 'Ak nemáte panel s povolením súborov cookie, môžete vyskúšať náš. Je veľmi prispôsobiteľný a keď niekto klikne na tlačidlo so súhlasom, pridávame do databázy záznam, že tento konkrétny zákazník súhlasil s vašimi zásadami ochrany osobných údajov.<br /><br />Môžete povoliť panel s povolením z karty "Súhlas so súbormi cookie" v nastaveniach modulu.';
+
+$_['text_consent_bar_copyright'] = '* The Cookie Consent Bar was created using the source code of cookieconsent.insites.com, which is originally developed by SilkTide Ltd.';
+$_['text_show_gdpr_footer'] = 'Zobraziť odkaz na rozšírenie v pätičke';
+$_['text_show_gdpr_footer_helper'] = 'Ak chcete zobraziť odkaz na modul GDPR v zápätí vášho obchodu, povoľte ho. Majte na pamäti, že ak je vaša šablóna upravená, odkaz sa nemusí zobraziť a musíte ho pridať ručne.';
+
+$_['text_data_security_breach'] = 'Porušenie bezpečnosti údajov (Data Security Breach)';
+$_['text_notify_customers'] = 'Oznamovanie porušenia bezpečnosti údajov';
+$_['text_data_security_breach_helper'] = 'Túto možnosť použite na informovanie svojich zákazníkov, keď dôjde k porušeniu bezpečnosti údajov.';
+$_['text_data_security_breach_helper_additional'] = 'Musíte svojich zákazníkov presne informovať, kedy došlo k porušeniu, aký je počet dotknutých účtov a aké sú akcie, ktoré ste vykonali po objavení.';
+
+$_['text_gdpr_checklist_helper'] = 'Použite nasledujúci osobný kontrolný zoznam GDPR na označenie každej akcie, ktorú ste dosiahli. Kliknutím zaškrtnite každý bod.';
+$_['text_gdpr_check_1'] = 'Je povolené rozšírenie o súlad s GDPR';
+$_['text_gdpr_check_1_helper'] = 'Toto je prvý jednoduchý krok k dodržiavaniu GDPR.';
+$_['text_gdpr_check_2'] = 'Vytvorte nový odkaz pre GDPR nástroje v pätičke';
+$_['text_gdpr_check_2_helper'] = 'Túto funkciu môžete povoliť na karte <strong>Nastavenia</strong>.';
+$_['text_gdpr_check_3'] = 'Otestujte rozšírenie GDPR na obchode';
+$_['text_gdpr_check_3_helper'] = 'Pridáme vám novú sekciu na fronte obchodu. Akonáhle je rozšírenie povolené, vaši zákazníci a návštevníci budú mať prístup k novej zostave funkcií súvisiacich so službou GDPR. Prosím, otestujte, ako je rozhranie rozšírenia GDPR vizualizované vo obchode - [link].';
+$_['text_gdpr_check_4'] = 'Aktualizujte svoje Zásady ochrany osobných údajov pomocou informácií GDPR';
+$_['text_gdpr_check_4_helper'] = 'Budeme sledovať prijatie zásad ochrany osobných údajov. Stačí sa ubezpečiť, že vaše zásady ochrany osobných údajov v obchode majú sekciu GDPR. Vašu stránku <strong>Zásady ochrany osobných údajov</strong> nájdete tu - [link]. Ak takúto stránku nemáte, vytvorte si ju tu - [link2]. Na [linkstart]tomto blogovom príspevku[linkend] nájdete dobré príklady toho, čo musíte zahrnúť do pravidiel ochrany osobných údajov.';
+$_['text_gdpr_check_5'] = 'Aktualizujte stránku Všeobecné ochodné podmienky s informáciami o GDPR';
+$_['text_gdpr_check_5_helper'] = 'Budeme sledovať prijatie Všeobecných obchodných podmienok. Stačí sa ubezpečiť, že Všeobecné obchodné podmienky v obchode majú sekciu GDPR. Vašu stránku <strong>Všeobecné obchodné podmienky</strong> nájdete tu - [link]. Ak takúto stránku nemáte, vytvorte si ju tu - [link2]. Môžete nájsť dobré príklady toho, čo musíte zahrnúť do svojich podmienok [linkstart]v tomto príspevku[linkend].';
+$_['text_gdpr_check_6'] = 'Umožnite svojim zákazníkom prístup k osobným údajom';
+$_['text_gdpr_check_6_helper'] = 'Máme túto funkciu. Vaši zákazníci si môžu vyžiadať a stiahnuť osobné údaje. Môžete to vyskúšať pomocou testovania s profilom zákazníka - [link]';
+$_['text_gdpr_check_7'] = 'Umožnite vašim zákazníkom "byť zabudnutý"';
+$_['text_gdpr_check_7_helper'] = 'Máme túto funkciu. Môžete si to vyskúšať v obchode - [link]';
+$_['text_gdpr_check_8'] = 'Umožnite svojim zákazníkom, aby mohli upravovať svoje údaje';
+$_['text_gdpr_check_8_helper'] = 'Máme túto funkciu. Môžete si to vyskúšať v obchode - [link]';
+$_['text_gdpr_check_9'] = 'Začnite používať súhlas s povolením cookies, ak ho stále nepoužívate (voliteľné)';
+$_['text_gdpr_check_9_helper'] = 'Máme túto funkciu. Môžete ju povoliť na karte <strong>Súhlas so súbormi cookie</strong> a nakonfigurovať ju podľa požadovaného štýlu.';
+$_['text_gdpr_check_10'] = 'Sledujte a spravujte žiadosti a akceptácie GDPR';
+$_['text_gdpr_check_10_helper'] = 'Máme túto funkciu. Môžete to urobiť v kartách <strong>Žiadosti</strong> a <strong>Prijímanie pravidiel</strong>.';
+$_['text_link'] = '[ODKAZ]';
+$_['text_link_test'] = 'KLIKNITE PRE TEST';
+$_['text_link_view'] = 'KLIKNITE PRE ZOBRAZENIE';
+$_['text_link_add'] = 'KLIKNITE NA PRIDANIE';
+
+// GDPR 1.2/2.2/3.2
+$_['text_cc_cookies_change'] = 'Zmena textu tlačidla s cookies';
+$_['text_cc_cookies_change_helper'] = 'Definujte text na tlačidle, ktoré poskytne zákazníkom možnosť povoliť / zakázať súbory cookie.';
+$_['text_cookie_management'] = 'Správa súborov cookie';
+$_['text_cc_customize'] = 'Prispôsobenie';
+$_['text_ccc_functional'] = 'Prísne vyžadované súbory cookie';
+$_['text_ccc_functional_helper'] = 'Tieto súbory cookie sa vyžadujú pre správnu funkciu webových stránok a nie je možné ich vypnúť. Takéto súbory cookie sa nastavujú iba ako reakcia na akcie, ktoré ste vykonali, napríklad nastavenie jazyka a meny, relácia prihlásenia a predvoľby ochrany osobných údajov. Môžete nastaviť prehliadač, aby zablokoval tieto súbory cookie, ale naša stránka nemusí správne fungovať.';
+$_['text_ccc_analytics'] = 'Analýzy a štatistiky';
+$_['text_ccc_analytics_helper'] = 'Štatistické súbory cookie pomáhajú majiteľom webových stránok pochopiť ako návštevníci interaktujú s webovými stránkami a to zhromažďovaním a oznamovaním informácií anonymne.';
+$_['text_ccc_marketing'] = 'Marketing a retargeting';
+$_['text_ccc_marketing_helper'] = 'Marketingové súbory cookie sa používajú na sledovanie návštevníkov na rôznych webových stránkach. Cieľom je zobrazovať reklamy, ktoré sú pre daného užívateľa relevantné a predávané, a tým sú cennejšie pre inzerentov vydavateľov a tretích strán.';
+$_['text_cc_close_action'] = 'Činnosť po kliknutí na tlačidlo Zavrieť';
+$_['text_cc_close_action_helper'] = 'Vyberte, aké kroky sa majú vykonať, keď zákazníci kliknú na tlačidlo "Zatvoriť" v paneli súhlasu so súbormi cookie.';
+$_['text_cc_stop_nothing'] = 'Nerob nič';
+$_['text_cc_stop_marketing'] = 'Zastaviť marketingové cookies';
+$_['text_cc_stop_analytics'] = 'Zastaviť analytické cookies';
+$_['text_cc_stop_both'] = 'Zastaviť marketingové a analytické cookies';
---
+++ slovak/src_3.0/upload/catalog/language/sk-sk/extension/module/isenselabs_gdpr.php
@@ -0,0 +1,103 @@
+<?php
+$_['text_account'] = 'Zákaznícky účet';
+$_['text_gdpr'] = 'GDPR nástroje';
+
+$_['button_back'] = 'Späť';
+$_['button_continue'] = 'Pokračovať';
+$_['entry_email'] = 'Emailová adresa';
+$_['text_your_email'] = 'Vaša emailová adresa';
+
+$_['text_data_rectification'] = 'Právo na úpravu osobných údajov';
+$_['text_data_rectification_helper'] = 'Ak chcete aktualizovať údaje svojho účtu, použite nižšie uvedené odkazy.';
+$_['text_edit'] = 'Upraviť svoje meno, email a telefónne číslo';
+$_['text_password'] = 'Zmeniť heslo';
+$_['text_address'] = 'Upraviť svoje adresy';
+$_['text_newsletter'] = 'Upraviť odber noviniek';
+
+$_['text_data_portability'] = 'Právo na prenos osobných údajov';
+$_['text_data_portability_helper'] = 'Pomocou odkazov uvedených nižšie si môžete stiahnuť všetky údaje, ktoré ukladáme.';
+$_['text_personal_information'] = 'Osobné údaje';
+$_['text_personal_addresses'] = 'Uložené adresy';
+$_['text_orders'] = 'Objednávky';
+$_['text_gdpr_requests'] = 'GDPR žiadosti';
+
+$_['text_access_to_personal_data'] = 'Právo na prístup k osobným údajom';
+$_['text_access_to_personal_data_helper'] = 'Pomocou odkazu nižšie môžete požiadať o správu, ktorá bude obsahovať všetky osobné informácie, ktoré ukladáme.';
+$_['text_personal_data_helper'] = 'Použite nižšie uvedený formulár a požiadajte o správu, ktorá bude obsahovať všetky osobné údaje, ktoré ukladáme.';
+$_['text_request_a_report'] = 'Požiadať o správu';
+$_['text_enter_your_email'] = 'Zadajte svoj e-mail';
+$_['text_enter_your_email_data_request_helper'] = 'Obdržíte e-mail s odkazom, ktorý Vám po kliknutí ukáže všetky informácie, ktoré o Vá ukladáme. Odkaz bude aktívny %s% hodín.';
+$_['text_how_this_works_data_request'] = 'Ako to funguje?';
+$_['text_successful_request'] = 'Úspech!';
+$_['text_successful_data_request_helper'] = 'Ďakujeme vám za Vašu žiadosť! Ak je e-mailová adresa správna a nachádza sa v našej databáze, obdržíte e-mail s odkazom. Kliknutím na odkaz Vám zobrazíme všetky osobné údaje, ktoré o Vás ukladáme. Nezabudnite, že odkaz bude aktívny len %s% hodín.';
+
+$_['text_personal_data_subject'] = 'Začiarknite toto, ak chcete zobraziť vaše osobné údaje';
+$_['text_greeting'] = 'Dobrý deň. Požiadali ste o zobrazenie Vašich osobných údajov v našom obchode. Kliknutím na odkaz nižšie overte, že:';
+$_['text_button_view_data'] = 'Zobraziť osobné údaje';
+$_['text_actions'] = 'Akcia';
+$_['text_active_link_helper'] = '* Nezabudnite, že odkaz bude aktívny len %s% hodín.';
+
+$_['text_view_personal_data'] = 'Zobraziť osobné údaje';
+$_['text_firstname'] = 'Krstné meno';
+$_['text_lastname'] = 'Priezvisko';
+$_['text_telephone'] = 'Telefón';
+$_['text_email'] = 'Email';
+$_['text_fax'] = 'Fax';
+$_['text_newsletter_subscription'] = 'Odoberanie noviniek';
+$_['text_ip'] = 'IP adresa';
+$_['text_customer_information'] = 'Informácie o zákazníkovi';
+$_['text_saved_addresses'] = 'Uložené adresy';
+$_['text_company'] = 'Spoločnosť';
+$_['text_address_1'] = 'Adresa';
+$_['text_address_2'] = 'Adresa 2';
+$_['text_country'] = 'Krajina';
+$_['text_city'] = 'Mesto';
+$_['text_postcode'] = 'PSČ';
+$_['text_zone'] = 'Kraj';
+$_['text_address'] = 'Adresa';
+$_['text_data_third_party'] = 'Údaje odosielané službám tretích strán';
+$_['text_data_third_party_helper'] = 'Niekedy môžeme poslať časť z Vašich osobných údajov nižšie uvedeným službám. Robíme tak cieľom zabezpečiť, aby ste v našom obchode mali čo najlepšie skúsenosti.';
+$_['text_third_party_services'] = 'Služby';
+$_['text_other'] = 'Ďalšie';
+
+$_['text_hash_error'] = 'Neexistujú žiadne údaje pre zobrazenie. Odkaz je neplatný alebo jeho platnosť vypršala. Ak chcete zobraziť požadované údaje, musíte vytvoriť novú žiadosť.';
+$_['text_no_data_third_party'] = 'Vaše osobné údaje neposielame tretím stranám.';
+$_['text_no_data'] = 'Neexistujú žiadne údaje pre tohto zákazníka!';
+
+$_['text_right_to_be_forgotten'] = 'Právo na zabudnutie';
+$_['text_right_to_be_forgotten_helper'] = 'Túto možnosť použite, ak chcete odstrániť Vaše osobné a iné údaje z nášho obchodu. Nezabudnite, že <strong>tento proces odstráni Váš účet, takže sa už nebudete môcť prihlásiť a účet používať</ strong>.';
+$_['text_request_personal_data_deletion'] = 'Žiadosť o vymazanie osobných údajov';
+$_['text_enter_your_email_data_deletion_helper'] = 'Obdržíte e-mail s odkazom, ktorý po kliknutí odstráni všetky údaje, ktoré v súvislosti s Vašim účtom ukladáme. Ak niektoré údaje nie je možné odstrániť, budú tieto údaje anonymizované. Odkaz bude aktívny %s% hodín.';
+$_['text_successful_data_deletion_helper'] = 'Ďakujeme Vám za Vašu žiadosť! Ak je e-mailová adresa správna a nachádza sa v databáze, obdržíte e-mail s odkazom. Kliknutím na odkaz odstránime všetky osobné údaje, ktoré v súvislosti s Vašim účtom ukladáme. Nezabudnite, že odkaz bude aktívny len %s% hodín.';
+
+$_['text_delete_data_subject'] = 'Žiadosť o vymazanie osobných údajov';
+$_['text_button_delete_data'] = 'Odstrániť moje osobné údaje';
+$_['text_greeting_delete'] = 'Dobrý deň. Požiadali ste o odstránenie Vašich osobných údajov z nášho obchodu. Majte na pamäti, že tento proces odstráni váš účet, takže sa už nebudete môcť prihlásiť a účet používať.<br /><br />Ak ste o vymazanie údajov nepožiadali Vy, ignorujte túto správu. V opačnom prípade kliknutím na odkaz nižšie pokračujte:';
+
+$_['text_personal_data_deletion'] = 'Zmazanie osobných údajov';
+$_['text_hash_error_delete'] = 'Akciu nemožno dokončiť. Odkaz je neplatný alebo jeho platnosť vypršala. Ak chcete vykonať túto akciu, musíte vytvoriť novú žiadosť.';
+$_['text_confirm_the_action'] = 'Potvrdenie zákazníka je potrebné';
+$_['text_confirm_the_action_helper'] = '<strong>Táto akcia je nezvratná!</ strong> Ak túto akciu potvrdíte, Váš účet na našom webe bude odstránený a nebudete sa s ním už môcť prihlásiť. Nebudete mať ani prístup k vašim objednávkam, adresám atď. Prosím, urobte svoje rozhodnutie nižšie:';
+$_['text_button_confirm_deletion'] = 'Áno, zmazať všetky moje údaje';
+$_['text_button_cancel_deletion'] = 'Nie, nechajte moje údaje neporušené';
+
+// GDPR Compliance 1.1/2.1./3.1
+$_['text_order'] = 'Objednávka';
+$_['text_personal_data'] = 'Osobné údaje hostí';
+$_['text_personal_data_helper'] = 'Ak zákazník nie je zaregistrovaný, v databáze sa môžu nachádzať aj údaje o tomto konkrétnom e-maile. Tu uvidíte informácie, ktoré sme mohli vytiahnuť z tabuľky objednávok.';
+$_['text_payment_view_data'] = 'Informácie o platbe';
+$_['text_shipping_view_data'] = 'informácie o doprave';
+$_['text_order_view_data'] = 'Informácie o objednávke';
+$_['text_user_agent'] = 'Agent používateľa';
+$_['text_accept_language'] = 'Prijať jazyk';
+
+// GDPR Compliance 1.2/2.2./3.2
+$_['text_cookie_settings_header'] = 'Zvoľte typ súborov cookie, s ktorých použitím súhlasíte';
+$_['text_btn_close'] = 'Zavrieť';
+$_['text_btn_save'] = 'Uložiť zmeny';
+$_['text_general_cookies'] = 'Povinné';
+$_['text_general_cookies_helper'] = 'Tieto súbory cookie sa vyžadujú pre správnu funkciu webových stránok a nie je možné ich vypnúť. Takéto súbory cookie sa nastavujú iba ako reakcia na akcie, ktoré ste vykonali, napríklad nastavenie jazyka a meny, relácia prihlásenia a predvoľby ochrany osobných údajov. Môžete nastaviť prehliadač, aby zablokoval tieto súbory cookie, ale naša stránka nemusí správne fungovať.';
+$_['text_general_analytics'] = 'Analýzy a štatistiky';
+$_['text_general_analytics_helper'] = 'Štatistické súbory cookie pomáhajú majiteľom webových stránok pochopiť ako návštevníci interaktujú s webovými stránkami a to zhromažďovaním a oznamovaním informácií anonymne.';
+$_['text_general_marketing'] = 'Marketing a retargeting';
+$_['text_general_marketing_helper'] = 'Marketingové súbory cookie sa používajú na sledovanie návštevníkov na rôznych webových stránkach. Cieľom je zobrazovať reklamy, ktoré sú pre daného užívateľa relevantné a predávané, a tým sú cennejšie pre inzerentov vydavateľov a tretích strán.';
