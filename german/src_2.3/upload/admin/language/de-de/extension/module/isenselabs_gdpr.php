e488b2eda2e93c44caee36900de4b01b0ed3012e
Aleksandar Banderov <banderov@isenselabs.com>
2018-05-24 08:51:21 +0300

    Add German translation

---
+++ german/src_2.3/upload/admin/language/de-de/extension/module/isenselabs_gdpr.php
@@ -0,0 +1,193 @@
+<?php
+// Heading
+$_['heading_title']    			= 'GDPR Compliance - DSGVO-Konformit&auml;t';
+
+// Text
+$_['text_extension']   			= 'Erweiterungen';
+$_['text_success']     			= '&Auml;nderungen erfolgreich &uuml;bernommen';
+$_['text_edit']        			= 'Module editieren';
+$_['text_module']      			= 'Module';
+$_['text_complete']    			= 'Vollst&auml;ndig';
+$_['text_error']       			= 'Fehler';
+
+// Entry
+$_['entry_status']     			= 'Status';
+$_['text_gdpr_status']			= 'Erweiterungsstatus';
+$_['text_gdpr_status_helper']   = 'Erweiterung aktivieren oder deaktivieren';
+
+// Errors
+$_['error_permission'] 			= 'Achtung: Sie haben keine Berechtigung, das Modul zu bearbeiten!';
+$_['error_unexpected'] 			= 'Ein unerwarteter Fehler ist aufgetreten. Bitte versuchen Sie es sp&auml;ter noch einmal oder kontaktieren Sie die Enwickler des Moduls mit folgender Fehler-ID: ';
+
+// Licensing
+$_['text_your_license']         = 'Ihre Lizenz';
+$_['text_please_enter_the_code']= 'Bitte geben Sie Ihren Lizenzschl&uuml;ssel ein:';
+$_['text_activate_license']     = 'Lizenz aktivieren';
+$_['text_not_having_a_license'] = 'Sie haben keinen Schl&uuml;ssel? Hier bekommen Sie einen.';
+$_['text_license_holder']       = 'Lizenznehmer';
+$_['text_registered_domains']   = 'Registrierte Domains';
+$_['text_expires_on']           = 'Lizenz l&auml;uft ab am';
+$_['text_valid_license']        = 'G&Uuml;LTIGE LIZENZ';
+$_['text_manage']               = 'bearbeiten';
+$_['text_get_support']          = 'Support erhalten';
+$_['text_community']            = 'Community';
+$_['text_ask_our_community']    = 'Wir haben eine gro&szlig;e Community. Sie k&ouml;nnen gern im Forum nach einer L&ouml;sung f&uuml;r Ihr Problem fragen.';
+$_['text_browse_forums']        = 'Foren aufrufen';
+$_['text_tickets']              = 'Tickets';
+$_['text_open_a_ticket']        = 'M&ouml;chten Sie direkt mit unseren Technikern in Kontakt treten? Dann er&ouml;ffnen Sie bitte ein Support-Ticket.';
+$_['text_open_ticket_for_real'] = 'Ein Ticket er&ouml;ffnen';
+$_['text_pre_sale']             = 'Pre-Sale';
+$_['text_pre_sale_text']        = 'Sie haben eine richtig gute Idee f&uuml;r Ihren Webshop? Unser Entwicklerteam kann sie realisieren.';
+$_['text_bump_the_sales']       = 'Verk&auml;ufe ankurbeln';
+
+$_['tab_home']       			= 'Checkliste';
+$_['tab_controlpanel'] 			= 'Einstellungen';
+$_['tab_support']       		= 'Support';
+$_['tab_requests']				= 'Daten-Anfragen';
+$_['tab_acceptances']			= 'Einwilligungen';
+$_['tab_personaldata']			= 'Drittanbieter';
+$_['tab_datasecurity']			= 'Datensicherheit';
+
+$_['text_loading_data']		 	= 'L&auml;dt, bitte warten ...';
+$_['text_no_results']			= 'Keine Ergebnisse in dieser Ansicht.';
+
+$_['column_request_id']			= 'Anfrage-ID';
+$_['column_type']				= 'Typ';
+$_['column_email']				= 'E-Mail';
+$_['column_accept_language']	= 'Sprache';
+$_['column_user_agent']			= 'User Agent';
+$_['column_server_ip']			= 'Server-IP';
+$_['column_client_ip']			= 'Client-IP';
+$_['column_date']				= 'Datum';
+$_['column_acceptance_id']		= 'Einwilligungs-ID';
+$_['column_name']				= 'Name';
+$_['column_content']			= 'Textinhalt';
+$_['column_actions']			= 'Aktionen';
+
+$_['text_date_start']			= 'Startdatum';
+$_['text_date_end']				= 'Endedatum';
+$_['text_filter']				= 'Filter';
+$_['text_download']				= 'Download';
+$_['text_view_current']			= 'Aktuelle Datenschutzerkl&auml;rung ansehen';
+
+$_['text_life_of_links']		= 'Ablaufzeit von DSGVO-Anfragen';
+$_['text_access_to_personal_data_link'] = 'Zugriff auf pers&ouml;nliche Daten';
+$_['text_access_to_personal_data_link_helper'] = 'Ablaufzeit (in Stunden) der Anfrage auf Herausgabe pers&ouml;nlicher Daten. Wenn der Kunde die Anfrage innerhalb dieser Zeit nicht best&auml;tigt, muss er eine neue stellen.';
+$_['text_access_default_hours'] = 'Voreinstellung: 5 Stunden';
+
+$_['text_remove_personal_data_link'] = 'L&ouml;schung pers&ouml;nlicher Daten';
+$_['text_remove_personal_data_link_helper'] = 'Ablaufzeit (in Stunden) des Antrags, pers&ouml;nliche Daten des Kunden zu l&ouml;schen/zu anonymisieren. Wenn der Kunde den Antrag innerhalb dieser Zeit nicht best&auml;tigt, muss er einen neuen stellen.';
+$_['text_delete_default_hours'] = 'Voreinstellung: 2 Stunden';
+
+$_['text_access_to_personal_data'] = 'Weitergabe pers&ouml;nlicher Daten';
+$_['text_access_to_personal_data_helper'] = 'Bitten geben Sie an, an welche Drittanbieter Sie pers&ouml;nliche Kundendaten &uuml;bermitteln (also Name, E-Mail, Kundennummer, IP usw.).';
+$_['text_third_party_services'] = 'Drittanbieter-Dienste';
+$_['text_third_party_services_helper'] = 'Bitte geben Sie an, welche Drittanbieter-Dienste Sie nutzen, um Ihre Kunden zu verfolgen (Tracking), das Kundenverhalten zu analysieren oder das Einkaufserlebnis zu verbessern. Beispiele: Facebook-Werbung, Facebook-Pixel, Google Analytics, Google AdWords, Google Retargeting, MailChimp usw.';
+$_['text_other_services'] = 'Andere Drittanbieter';
+$_['text_other_services_helper'] = 'Bitte geben Sie hier an, welche nicht-&ouml;ffentlichen Drittanbieter oder Dienste, die ausschlie&szlig;lich f&uuml;r internen Gebrauch bestimmt sind, Sie nutzen. Beispiele: ERP-Integration, PIWIK, Matomo usw.';
+
+$_['text_general'] = 'Allgemeine Einstellungen';
+$_['text_show_gdpr_menu_left'] = 'Link zu dieser Erweiterung im Haupt-Admin-Men&uuml; anzeigen';
+$_['text_show_gdpr_menu_left_helper'] = 'Aktivieren Sie das, wenn Sie einen Link zum DSGVO-Modul im Haupt-Admin-Men&uuml; angezeigt haben m&ouml;chten.';
+$_['text_enabled'] = 'Aktiviert';
+$_['text_disabled'] = 'Deaktiviert';
+$_['text_show_gdpr_menu_left_default'] = 'Voreinstellung: Deaktiviert';
+
+$_['tab_cookieconsent'] = 'Cookie-Hinweis';
+$_['text_cc_enable'] = 'Leiste mit Cookie-Hinweis anzeigen';
+$_['text_cc_enable_helper'] = 'W&auml;hlen Sie aus, ob Sie die Cookie-Hinweis-Leiste aktivieren oder deaktivieren m&ouml;chten.';
+$_['text_cc_positioning'] = 'Position';
+$_['text_cc_positioning_helper'] = 'Bitte w&auml;hlen Sie die Position der Cookie-Leiste.';
+$_['text_cc_banner_bottom'] = 'Banner unten';
+$_['text_cc_banner_top'] = 'Banner oben';
+$_['text_cc_banner_top_pushdown'] = 'Banner oben (herunterrollend)';
+$_['text_cc_banner_float_left'] = 'Links schwebend';
+$_['text_cc_banner_float_right'] = 'Rechts schwebend';
+$_['text_cc_link_to_pp'] = 'Link zur Datenschutzerkl&auml;rung in der Leiste anzeigen';
+$_['text_cc_link_to_pp_helper'] = 'Wenn aktiviert, wird ein Link zur Datenschutzerkl&auml;rung in der Cookie-Leiste angezeigt.';
+$_['text_cc_texts'] = 'Eigene Texte';
+$_['text_cc_texts_helper'] = 'Bitte geben Sie an, welche Texte in der Cookie-Leiste angezeigt werden sollen.';
+$_['text_cc_main_message'] = 'Cookie-Hinweistext';
+$_['text_cc_main_message_helper'] = 'Bitte geben Sie den Text ein, der in der Cookie-Leiste erscheinen soll. Ein Beispieltext ist bereits vorgegeben.';
+$_['text_cc_pp_message'] = 'Link-Text zur Datenschutzerkl&auml;rung';
+$_['text_cc_pp_message_helper'] = 'Wenn Sie einen Link zur Datenschutzerkl&auml;rung anzeigen m&ouml;chten, geben Sie hier bitte den Link-Text an.';
+$_['text_cc_accept_text'] = 'Akzeptieren-Schaltfl&auml;che';
+$_['text_cc_accept_text_helper'] = 'Die Beschriftung der Akzeptieren-Schaltfl&auml;che.';
+$_['text_cc_dismiss_text'] = 'Ablehnen-Schaltfl&auml;che';
+$_['text_cc_dismiss_text_helper'] = 'Die Beschriftung der Ablehnen-Schaltfl&auml;che.';
+$_['text_cc_track_pp_clicks'] = 'Klicks als Einwilligung in die Datenverarbeitung laut Datenschutzrerkl&auml;rung werten';
+$_['text_cc_track_pp_clicks_helper'] = 'Wenn aktiviert, werden Klicks auf die "Ich stimme zu"-Schaltfl&auml;che als Einwilligung in die Datenverarbeitung laut Datenschutzrerkl&auml;rung gewertet und im Log aufgezeichnet.';
+$_['text_cc_bar_bg'] = 'Banner-Hintergrund';
+$_['text_cc_bar_text'] = 'Banner-Text';
+$_['text_cc_button_bg'] = 'Schaltfl&auml;chenhintergrund';
+$_['text_cc_button_text'] = 'Schaltfl&auml;chenbeschriftung';
+$_['text_cc_palette'] = 'Farbpalette';
+$_['text_cc_palette_styling'] = 'Bitte w&auml;hlen Sie den Stil der Cookie-Leiste.';
+
+// Dashboard
+$_['text_steps_gdpr_compliant'] = 'DSGVO-Checkliste';
+$_['text_install_the_extension'] = 'Erweiterung installieren';
+$_['text_install_the_extension_helper'] = 'Stellen Sie sicher, dass das Modul installiert ist und alle Dateien in den Shop hochgeladen sind.';
+$_['text_enable_the_extension'] = 'Erweiterung aktivieren';
+$_['text_enable_the_extension_helper'] = 'Rufen Sie den Systemeinstellungstab auf und aktivieren Sie das Modul.';
+$_['text_track_policy_acceptances'] = 'Das Einwilligen in die Datenverarbeitung aufzeichnen';
+$_['text_track_policy_acceptances_helper'] = 'Die DSGVO erfordert, dass Sie genau dar&uuml;ber Auskunft geben k&ouml;nnen, wann Ihre Kunden in welche Form der Datenverarbeitung und -speicherung eingewilligt haben.';
+$_['text_respont_gdpr_requests'] = 'DSGVO-Anfragen annehmen/beantworten';
+$_['text_respont_gdpr_requests_helper'] = 'Das handhabt die Erweiterung automatisch. Die DSGVO erfordert, dass Sie alle den Datenschutz betreffenden Anfragen innerhalb von 72 Stunden beantworten m&uuml;ssen. Unsere Erweiterung &uuml;bernimmt das f&uuml;r Sie sofort automatisch.';
+$_['text_update_pp'] = 'Datenschutzerkl&auml;rung aktualisieren';
+$_['text_update_pp_helper'] = 'Das m&uuml;ssen Sie leider selbst &uuml;bernehmen. Stellen Sie sicher, dass die Datenschutzerkl&auml;rung Ihres Shops der DSGVO entspricht.<br /><br />Laut DSGVO m&uuml;ssen die Informationen dar&uuml;ber, wie Sie die pers&ouml;nlichen Daten Ihrer Kunden verarbeiten und speichern, "in pr&auml;ziser, transparenter, verst&auml;ndlicher und leicht zug&auml;nglicher Form in einer klaren und einfachen Sprache" &uuml;bermittelt werden, und sie m&uuml;ssen kostenfrei erreichbar sein.';
+$_['text_enable_cookie_consent_bar'] = 'Cookie-Leiste aktivieren (optional)';
+$_['text_enable_cookie_consent_bar_helper'] = 'Wenn Sie noch keine Cookie-Leiste haben, dann versuchen Sie es doch mal mit unserer. Sie ist vielseitig konfigurierbar, und sobald die "Ich stimme zu"-Schaltfl&auml;che angeklickt wird, wird in der Datenbank ein Datensatz angelegt, dass dieser Kunde in die Verarbeitung seiner Daten eingewilligt hat. <br /><br />Sie k&ouml;nnen die Cookie-Leiste in dem Tab "Cookie Leiste" in den Einstellungen dieses Moduls aktivieren.';
+
+$_['text_consent_bar_copyright'] = '* Die Cookie-Leiste wurde erstellt unter Verwendung von Quellcode von cookieconsent.insites.com, der urspr&uuml;nglich von SilkTide Ltd entwickelt wurde.';
+$_['text_show_gdpr_footer'] = 'Link zur Erweiterung in der Fu&szlig;zeile anzeigen';
+$_['text_show_gdpr_footer_helper'] = 'Aktivieren Sie das, wenn Sie einen Link zum DSGVO-Modul in die Fu&szlig;zeile Ihres Shops einf&uuml;gen wollen. Haben Sie Ihren Shop jedoch sehr stark modifiziert, kann es sein, dass der Link nicht angezeigt wird. Sie m&uuml;ssen ihn dann manuell hinzuf&uuml;gen.';
+
+$_['text_data_security_breach'] = 'Verletzung der Datensicherheit';
+$_['text_notify_customers'] = 'Benachrichtigung &uuml;ber eine Verletzung der Datensicherheit/Datenpanne';
+$_['text_data_security_breach_helper'] = 'Benutzen Sie diese Option, um Ihre Kunden &uuml;ber eine Verletzung der Datensicherheit zu informieren (Datenpanne, Verlust von pers&ouml;nlichen Daten durch Datendiebstahl).';
+$_['text_data_security_breach_helper_additional'] = 'Sie m&uuml;ssen Ihre Kunden dar&uuml;ber informieren, wann genau die Datenpanne aufgetreten ist, wie viele Kundenkonten betroffen sind und welche Ma&szlig;nahmen Sie nach Entdeckung des Sicherheitslecks ergriffen haben/ergreifen.';
+
+$_['text_gdpr_checklist_helper'] = 'In der folgenden Checkliste k&ouml;nnen Sie abhaken, welche Ma&szlig;nahmen Sie bereits getroffen haben, um Ihren Shop DSGVO-gerecht abzusichern. Klicken Sie dazu jeden Punkt an.';
+$_['text_gdpr_check_1'] = 'DSGVO-Erweiterung ist aktiviert';
+$_['text_gdpr_check_1_helper'] = 'Der erste Schritt zur DSGVO-Konformit&auml;t.';
+$_['text_gdpr_check_2'] = 'DSGVO-Link in der Fu&szlig;zeile angezeigt';
+$_['text_gdpr_check_2_helper'] = 'Sie brauchen das nur im Einstellungs-Tab zu aktivieren, dann wird der Link automatisch angezeigt.';
+$_['text_gdpr_check_3'] = 'DSGVO-Erweiterung im Front-End getestet';
+$_['text_gdpr_check_3_helper'] = 'Wir f&uuml;gen dem Front-End des Shops einen neuen Bereich hinzu. Wenn die Erweiterung aktiviert ist, haben Ihre Kunden und Besucher Zugang zu DSGVO-relevanten Funktionen im Shop. Bitte testen Sie, wie die Erweiterung im Front-End dargestellt wird - [link] - und ob sie sich gut in Ihr Shop-Theme einpasst. Testen Sie auch die einzelnen Funktionen durch.';
+$_['text_gdpr_check_4'] = 'Datenschutzerkl&auml;rung DSGVO-gem&auml;&szlig; aktualisiert';
+$_['text_gdpr_check_4_helper'] = 'Wir zeichnen die Einwilligungen der Kunden/Besucher in die Verarbeitung der Daten laut Datenschutzerkl&auml;rung auf. Stellen Sie nur sicher, dass die Datenschutzerkl&auml;rung Ihres Shops DSGVO-konform ist. Sie k&ouml;nnen die Erl&auml;rung hier aufrufen: [link]. Wenn Sie noch keine Seite mit der Datenschutzerkl&auml;rung eingerichtet haben, k&ouml;nnen Sie das hier tun: [link2]. Eine gute ausf&uuml;hrliche Erkl&auml;rung dar&uuml;ber, was in der Datenschutzerkl&auml;rung drinstehen sollte, gibt es bei [linkstart]Selbst&auml;ndig im Netz[linkend].';
+$_['text_gdpr_check_5'] = 'AGB aktualisiert';
+$_['text_gdpr_check_5_helper'] = 'Wir zeichen die Einwilligungen Ihrer Kunden in die AGB auf. Stellen Sie nur sicher, dass die AGB Ihres Shops ebenfalls DSGVO-konform sind. Ihre AGB-Seite finden Sie hier: [link].';
+$_['text_gdpr_check_6'] = 'Kunden ist der Zugriff auf ihre im Shop gespeicherten pers&ouml;nlichen Daten gew&auml;hrt';
+$_['text_gdpr_check_6_helper'] = 'Kunden k&ouml;nnen per Mausklick die gespeicherten pers&ouml;nlichen Daten anfordern und herunterladen. Sie k&ouml;nnen das hier mit einem Kundenprofil ausprobieren - [link]';
+$_['text_gdpr_check_7'] = 'Kunden ist das Recht auf L&ouml;schung aller Daten (Recht auf Vergessenwerden) einger&auml;umt';
+$_['text_gdpr_check_7_helper'] = 'Diese Funktion ist in unserem Modul ebenfalls vorhanden. Sie k&ouml;nnen das hier ausprobieren - [link]';
+$_['text_gdpr_check_8'] = 'Kunden k&ouml;nnen ihre pers&ouml;nlichen Daten selbst &auml;ndern';
+$_['text_gdpr_check_8_helper'] = 'Auch diese Funktion ist in unserem Modul enthalten. Sie k&ouml;nnen das hier ausprobieren - [link]';
+$_['text_gdpr_check_9'] = 'Die Cookie-Einwilligung ist aktiviert (optional)';
+$_['text_gdpr_check_9_helper'] = 'Falls Sie es noch nicht getan haben, k&ouml;nnen Sie die Cookie-Leiste im Tab "Cookie-Leiste" aktivieren und konfigurieren.';
+$_['text_gdpr_check_10'] = 'DSGVO-Anfragen und -Einwilligungen aufzeichen';
+$_['text_gdpr_check_10_helper'] = 'Und auch das kann unsere Erweiterung. In den Tabs "Daten-Anfragen" und "Einwilligungen" k&ouml;nnen Sie das einstellen.';
+$_['text_link'] = '[LINK]';
+$_['text_link_test'] = 'KLICKEN F&Uuml;R TEST';
+$_['text_link_view'] = 'KLICKEN UM ANZUZEIGEN';
+$_['text_link_add'] = 'KLICKEN UM HINZUZUF&Uuml;GEN';
+
+// GDPR 1.2/2.2/3.2
+$_['text_cc_cookies_change'] = 'Cookie-Auswahlschaltfl&auml;che anpassen';
+$_['text_cc_cookies_change_helper'] = 'Ihre Kunden haben die M&ouml;glichkeit zu entscheiden, welche Cookies sie akzeptieren wollen. Bitte definieren Sie hier die Beschriftung dieser Schaltfl&auml;che.';
+$_['text_cookie_management'] = 'Cookie-Management';
+$_['text_cc_customize'] = 'Anpassen';
+$_['text_ccc_functional'] = 'Unverzichtbare Cookies';
+$_['text_ccc_functional_helper'] = 'Diese Cookies sind f&uuml;r das vollst&auml;ndige Funktionieren des Webshops erforderlich und k&ouml;nnen nicht abgeschaltet werden. Sie enthalten die Einstellungen, die Sie f&uuml;r sich im Shop vorgenommen haben, also bspw. Sprache, W&auml;hrung, Login-Sitzung (um zwischen Seitenwechseln eingeloggt zu bleiben), Privatsph&auml;reneinstellungen. Sie k&ouml;nnen in Ihrem Browser zwar einstellen, diese Cookies zu blockieren, aber dann wird der Shop nur noch eingeschr&auml;nkt oder gar nicht mehr funktionieren.';
+$_['text_ccc_analytics'] = 'Datenanalyse und Statistiken';
+$_['text_ccc_analytics_helper'] = 'Mit diesen Cookies erfassen wir die Besuchsstatistiken des Shops, also bspw. welche Artikel wie oft angeklickt und anschlie&szlig;end gekauft wurden. Die Analyse dieser Daten hilft uns, das Warenangebot unseres Shops besser den Kundenw&uuml;nschen anzupassen.';
+$_['text_ccc_marketing'] = 'Marketing und Re-Targeting';
+$_['text_ccc_marketing_helper'] = 'Solche Cookies werden im Normalfall von unseren Marketing- und Werbepartnern gesetzt. Mit diesen Cookies kann von Ihnen ein Interessenprofil erstellt werden, um Ihnen f&uuml;r Sie zugeschnittene Werbung anzuzeigen (Targeted Ads). Wenn Sie diese Cookies ablehnen, wird Ihnen die Werbung nicht basierend auf Ihren Interessen angezeigt.';
+$_['text_cc_close_action'] = 'Auszuf&uuml;hrende Aktion nach Klicken der "Schlie&szlig;en"-Schaltfl&auml;che';
+$_['text_cc_close_action_helper'] = 'Was soll passieren, wenn die Kunden auf "Schlie&szlig;en" in der Cookie-Leiste klicken?';
+$_['text_cc_stop_nothing'] = 'Nichts';
+$_['text_cc_stop_marketing'] = 'Keine Marketing-Cookies setzen';
+$_['text_cc_stop_analytics'] = 'Keine Datenanalyse-Cookies setzen';
+$_['text_cc_stop_both'] = 'Keine Marketing- und Datenanalyse-Cookies setzen';
