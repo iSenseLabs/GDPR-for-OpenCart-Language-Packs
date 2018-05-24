7b974dc513e02aa4219b5010a946d785622a46a9
Aleksandar Banderov <banderov@isenselabs.com>
2018-05-24 10:23:24 +0300

    Add Slovenian translation

---
+++ slovenian/src_1.5/upload/admin/language/slovenian/module/isenselabs_gdpr.php
@@ -0,0 +1,193 @@
+<?php
+// Heading
+$_['heading_title']    			= 'Skladnost z GDPR';
+
+// Text
+$_['text_extension']   			= 'Razširitve';
+$_['text_success']     			= 'Uspeh: spremenili ste Skladnost z GDPR!';
+$_['text_edit']        			= 'Uredi modul';
+$_['text_module']      			= 'Moduli';
+$_['text_complete']    			= 'Dokončano';
+$_['text_error']       			= 'Napaka';
+
+// Entry
+$_['entry_status']     			= 'Status';
+$_['text_gdpr_status']			= 'Status razširitve';
+$_['text_gdpr_status_helper']   = 'Omogočite ali onemogočite razširitev';
+
+// Errors
+$_['error_permission'] 			= 'Opozorilo: nimate dovoljenja za spreminjanje modula!';
+$_['error_unexpected'] 			= 'Prišlo je do nepričakovane napake. Poskusite znova pozneje ali se obrnite na razvijalce modula z naslednjim ID-jem napake: ';
+
+// Licensing
+$_['text_your_license']         = 'Licenca';
+$_['text_please_enter_the_code']= 'Prosimo, vnesite kodo za nakup izdelka:';
+$_['text_activate_license']     = 'Aktiviraj licenco';
+$_['text_not_having_a_license'] = 'Nimate kode? Pridobite jo tukaj.';
+$_['text_license_holder']       = 'Imetnik licence';
+$_['text_registered_domains']   = 'Registrirane domene';
+$_['text_expires_on']           = 'Licenca poteče';
+$_['text_valid_license']        = 'VELJAVNA LICENCA';
+$_['text_manage']               = 'uredi';
+$_['text_get_support']          = 'Podpora';
+$_['text_community']            = 'Skupnost';
+$_['text_ask_our_community']    = 'Imamo veliko skupnost. Lahko povprašate o vaši težavi na forumu.';
+$_['text_browse_forums']        = 'Prebrskajte forume';
+$_['text_tickets']              = 'Zahtevki za podporo';
+$_['text_open_a_ticket']        = 'Želite komunicirati neposredno z našimi strokovnjaki? Odprite zahtevek za podporo.';
+$_['text_open_ticket_for_real'] = 'Odprite zahtevek za podporo';
+$_['text_pre_sale']             = 'Predprodaja';
+$_['text_pre_sale_text']        = 'Imate briljantno idejo za svojo spletno trgovino? Naša ekipa razvijalcev jo lahko uresniči.';
+$_['text_bump_the_sales']       = 'Pospešite prodajo';
+
+$_['tab_home']       			= 'Kontrolni seznam';
+$_['tab_controlpanel'] 			= 'Nastavitve';
+$_['tab_support']       		= 'Podpora';
+$_['tab_requests']				= 'Zahtevki';
+$_['tab_acceptances']			= 'Sprejem pravilnika';
+$_['tab_personaldata']			= 'Tretja stranka';
+$_['tab_datasecurity']			= 'Varnost podatkov';
+
+$_['text_loading_data']		 	= 'Nalaganje podatkov, prosim počakajte...';
+$_['text_no_results']			= 'V trenutnem pogledu ni rezultatov.';
+
+$_['column_request_id']			= 'ID zahteve';
+$_['column_type']				= 'Tip';
+$_['column_email']				= 'Email';
+$_['column_accept_language']	= 'Jezik';
+$_['column_user_agent']			= 'Uporabniški vmesnik';
+$_['column_server_ip']			= 'IP strežnika';
+$_['column_client_ip']			= 'IP stranke';
+$_['column_date']				= 'Datum';
+$_['column_acceptance_id']		= 'ID potrditve';
+$_['column_name']				= 'Ime';
+$_['column_content']			= 'Besedilo';
+$_['column_actions']			= 'Dejanja';
+
+$_['text_date_start']			= 'Datum začetka';
+$_['text_date_end']				= 'Datum konca';
+$_['text_filter']				= 'Filter';
+$_['text_download']				= 'Prenesi';
+$_['text_view_current']			= 'Oglejte si trenutni pravilnik';
+
+$_['text_life_of_links']		= 'Potek GDPR zahtev';
+$_['text_access_to_personal_data_link'] = 'Dostop do osebnih podatkov';
+$_['text_access_to_personal_data_link_helper'] = 'Čas poteka (v urah) vsakega zahtevka za pridobitev osebnih podatkov. Če stranka v tem času ne potrdi, bo morala vložiti novo zahtevo.';
+$_['text_access_default_hours'] = 'Privzeta vrednost: 5 ur';
+
+$_['text_remove_personal_data_link'] = 'Odstranitev osebnih podatkov';
+$_['text_remove_personal_data_link_helper'] = 'Čas poteka (v urah) za brisanje / anonimiziranje osebnih podatkov stranke. Če stranka v tem času ne potrdi, bo morala vložiti novo zahtevo.';
+$_['text_delete_default_hours'] = 'Privzeta vrednost: 2 uri';
+
+$_['text_access_to_personal_data'] = 'Dostop do osebnih podatkov';
+$_['text_access_to_personal_data_helper'] = 'V spodnjih poljih morate navesti, katerim ponudnikom storitev pošiljate osebne podatke o strankah (npr. ime, elektronska pošta, id_stranke, IP, itd.).';
+$_['text_third_party_services'] = 'Storitve tretjih oseb';
+$_['text_third_party_services_helper'] = 'Tu navedite storitve, ki jih uporabljate za sledenje, analizo ali izboljšanje izkušenj vaših strank. Primer: Facebook Ads, Facebook Pixel, Google Analytics, Google AdWords, Google Retargeting, MailChimp itd.';
+$_['text_other_services'] = 'Druge storitve';
+$_['text_other_services_helper'] = 'Navedite storitve, ki niso javne ali to kar se uporablja izključno za vašo lastno rabo. Primer: Integracija z ERP, PIWIK, Matomo itd.';
+
+$_['text_general'] = 'Splošne nastavitve';
+$_['text_show_gdpr_menu_left'] = 'Prikaži povezavo do razširitve v glavnem meniju administracije';
+$_['text_show_gdpr_menu_left_helper'] = 'Omogočite to, če želite v glavnem meniju skrbnika videti povezavo do modula GDPR.';
+$_['text_enabled'] = 'Omogočeno';
+$_['text_disabled'] = 'Onemogočeno';
+$_['text_show_gdpr_menu_left_default'] = 'Privzeta vrednost: Onemogočeno';
+
+$_['tab_cookieconsent'] = 'Soglasje za piškotke';
+$_['text_cc_enable'] = 'Pokaži vrstico soglasja za piškotke';
+$_['text_cc_enable_helper'] = 'Izberite, ali želite omogočiti ali onemogočiti vrstico soglasja za piškotke.';
+$_['text_cc_positioning'] = 'Izbira položaja';
+$_['text_cc_positioning_helper'] = 'Izberite položaj vrstice soglasja za piškotke.';
+$_['text_cc_banner_bottom'] = 'Pasica na dnu';
+$_['text_cc_banner_top'] = 'Pasica na vrhu';
+$_['text_cc_banner_top_pushdown'] = 'Pasica na vrhu (potisne stran navzdol)';
+$_['text_cc_banner_float_left'] = 'Plavajoča levo';
+$_['text_cc_banner_float_right'] = 'Plavajoča desno';
+$_['text_cc_link_to_pp'] = 'Prikaži povezavo do pravilnika o zasebnosti v vrstici';
+$_['text_cc_link_to_pp_helper'] = 'Če je omogočeno, bo v vrstici prikazana povezava do pravilnika o zasebnosti.';
+$_['text_cc_texts'] = 'Besedila po meri';
+$_['text_cc_texts_helper'] = 'Določite besedila, ki jih želite prikazati v vrstici soglasja za piškotke.';
+$_['text_cc_main_message'] = 'Glavno sporočilo';
+$_['text_cc_main_message_helper'] = 'Vnesite glavno sporočilo vrstice tukaj. Primer je že prikazan.';
+$_['text_cc_pp_message'] = 'Besedilo povezave do Pravilnika o zasebnosti';
+$_['text_cc_pp_message_helper'] = 'Če se odločite za prikaz povezave do pravilnika v vrstici, navedite besedilo povezave tukaj.';
+$_['text_cc_accept_text'] = 'Besedilo gumba Sprejmi';
+$_['text_cc_accept_text_helper'] = 'Določite besedilo na gumbu za sprejem.';
+$_['text_cc_dismiss_text'] = 'Besedilo gumba Zapri';
+$_['text_cc_dismiss_text_helper'] = 'Določite besedilo na gumbu Zapri.';
+$_['text_cc_track_pp_clicks'] = 'Beleži klike kot sprejem pravilnika o zasebnosti';
+$_['text_cc_track_pp_clicks_helper'] = 'Če je omogočeno, se bodo kliki na gumb »Strinjam se« šteli kot sprejem pravilnika o zasebnosti in bodo vpisani v modul.';
+$_['text_cc_bar_bg'] = 'Ozadje vrstice';
+$_['text_cc_bar_text'] = 'Besedilo vrstice';
+$_['text_cc_button_bg'] = 'Ozadje gumba';
+$_['text_cc_button_text'] = 'Besedilo gumba';
+$_['text_cc_palette'] = 'Barvna paleta';
+$_['text_cc_palette_styling'] = 'Izberite obliko vrstice soglasja s piškotki.';
+
+// Dashboard
+$_['text_steps_gdpr_compliant'] = 'Kontrolni seznam za GDPR';
+$_['text_install_the_extension'] = 'Namestite razširitev';
+$_['text_install_the_extension_helper'] = 'Poskrbite, da je modul nameščen &amp; vse datoteke so naložene v vašo trgovino.';
+$_['text_enable_the_extension'] = 'Omogočite razširitev';
+$_['text_enable_the_extension_helper'] = 'Pojdite v nadzorno ploščo in omogočite modul.';
+$_['text_track_policy_acceptances'] = 'Začnite z evidentiranjem sprejemanja pravilnika o sledenju';
+$_['text_track_policy_acceptances_helper'] = 'To je del zakona o GDPR, ki od vas zahteva, da veste, kdaj in s katerim pravilnikom so se stranke strinjale.';
+$_['text_respont_gdpr_requests'] = 'Začetek sprejemanja / odziva na GDPR zahteve';
+$_['text_respont_gdpr_requests_helper'] = 'To samodejno ureja razširitev. Po zakonu o GDPR se morate odzvati na vse GDPR zahtevke v 72 urah. Zahvaljujoč naši razširitvi bodo obdelani takoj.';
+$_['text_update_pp'] = 'Posodobite svoj pravilnik o zasebnosti.';
+$_['text_update_pp_helper'] = 'To je nekaj, kar morate storiti ročno. Poskrbite, da posodobite svoj pravilnik o zasebnosti in dodajte, da je vaša trgovina skladna z zakonodajo o BDP.<br /><br />GDPR pravi, da morajo biti informacije, ki jih posredujete ljudem, o tem kako obdelujete njihove osebne podatke: <ul><li>jedrnate, pregledne, razumljive in lahko dostopne;</li><li>napisana v jasnem in preprostem jeziku, še posebej, če so naslovljena na otroka;</li><li>brezplačne.</li></ul>';
+$_['text_enable_cookie_consent_bar'] = 'Omogoči vrstico soglasja za piškotke (izbirno)';
+$_['text_enable_cookie_consent_bar_helper'] = 'Če nimate vrstice soglasja za piškotke, lahko poskusite našo. Je zelo prilagodljiva in ko nekdo klikne na gumb "Potrjujem", v naši podatkovni bazi dodamo zapis, da se je ta stranka strinjala z vašim pravilnikom o zasebnosti.<br /><br />V nastavitvah modula lahko omogočite našo vrstico soglasja iz zavihka »Soglasje s piškotki«.';
+
+$_['text_consent_bar_copyright'] = '* Vrstica za odobritev piškotka je bila ustvarjena z uporabo izvorne kode cookieconsent.insites.com, ki jo je prvotno razvila družba SilkTide Ltd.';
+$_['text_show_gdpr_footer'] = 'Prikaži povezavo do razširitve v nogi';
+$_['text_show_gdpr_footer_helper'] = 'Omogočite to, če želite videti povezavo do modula GDPR v nogi vaše trgovine. Upoštevajte, da če je vaša tema zelo spremenjena, se povezava morda ne bo prikazala in jo morate dodati ročno.';
+
+$_['text_data_security_breach'] = 'Kršitev varnosti podatkov';
+$_['text_notify_customers'] = 'Obvestilo o kršitvi varnosti podatkov';
+$_['text_data_security_breach_helper'] = 'S to možnostjo obvestite svoje stranke, ko pride do kršitve varnosti podatkov.';
+$_['text_data_security_breach_helper_additional'] = 'Kupce morate natančno informirati o tem, kdaj je prišlo do kršitve, kakšno je število prizadetih računov in kakšne ukrepe ste sprejeli po odkritju.';
+
+$_['text_gdpr_checklist_helper'] = 'Prosimo, uporabite naslednji osebni kontrolni seznam za GDPR, da označite vsak ukrep, ki ste ga zaključili. Kliknite, če želite potrditi vsako točko.';
+$_['text_gdpr_check_1'] = 'Razširitev za skladnosti z GDPR je omogočena';
+$_['text_gdpr_check_1_helper'] = 'To je prvi enostaven korak k BDPR skladnosti.';
+$_['text_gdpr_check_2'] = 'V nogo dodajte novo povezavo do GDPR orodij';
+$_['text_gdpr_check_2_helper'] = 'Ta funkcija je na razpolago, omogočite jo v zavihku <strong>Nastavitve</strong>.';
+$_['text_gdpr_check_3'] = 'Preizkusite razširitev za BDPR skladnost v trgovini';
+$_['text_gdpr_check_3_helper'] = 'V trgovini dodamo nov oddelek. Ko bo razširitev omogočena, bodo vaše stranke in obiskovalci imeli dostop do novega sklopa funkcij, povezanih z GDPR. Preverite kako je razširitev za BDPR skladnost prikazana v trgovini - [link] - in če izgleda dobro na vaši temi. Lahko tudi preskusite uporabniške funkcije.';
+$_['text_gdpr_check_4'] = 'Posodobite svoj pravilnik o zasebnosti z informacijami o GDPR';
+$_['text_gdpr_check_4_helper'] = 'Spremljali bomo sprejemanje vašega pravilnika o zasebnosti. Zagotoviti morate le, da ima pravilnik o zasebnosti vaše spletne trgovine razdelek o GDPR. Svojo stran <strong>Pravilnik o zasebnosti</ strong> lahko najdete tukaj - [link]. Če nimate take strani, jo ustvarite tukaj - [link2]. Dobre primere, kaj morate vključiti v pravilnik o zasebnosti lahko najdete [linkstart]v tem blogu[linkend].';
+$_['text_gdpr_check_5'] = 'Posodobite svojo stran z določili in pogoji z informacijami o GDPR';
+$_['text_gdpr_check_5_helper'] = 'Spremljali bomo sprejemanje vaših splošnih pogojev strani. Zagotoviti morate le, da imajo splošni pogoji spletne trgovine razdelek o GDPR. Vaše <strong>Splošne pogoje</strong> lahko najdete tukaj - [link]. Če nimate take strani, jo ustvarite tukaj - [link2]. Dobre primere, kaj morate vključiti v splošne pogoje lahko najdete [linkstart]v tem blogu[linkend].';
+$_['text_gdpr_check_6'] = 'Omogočite svojim strankam dostop do osebnih podatkov';
+$_['text_gdpr_check_6_helper'] = 'Ta funkcija je na razpolago. Vaše stranke lahko zahtevajo in prenesejo osebne podatke. To lahko preiskusite s profilom kupca - [link]';
+$_['text_gdpr_check_7'] = 'Omogočite vašim strankam, da se njihovi osebni podatki izbrišejo ali anonimizirajo';
+$_['text_gdpr_check_7_helper'] = 'Ta funkcija je na razpolago. To lahko preiskusite v spletni trgovini - [link]';
+$_['text_gdpr_check_8'] = 'Omogočite svojim strankam, da lahko uredijo svoje podatke';
+$_['text_gdpr_check_8_helper'] = 'Ta funkcija je na razpolago. To lahko preiskusite v spletni trgovini - [link]';
+$_['text_gdpr_check_9'] = 'Začnite uporabljati dovoljenje za piškotke, če ga še vedno ne uporabljate (neobvezno)';
+$_['text_gdpr_check_9_helper'] = 'Ta funkcija je na razpolago. Omogočite jo lahko na zavihku <strong>Soglasje s piškotki</strong> in jo nastavite glede na želeni slog.';
+$_['text_gdpr_check_10'] = 'Spremljajte in urejejte GDPR zahtevke in potrditve';
+$_['text_gdpr_check_10_helper'] = 'Ta funkcija je na razpolago. To lahko naredite na zavihku <strong>Zahtevki</strong> in <strong>Potrditve pravilnika</strong>.';
+$_['text_link'] = '[LINK]';
+$_['text_link_test'] = 'KLIKNITE ZA TEST';
+$_['text_link_view'] = 'KLIKNITE ZA OGLED';
+$_['text_link_add'] = 'KLIKNITE ZA DODAJANJE';
+
+// GDPR 1.2/2.2/3.2
+$_['text_cc_cookies_change'] = 'Spremeni besedilo gumba piškotkov';
+$_['text_cc_cookies_change_helper'] = 'Določite besedilo na gumbu, ki bo strankam omogočil, da omogočijo / onemogočijo piškotke.';
+$_['text_cookie_management'] = 'Upravljanje piškotkov';
+$_['text_cc_customize'] = 'Prilagodi';
+$_['text_ccc_functional'] = 'Nujni piškotki';
+$_['text_ccc_functional_helper'] = 'Ti piškotki so potrebni za delovanje spletne strani in jih ni mogoče izklopiti. Taki piškotki so nastavljeni na osnovi vaše izbire jezika, valute, nastavitve zasebnosti in prijavne seje. Brskalnik lahko nastavite tako, da blokira te piškotke, vendar naša spletna mesta morda ne bodo delovala.';
+$_['text_ccc_analytics'] = 'Analitika in statistika';
+$_['text_ccc_analytics_helper'] = 'Ti piškotki omogočajo merjenje prometa obiskovalcev in ogled virov prometa z zbiranjem podatkov v nizih podatkov. Prav tako nam pomagajo razumeti, kateri izdelki in dejanja so bolj priljubljeni kot drugi.';
+$_['text_ccc_marketing'] = 'Marketing in remarketing';
+$_['text_ccc_marketing_helper'] = 'Te piškotke običajno uporabljajo naši tržni in oglaševalski partnerji. Uporabljajo jih lahko za oblikovanje profila vašega zanimanja in kasneje prikazujejo ustrezne oglase. Če teh piškotkov ne dovolite, se vam ne bodo prikazovali oglasi, ki ustrezajo vašim interesom.';
+$_['text_cc_close_action'] = 'Dejanje, ko kliknete gumb "Zapri"';
+$_['text_cc_close_action_helper'] = 'Izberite, kaj se zgodi, ko stranke kliknejo gumb »Zapri« v vrstici soglasja za piškotke.';
+$_['text_cc_stop_nothing'] = 'Ne zgodi se nič';
+$_['text_cc_stop_marketing'] = 'Blokiraj piškotke za marketing';
+$_['text_cc_stop_analytics'] = 'Blokiraj piškotke za analitiko';
+$_['text_cc_stop_both'] = 'Blokiraj piškotke za marketing & analitiko';
---
+++ slovenian/src_2.0.x-2.2/upload/catalog/language/slovenian/module/isenselabs_gdpr.php
@@ -0,0 +1,106 @@
+<?php
+$_['text_account'] = 'Uporabniški račun';
+$_['text_gdpr'] = 'GDPR Orodja';
+
+$_['button_back'] = 'Nazaj';
+$_['button_continue'] = 'Naprej';
+$_['entry_email'] = 'e-naslov';
+$_['text_your_email'] = 'Vaš e-naslov';
+
+$_['text_data_rectification'] = 'Urejanje osebnih podatkov';
+$_['text_data_rectification_helper'] = 'Za urejanje vaših osebnih podatkov, uporabite spodnje povezave.';
+$_['text_edit'] = 'Uredite svoje ime, e-mail, in telefonsko številko';
+$_['text_password'] = 'Spremenite svoje geslo';
+$_['text_address'] = 'Uredite svoj naslov';
+$_['text_newsletter'] = 'Spremenitve nastavitve novic';
+
+$_['text_data_portability'] = 'Prenos podatkov';
+$_['text_data_portability_helper'] = 'Na svoj računalnik lahko prenesete podatke o vašem računu:';
+$_['text_personal_information'] = 'Osebni podatki';
+$_['text_personal_addresses'] = 'Naslovi';
+$_['text_orders'] = 'Naročila';
+$_['text_gdpr_requests'] = 'GDPR zahteve';
+
+$_['text_access_to_personal_data'] = 'Dostop do osebnih podatkov';
+$_['text_access_to_personal_data_helper'] = 'S klikom na spodnjo povezavo lahko zahtevate poročilo z vsemi vašimi osebnimi podatki, ki jih hranimo.';
+$_['text_personal_data_helper'] = 'S tem obrazcem lahko zahtevate poročilo z vsemi vašimi osebnimi podatki, ki jih hranimo.';
+$_['text_request_a_report'] = 'Zahtevajte osebno GDPR poročilo';
+$_['text_enter_your_email'] = 'Vnesite e-naslov';
+$_['text_enter_your_email_data_request_helper'] = 'Potrditveno e-poštno sporočilo bo poslano na vaš naslov. Za nadaljevanje postopka morate klikniti na povezavo v sporočilu. Povezava v sporočilu bo aktivna %s% ur. GDPR poročilo bo nato avtomatično poslano na vaš e-naslov.';
+$_['text_how_this_works_data_request'] = 'Kako to deluje?';
+$_['text_successful_request'] = 'Uspešno!';
+$_['text_successful_data_request_helper'] = 'Vaša zahteva je bila poslana! Če je e-poštni naslov pravilen in je prisoten v bazi podatkov, boste prejeli e-poštno sporočilo s povezavo. Z uporabo povezave boste videli vse osebne podatke, ki jih hranimo za vas. Upoštevajte, da bo povezava aktivna %s% ur.';
+
+$_['text_personal_data_subject'] = 'Preverite svoje osebne podatke';
+$_['text_greeting'] = 'Pozdravljeni! Zahtevali ste vpogled v vaše osebne podatke, ki jih hranimo v naši trgovini. Za ogled kliknite na povezavo spodaj:';
+$_['text_button_view_data'] = 'Ogled osebnih podatkov';
+$_['text_actions'] = 'Možnosti';
+$_['text_active_link_helper'] = '* Upoštevajte, da bo povezava aktivna %s% ur.';
+
+$_['text_view_personal_data'] = 'Ogled osebnih podatkov';
+$_['text_firstname'] = 'Ime';
+$_['text_lastname'] = 'Priimek';
+$_['text_telephone'] = 'Telefon';
+$_['text_email'] = 'E-Mail';
+$_['text_fax'] = 'Fax';
+$_['text_newsletter_subscription'] = 'Naročnina na novice';
+$_['text_ip'] = 'IP';
+$_['text_customer_information'] = 'Informacije o stranki';
+$_['text_saved_addresses'] = 'Shranjeni naslovi';
+$_['text_company'] = 'Podjetje';
+$_['text_address_1'] = '1. vrstica naslova';
+$_['text_address_2'] = '2. vrstica naslova';
+$_['text_country'] = 'Država';
+$_['text_city'] = 'Mesto / kraj';
+$_['text_postcode'] = 'Poštna številka';
+$_['text_zone'] = 'Regija';
+$_['text_address'] = 'Naslov';
+$_['text_data_third_party'] = 'Podatki poslani drugim podjetjem za njihove storitve';
+$_['text_data_third_party_helper'] = 'Za zagotovitev najboljše možne uporabniške izkušnje, lahko včasih posredujemo vaše osebne podatke spodaj naštetim podjetjem oz. ponudnikom storitev.';
+$_['text_third_party_services'] = 'Ponudniki storitev';
+$_['text_other'] = 'Drugo';
+
+$_['text_hash_error'] = 'Ni podatkov, ki bi jih lahko prikazali. Povezava je neveljavna ali je potekla. Če želite videti zahtevane podatke, morate ponoviti zahtevo za prikaz podatkov.';
+$_['text_no_data_third_party'] = 'Vaših osebnih podatkov ne posredujemo tretjim osebam.';
+$_['text_no_data'] = 'Za to stranko ni podatkov!';
+
+$_['text_right_to_be_forgotten'] = 'Pravica do izbrisa';
+$_['text_right_to_be_forgotten_helper'] = 'Z klikom na spodnjo povezavo lahko zahtevate izbris/anonimizacijo vseh vaših podatkov ki jih hranimo. <strong>POMEMBNO! Zavedajte se da je ta proces identičen brisanju vašega računa. Po končanem postopku se v svoj račun NE BOSTE mogli več prijaviti.</strong>.';
+$_['text_request_personal_data_deletion'] = 'Zahteva za izbris osebnih podatkov';
+$_['text_enter_your_email_data_deletion_helper'] = 'Prejeli boste e-poštno sporočilo s povezavo, ki ob kliku izbriše vse podatke, ki jih shranimo za vas. Če nečesa ni možno izbrisati, se bo anonimiziralo. Povezava bo aktivna %s% ur.';
+$_['text_successful_data_deletion_helper'] = 'Vaša zahteva je bila poslana! Če je e-poštni naslov pravilen in je prisoten v bazi podatkov, boste prejeli e-poštno sporočilo s povezavo. Z uporabo povezave boste izbrisali vse osebne podatke, ki jih hranimo za vas. Upoštevajte, da bo povezava aktivna %s% ur.';
+
+$_['text_delete_data_subject'] = 'Zahteva za brisanje osebnih podatkov';
+$_['text_button_delete_data'] = 'Izbrišite moje osebne podatke';
+$_['text_greeting_delete'] = 'Pozdravljeni! Zahtevali ste izbris osebnih podatkov iz naše trgovine. Zavedajte se da je ta proces identičen brisanju vašega računa. Po končanem postopku se v svoj račun NE BOSTE mogli več prijaviti.<br /><br />Če tega niste zahtevali, prosimo, prezrite to sporočilo. V nasprotnem primeru za nadaljevanje kliknite spodnjo povezavo:';
+
+$_['text_personal_data_deletion'] = 'Brisanje osebnih podatkov';
+$_['text_hash_error_delete'] = 'Dejanja ni mogoče dokončati. Povezava je neveljavna ali je potekla. Če želite izvesti ta postopek, morate ponoviti zahtevo.';
+$_['text_confirm_the_action'] = 'Zahtevana je potrditev stranke';
+$_['text_confirm_the_action_helper'] = '<strong>Ta ukrep je nepovraten!</strong> Če potrdite dejanje, bo vaš račun na naši spletni strani izbrisan in se ne boste več mogli prijaviti z njim. Prav tako ne boste imeli dostopa do svojih naročil, naslovov itd. Izberite svojo odločitev spodaj:';
+$_['text_button_confirm_deletion'] = 'Da, izbrišite vse moje podatke';
+$_['text_button_cancel_deletion'] = 'Ne, pustite moje podatke nedotaknjene';
+
+// GDPR Compliance 1.1/2.1./3.1
+$_['text_order'] = 'Naročilo';
+$_['text_personal_data'] = 'Osebni podatki gosta';
+$_['text_personal_data_helper'] = 'Če stranka ni registrirana, lahko v naši podatkovni bazi obstajajo nekateri podatki, ki so bili zabeleženi ob nakupu ali naročilu prejemanja novic, za določen e-poštni naslov. Tukaj boste videli podatke, ki smo jih lahko zbrali iz tabele naročil.';
+$_['text_payment_view_data'] = 'Informacije o plačilu';
+$_['text_shipping_view_data'] = 'Informacije o pošiljanju';
+$_['text_order_view_data'] = 'Informacije o naročilu';
+$_['text_user_agent'] = 'Uporabniški vmesnik';
+$_['text_accept_language'] = 'Izbira jezika';
+
+// GDPR Compliance 1.2/2.2./3.2
+$_['text_cookie_settings_header'] = 'Izberite vrsto piškotkov, ki jih želite uporabljati';
+$_['text_btn_close'] = 'Zapri';
+$_['text_btn_save'] = 'Shrani spremembe';
+$_['text_general_cookies'] = 'Nujni piškotki';
+$_['text_general_cookies_helper'] = 'Ti piškotki so potrebni za delovanje spletne strani in jih ni mogoče izklopiti. Taki piškotki so nastavljeni na osnovi vaše izbire jezika, valute, nastavitve zasebnosti in prijavne seje. Brskalnik lahko nastavite tako, da blokira te piškotke, vendar naša spletna mesta morda ne bodo delovala.';
+$_['text_general_analytics'] = 'Analitika in statistika';
+$_['text_general_analytics_helper'] = 'Ti piškotki omogočajo merjenje prometa obiskovalcev in ogled virov prometa z zbiranjem podatkov v nizih podatkov. Prav tako nam pomagajo razumeti, kateri izdelki in dejanja so bolj priljubljeni kot drugi.';
+$_['text_general_marketing'] = 'Marketing in remarketing';
+$_['text_general_marketing_helper'] = 'Te piškotke običajno uporabljajo naši tržni in oglaševalski partnerji. Uporabljajo jih lahko za oblikovanje profila vašega zanimanja in kasneje prikazujejo ustrezne oglase. Če teh piškotkov ne dovolite, se vam ne bodo prikazovali oglasi, ki ustrezajo vašim interesom.';
+
+// GDPR Compliance 1.2.1/2.2.1/3.2.1
+$_['text_my_gdpr_tools_header'] = 'Moja GDPR orodja';
