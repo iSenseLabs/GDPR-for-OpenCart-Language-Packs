7eccc509dbfacf139855ec6ccf6c7b5a0bd57011
Aleksandar Banderov <banderov@isenselabs.com>
2018-05-24 13:04:35 +0300

    Add new Spanish translation (admin included)

---
+++ spanish/src_2.3/upload/admin/language/es-es/extenstion/module/isenselabs_gdpr.php
@@ -0,0 +1,193 @@
+<?php
+// Heading
+$_['heading_title']    			= 'Cumplimiento RGPD';
+
+// Text
+$_['text_extension']   			= 'Extensiones';
+$_['text_success']     			= 'Exito: ¡Has modificado el Cumplimiento RGPD!';
+$_['text_edit']        			= 'Editar Módulo';
+$_['text_module']      			= 'Módulos';
+$_['text_complete']    			= 'Completar';
+$_['text_error']       			= 'Error';
+
+// Entry
+$_['entry_status']     			= 'Estado';
+$_['text_gdpr_status']			= 'Estado de la Extensión';
+$_['text_gdpr_status_helper']   = 'Habilitar o deshabilitar extensión';
+
+// Errors
+$_['error_permission'] 			= 'Advertencia: ¡No posees los permisos para modificar este módulo!';
+$_['error_unexpected'] 			= 'Error inesperado. Por favor intenta nuevamente o contacta a los desarrolladores del módulo con el siguiente ID de error: ';
+
+// Licensing
+$_['text_your_license']         = 'Tu licencia';
+$_['text_please_enter_the_code']= 'Por favor ingresa el código de licencia de compra del producto:';
+$_['text_activate_license']     = 'Activar licencia';
+$_['text_not_having_a_license'] = '¿No tienes un código? Consíguelo aquí.';
+$_['text_license_holder']       = 'Titular de la licencia';
+$_['text_registered_domains']   = 'Dominios registrados';
+$_['text_expires_on']           = 'La licencia expira el';
+$_['text_valid_license']        = 'LICENCIA VALIDA';
+$_['text_manage']               = 'Administrar';
+$_['text_get_support']          = 'Obtener ayuda';
+$_['text_community']            = 'Comunidad';
+$_['text_ask_our_community']    = 'Tenemos una gran comunidad. Eres libre de preguntar sobre cualquier incidente en los foros.';
+$_['text_browse_forums']        = 'Busca en los foros';
+$_['text_tickets']              = 'Tickets';
+$_['text_open_a_ticket']        = '¿Quieres comunicarte directamente con nuestros técnicos? Abre un caso de soporte.';
+$_['text_open_ticket_for_real'] = 'Abrir un ticket';
+$_['text_pre_sale']             = 'Pre-venta';
+$_['text_pre_sale_text']        = '¿Tienes una idea brillante para tu tienda web? Nuestro equipo de desarrolladores puede hacerla realidad.';
+$_['text_bump_the_sales']       = 'Incrementa las ventas';
+
+$_['tab_home']       			= 'Lista de control';
+$_['tab_controlpanel'] 			= 'Configuración';
+$_['tab_support']       		= 'Soporte';
+$_['tab_requests']				= 'Solicitudes';
+$_['tab_acceptances']			= 'Aceptación de la Política';
+$_['tab_personaldata']			= 'Terceros';
+$_['tab_datasecurity']			= 'Seguridad de Datos';
+
+$_['text_loading_data']		 	= 'Cargando datos, por favor espere...';
+$_['text_no_results']			= 'No hay resultados en la vista actual.';
+
+$_['column_request_id']			= 'ID de Solicitud';
+$_['column_type']				= 'Tipo';
+$_['column_email']				= 'Correo Electrónico';
+$_['column_accept_language']	= 'Idioma de Aceptación';
+$_['column_user_agent']			= 'Agente del Usuario';
+$_['column_server_ip']			= 'IP del Servidor';
+$_['column_client_ip']			= 'IP del Cliente';
+$_['column_date']				= 'Fecha';
+$_['column_acceptance_id']		= 'ID de Aceptación';
+$_['column_name']				= 'Nombre';
+$_['column_content']			= 'Contenido de Texto';
+$_['column_actions']			= 'Acciones';
+
+$_['text_date_start']			= 'Fecha de Inicio';
+$_['text_date_end']				= 'Fecha de Término';
+$_['text_filter']				= 'Filtro';
+$_['text_download']				= 'Descargar';
+$_['text_view_current']			= 'Ver Política Actual';
+
+$_['text_life_of_links']		= 'Expiración de Solicitudes RGPD';
+$_['text_access_to_personal_data_link'] = 'Acceso a los Datos Personales';
+$_['text_access_to_personal_data_link_helper'] = 'Tiempo de Expiración (en horas) de cada solicitud para obtener datos personales. Si el cliente no confirma dentro de este tiempo, necesitará realizar una nueva solicitud.';
+$_['text_access_default_hours'] = 'Valor preestablecido: 5 horas';
+
+$_['text_remove_personal_data_link'] = 'Eliminación de Datos Personales';
+$_['text_remove_personal_data_link_helper'] = 'Tiempo de Expiración (en horas) para eliminar/anonimizar los datos personales de un cliente. Si el cliente no confirma dentro de este tiempo, necesitará realizar una nueva solicitud.';
+$_['text_delete_default_hours'] = 'Valor preestablecido: 2 horas';
+
+$_['text_access_to_personal_data'] = 'Acceso a los Datos Personales';
+$_['text_access_to_personal_data_helper'] = 'Necesitas especificar a qué servicios envías datos privados de clientes (ej. nombre, correo electrónico, id de cliente, IP, etc) en los campos a continuación.';
+$_['text_third_party_services'] = 'Servicios de Terceros';
+$_['text_third_party_services_helper'] = 'Especifica aquí los servicios que usas para rastrear, analizar o mejorar la experiencia de tus usuarios. Ejemplo: Anuncios de Facebook, Facebook Pixel, Google Analytics, Google AdWords, Google Retargeting, Mailchimp, etc.';
+$_['text_other_services'] = 'Otros Servicios';
+$_['text_other_services_helper'] = 'Especifica aquí los servicios que no son públicos o son usados estrictamente para uso interno. Ejemplo: Integración ERP, PIWIK, Matomo, etc.';
+
+$_['text_general'] = 'Configuración General';
+$_['text_show_gdpr_menu_left'] = 'Mostrar un Enlace a la Extensión en el Menú Principal de Administración';
+$_['text_show_gdpr_menu_left_helper'] = 'Habilita esto si quieres ver un enlace al módulo RGPD en el menú principal de administración.';
+$_['text_enabled'] = 'Habilitado';
+$_['text_disabled'] = 'Deshabilitado';
+$_['text_show_gdpr_menu_left_default'] = 'Valor preestablecido: Deshabilitado';
+
+$_['tab_cookieconsent'] = 'Consentimiento de Cookies';
+$_['text_cc_enable'] = 'Mostrar la Barra de Consentimiento de Cookies';
+$_['text_cc_enable_helper'] = 'Escoge si deseas habilitar o deshabilitar la barra de consentimiento de cookies.';
+$_['text_cc_positioning'] = 'Posicionamiento';
+$_['text_cc_positioning_helper'] = 'Escoge el posicionamiento de la barra de consentimiento.';
+$_['text_cc_banner_bottom'] = 'Banner en la parte inferior';
+$_['text_cc_banner_top'] = 'Banner en la parte superior';
+$_['text_cc_banner_top_pushdown'] = 'Banner en la parte superior (push down)';
+$_['text_cc_banner_float_left'] = 'Flotante a la Izquierda';
+$_['text_cc_banner_float_right'] = 'Flotante a la Derecha';
+$_['text_cc_link_to_pp'] = 'Mostrar Enlace a la Política de Privacidad en la Barra';
+$_['text_cc_link_to_pp_helper'] = 'Si está habilitado, mostrará un enlace a la política de privacidad en la barra.';
+$_['text_cc_texts'] = 'Textos Personalizados';
+$_['text_cc_texts_helper'] = 'Define los textos que deseas mostrar en la barra de consentimiento de cookies.';
+$_['text_cc_main_message'] = 'Mensaje Principal';
+$_['text_cc_main_message_helper'] = 'Ingresa el mensaje principal de la barra aquí. Ya se muestra un ejemplo.';
+$_['text_cc_pp_message'] = 'Texto de Enlace a la Política de Privacidad';
+$_['text_cc_pp_message_helper'] = 'Si decides mostrar un enlace a la política en la barra, especifica el texto del enlace aquí.';
+$_['text_cc_accept_text'] = 'Texto del Botón Aceptar';
+$_['text_cc_accept_text_helper'] = 'Define el texto del botón aceptar.';
+$_['text_cc_dismiss_text'] = 'Texto del Botón Descartar';
+$_['text_cc_dismiss_text_helper'] = 'Define el texto del botón descartar.';
+$_['text_cc_track_pp_clicks'] = 'Realiza seguimiento de clics como aceptaciones de Política de Privacidad';
+$_['text_cc_track_pp_clicks_helper'] = 'Si está habilitado, los clics en el botón “Estoy de acuerdo” serán considerados como aceptaciones de la política de privacidad y serán registrados en el módulo.';
+$_['text_cc_bar_bg'] = 'Fondo del Banner';
+$_['text_cc_bar_text'] = 'Texto del Banner';
+$_['text_cc_button_bg'] = 'Fondo del Botón';
+$_['text_cc_button_text'] = 'Texto del Botón';
+$_['text_cc_palette'] = 'Paleta de Colores';
+$_['text_cc_palette_styling'] = 'Escoge el estilo de tu barra de consentimiento de cookies.';
+
+// Dashboard
+$_['text_steps_gdpr_compliant'] = 'Lista de Control RGPD';
+$_['text_install_the_extension'] = 'Instalar la extensión';
+$_['text_install_the_extension_helper'] = 'Asegúrate de que el módulo esté instalado &amp; todos los archivos estén cargados en tu tienda.';
+$_['text_enable_the_extension'] = 'Habilita la extensión';
+$_['text_enable_the_extension_helper'] = 'Ve a la pestaña del Panel de Control y habilita este módulo.';
+$_['text_track_policy_acceptances'] = 'Comienza a rastrear las aceptaciones de la Política de Privacidad';
+$_['text_track_policy_acceptances_helper'] = 'Esto es parte de la ley RGPD, que requiere que sepas exactamente cuándo y qué póliza aceptaron tus clientes.';
+$_['text_respont_gdpr_requests'] = 'Empieza a Tomar/Responder las solicitudes RGPD';
+$_['text_respont_gdpr_requests_helper'] = 'Esto es manejado de forma automática por la extensión. De acuerdo a la ley RGPD, debes responder a todas las solicitudes RGPD antes de las 72 horas. Gracias a nuestra extensión, serán tomadas en cuenta de forma instantánea.';
+$_['text_update_pp'] = 'Actualiza tu Política de Privacidad';
+$_['text_update_pp_helper'] = 'Esto es algo que debes hacer de forma manual. Asegúrate de actualizar tu política de privacidad y añadir que tu tienda ahora está en cumplimiento con la ley RGPD.<br /><br />El RGPD establece que la información que proveas a las personas sobre cómo procesas su información personal debe ser: <ul><li>concisa, transparente, inteligible y de fácil acceso;</li><li>escrita en lenguaje claro y simple, particularmente si está dirigido a un niño;</li><li>libre de cargos.</li></ul>';
+$_['text_enable_cookie_consent_bar'] = 'Habilitar la Barra de Consentimiento de Cookies (opcional)';
+$_['text_enable_cookie_consent_bar_helper'] = 'Si no tienes una barra de consentimiento de cookies, puedes probar la tuya. Es altamente personalizable y cuando alguien hace clic en el botón “Estoy de acuerdo”, agregamos un registro a nuestra base de datos, que este cliente en particular aceptó tu política de privacidad.<br /><br />Puedes habilitar nuestra barra de consentimiento desde la pestaña “Consentimiento de Cookies” en la configuración del módulo.';
+
+$_['text_consent_bar_copyright'] = '* La Barra de Consentimiento de Cookies fue creada usando el código fuente de cookieconsent.insites.com, originalmente desarrollado por SilkTide Ltd.';
+$_['text_show_gdpr_footer'] = 'Mostrar un Enlace a la Extensión en el Pie de Página';
+$_['text_show_gdpr_footer_helper'] = 'Habilita esto si quieres ver un enlace al módulo RGPD en el pie de página de tu tienda. Ten presente que si tu tema está altamente modificado, el enlace podría no mostrarse, y tendrías que añadirlo manualmente.';
+
+$_['text_data_security_breach'] = 'Vulneración de Seguridad de Datos';
+$_['text_notify_customers'] = 'Notificación de Vulneración de Seguridad de Datos';
+$_['text_data_security_breach_helper'] = 'Utiliza esta opción para notificar a tus clientes cuando haya una vulneración de seguridad de datos.';
+$_['text_data_security_breach_helper_additional'] = 'Debes informar a tus clientes cuándo exactamente ocurrió la vulneración, cuál es el número de cuentas afectadas y cuáles son las acciones que han sido tomadas por ti después de su descubrimiento.';
+
+$_['text_gdpr_checklist_helper'] = 'Por favor, usa la siguiente lista de control personal de RGPD para marcar cada acción que hayas completado. Haz clic para marcar cada punto.';
+$_['text_gdpr_check_1'] = 'Tener habilitada la extensión Cumplimiento RGPD';
+$_['text_gdpr_check_1_helper'] = 'Este es el primer sencillo paso para el cumplimiento de RGPD.';
+$_['text_gdpr_check_2'] = 'Haz que un nuevo enlace RGPD aparezca en tu pie de página';
+$_['text_gdpr_check_2_helper'] = 'Tenemos esta funcionalidad. Puedes habilitarla desde la pestaña de <strong>Configuración</strong>.';
+$_['text_gdpr_check_3'] = 'Probar la extensión de Cumplimiento RGPD en la interfaz de la tienda';
+$_['text_gdpr_check_3_helper'] = 'Hemos añadido una nueva sección a la interfaz de tu tienda. Una vez que la extensión sea habilitada, tus clientes y visitantes tendrán acceso a un nuevo conjunto de funcionalidades relacionadas a RGPD. Por favor prueba cómo la interfaz de la extensión RGPD es visualizada en tu interfaz de tienda - [link] - y si se ve bien en tu tema. También puedes probar las funcionalidades de usuario.';
+$_['text_gdpr_check_4'] = 'Actualiza tu Política de Privacidad con la Información RGPD';
+$_['text_gdpr_check_4_helper'] = 'Realizaremos seguimiento de las aceptaciones de la Política de Privacidad. Solo necesitas asegurarte que la Política de Privacidad de tu tienda web tenga una sección RGPD. Puedes encontrar tu página de <strong>Política de Privacidad</strong> aquí - [link]. Si no tienes tal página, por favor crea una desde aquí - [link2]. Puedes encontrar buenos ejemplos de lo que necesitas incluir en tu Política de Privacidad [linkstart]en esta entrada de blog[linkend].';
+$_['text_gdpr_check_5'] = 'Actualiza tu página de Términos y Condiciones con Información de RGPD';
+$_['text_gdpr_check_5_helper'] = 'Realizaremos seguimiento de las aceptaciones de tu página Términos y Condiciones. Solo necesitas asegurarte de que la página Términos y Condiciones de tu tienda web tenga una sección RGPD. Puedes encontrar tu página de <strong>Términos y Condiciones</strong> aquí - [link]. Si no tienes tal página, por favor crea una desde aquí - [link2]. Puedes encontrar buenos ejemplos de lo que necesitas incluir en tus términos &amp condiciones; [linkstart]en esta entrada de blog[linkend].';
+$_['text_gdpr_check_6'] = 'Permite a tus Clientes tener Acceso a Datos Personales';
+$_['text_gdpr_check_6_helper'] = 'Tenemos esta funcionalidad. Tus clientes pueden solicitar y descargar datos personales. Puedes probarla aquí con un perfil de cliente - [link]';
+$_['text_gdpr_check_7'] = 'Permite a tus Clientes ser Olvidados';
+$_['text_gdpr_check_7_helper'] = 'Tenemos esta funcionalidad. Puedes probar aquí en la interfaz de tu tienda - [link]';
+$_['text_gdpr_check_8'] = 'Permite a tus Clientes Editar sus Datos';
+$_['text_gdpr_check_8_helper'] = 'Tenemos esta funcionalidad. Puedes probar aquí en la interfaz de tu tienda - [link]';
+$_['text_gdpr_check_9'] = 'Empieza a usar el Consentimiento de Cookies si aún no lo usas (Opcional)';
+$_['text_gdpr_check_9_helper'] = 'Tenemos esta funcionalidad. Puedes habilitarla desde la pestaña <strong>Consentimiento de Cookies</strong> y configurarla de acuerdo a tu estilo deseado.';
+$_['text_gdpr_check_10'] = 'Observa y Administra las solicitudes y aceptaciones RGPD';
+$_['text_gdpr_check_10_helper'] = 'Tenemos esta funcionalidad. Puedes hacer esto en las pestañas <strong>Solicitudes</strong> y <strong>Aceptación de Política</strong>.';
+$_['text_link'] = '[LINK]';
+$_['text_link_test'] = 'HAZ CLIC PARA PROBAR';
+$_['text_link_view'] = 'HAZ CLIC PARA VER';
+$_['text_link_add'] = 'HAZ CLIC PARA AÑADIR';
+
+// GDPR 1.2/2.2/3.2
+$_['text_cc_cookies_change'] = 'Cambia el Texto del Botón de Cookies';
+$_['text_cc_cookies_change_helper'] = 'Define el texto en el botón que le dará la opción a los clientes de habilitar/deshabilitar las cookies.';
+$_['text_cookie_management'] = 'Administración de Cookies';
+$_['text_cc_customize'] = 'Personalizar';
+$_['text_ccc_functional'] = 'Cookies Estrictamente Requeridas';
+$_['text_ccc_functional_helper'] = 'Estas cookies son requeridas por el sitio web para su funcionamiento y no pueden ser deshabilitadas. Dichas cookies solo se configuran en respuesta a acciones realizadas por ti como idioma, moneda, sesión de inicio, preferencias de privacidad. Puedes configurar tu navegador para bloquear estas cookies pero nuestro sitio podría dejar de funcionar.';
+$_['text_ccc_analytics'] = 'Analíticas y Estadísticas';
+$_['text_ccc_analytics_helper'] = 'Estas cookies nos permiten medir el tráfico de visitantes y ver las fuentes de tráfico al recolectar información en conjunto de datos. También nos ayudan a entender qué productos y acciones son más populares que otros.';
+$_['text_ccc_marketing'] = 'Marketing and Retargeting';
+$_['text_ccc_marketing_helper'] = 'Estas cookies son usualmente configuradas por nuestros socios de marketing y publicidad. Pueden ser usadas por ellos para construir un perfil de tus intereses y luego, mostrarte anuncios relevantes. Si no permites el uso de estas cookies, no experimentarás anuncios dirigidos a tus intereses.';
+$_['text_cc_close_action'] = 'Acción cuando haces clic en el botón "Cerrar"';
+$_['text_cc_close_action_helper'] = 'Escoge qué acción tomar cuando los clientes hacen clic en el botón "Cerrar” en la barra de consentimiento de cookies.';
+$_['text_cc_stop_nothing'] = 'No hacer nada';
+$_['text_cc_stop_marketing'] = 'Detener cookies de marketing';
+$_['text_cc_stop_analytics'] = 'Detener cookies de analíticas';
+$_['text_cc_stop_both'] = 'Detener cookies de marketing y analíticas';
