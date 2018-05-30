558b6b59e919e1178c7b3b8b6d566c80aeec418c
Aleksandar Banderov <banderov@isenselabs.com>
2018-05-30 17:46:53 +0300

    Add Finnish

---
+++ finnish/src_3.0/upload/catalog/language/fi-fi/extension/module/isenselabs_gdpr.php
@@ -0,0 +1,94 @@
+<?php
+// Community translation
+// If you spot any errors or have better translation, please contact niklas@kuldlelu.com for corrections. Thank you!
+$_['text_account'] = 'Asiakastili';
+$_['text_gdpr'] = 'GDPR Työkalut';
+
+$_['button_back'] = 'Takaisin';
+$_['button_continue'] = 'Jatka';
+$_['entry_email'] = 'Sähköpostiosoite';
+$_['text_your_email'] = 'Sähköpostiosoitteesi';
+
+$_['text_data_rectification'] = 'Tietojen korjaus/oikaiseminen';
+$_['text_data_rectification_helper'] = 'Voit käyttää allaolevia linkkejä päivittääksesi ja korjataksesi tilisi tietoja, mikäli ne eivät ole oikein.';
+$_['text_edit'] = 'Muokkaa tilitietoja';
+$_['text_password'] = 'Vaihda salasana';
+$_['text_address'] = 'Muokkaa osoitekirjan tietoja';
+$_['text_newsletter'] = 'Vahvista / Peru uutiskirjeen tilaus';
+
+$_['text_data_portability'] = 'Tietojen siirrettävyys';
+$_['text_data_portability_helper'] = 'Voit käyttää allaolevia linkkejä ladataksesi kaikki tietosi .CSV muodossa.';
+$_['text_personal_information'] = 'Henkilötiedot';
+$_['text_personal_addresses'] = 'Tallennetut osoitteet';
+$_['text_orders'] = 'Tilaukset';
+$_['text_gdpr_requests'] = 'GDPR tietopyynnöt';
+
+$_['text_access_to_personal_data'] = 'Pääsy henkilötietoihin';
+$_['text_access_to_personal_data_helper'] = 'Voit käyttää allaolevaa linkkiä tilataksesi raportin joka sisältää kaikki henkilötiedot mitkä olemme tallentaneet järjestelmäämme.';
+$_['text_personal_data_helper'] = 'Käytä allaolevaa lomaketta tilataksesi raportin joka sisältää kaikki henkiötiedot mitkä olemme tallentaneet järjestelmäämme.';
+$_['text_request_a_report'] = 'Tilaa raportti';
+$_['text_enter_your_email'] = 'Anna sähköpostiosoitteesi';
+$_['text_enter_your_email_data_request_helper'] = 'Saat sähköpostiisi linkin, mitä klikkaamalla näytämme kaiken henkilökohtaisen tiedon mitä olemme tallentaneet järjestelmäämme. Linkki on käytettävissä seuraavat %s% tuntia.';
+$_['text_how_this_works_data_request'] = 'Kuinka tämä toimii?';
+$_['text_successful_request'] = 'Onnistui!';
+$_['text_successful_data_request_helper'] = 'Kiitoksia pyynnöstäsi. Jos antamasi sähköpostiosoite täsmää järjestelmäämme tallennetun kanssa, saat sähköpostin jossa on linkki tietoihisi. Käyttämällä annettua linkkiä saat nähtäväksesi kaiken henkilökohtaisen tiedon mitä olemme tallentaneet järjestelmäämme. Otathan huomioon, että linkki on toiminnassa vain seuraavat %s% tuntia.';
+
+$_['text_personal_data_subject'] = 'Tarkista tämä saadaksesi pääsyn henkilötietoihisi.';
+$_['text_greeting'] = 'Hei! Teit pyynnön saadaksesi nähtäväksesi henkilötietosi jotka on tallennettu järjestelmäämme. Klikkaa allaolevaa linkkiä nähdäksesi tiedot:';
+$_['text_button_view_data'] = 'Katso tallentamamme henkilötiedot';
+$_['text_actions'] = 'Tapahtumat';
+$_['text_active_link_helper'] = '* Muista, että tämä linkki on toiminnassa vain seuraavat %s% tuntia.';
+
+$_['text_view_personal_data'] = 'Henkilötietojen tarkastelu';
+$_['text_firstname'] = 'Etunimi';
+$_['text_lastname'] = 'Sukunimi';
+$_['text_telephone'] = 'Puhelinnumero';
+$_['text_email'] = 'Sähköpostiosoite';
+$_['text_fax'] = 'Fax';
+$_['text_newsletter_subscription'] = 'Uutiskirjeen tilaus';
+$_['text_ip'] = 'IP-osoite';
+$_['text_customer_information'] = 'Asiakastiedot';
+$_['text_saved_addresses'] = 'Tallennetut osoitteet';
+$_['text_company'] = 'Yritys';
+$_['text_address_1'] = 'Osoiterivi 1';
+$_['text_address_2'] = 'Osoiterivi 2';
+$_['text_country'] = 'Maa';
+$_['text_city'] = 'Kaupunki';
+$_['text_postcode'] = 'Postinumero';
+$_['text_zone'] = 'Zone';
+$_['text_address'] = 'Osoite';
+$_['text_data_third_party'] = 'Kolmansille osapuolille lähetyt tiedot';
+$_['text_data_third_party_helper'] = 'Saatamme joskus lähettää osia tiedoistasi allamainituille kolmansille osapuolille mahdollistaaksemme parhaan mahdollisen käyttökokemuksen kaupassamme:';
+$_['text_third_party_services'] = 'Palvelut';
+$_['text_other'] = 'Muut';
+
+$_['text_hash_error'] = 'Ei näytettäviä tietoja. Linkki on epäkelpo tai se on erääntynyt. Jos haluat nähdä pyytämäsi tiedot, sinun täytyy tehdä uusi GDPR tietopyyntö.';
+$_['text_no_data_third_party'] = 'Emme lähetä tietojasi kolmansille osapuolille.';
+$_['text_no_data'] = 'Asiakkaasta ei löytynyt tallennettuja tietoja!';
+
+$_['text_right_to_be_forgotten'] = 'Oikeus tulla unohdetuksi';
+$_['text_right_to_be_forgotten_helper'] = 'Käytä tätä toimintoa mikäli haluat poistaa sekä henkilötietosi että muut tietosi kaupastamme. Haluamme kuitenkin huomauttaa, että <strong>tämä toiminto POISTAA asiakastilisi, joten et voi enää kirjautua tai saada pääsyä tietoihisi tämän jälkeen</strong>.';
+$_['text_request_personal_data_deletion'] = 'Henkilötietojen poistopyyntö';
+$_['text_enter_your_email_data_deletion_helper'] = 'Lähetämme sähköpostiisi linkin, mitä klikkaamalla viimeistelet kaiken järjestelmäämme tallennetun henkilötietosi poistamisen. Kaikkia tietoja ei voida välttämättä poistaa, mutta silloin tiedot anonymisoidaan. Linkki toimii seuraavat %s% tuntia.';
+$_['text_successful_data_deletion_helper'] = 'Kiitoksia pyynnöstäsi. Jos antamasi sähköpostiosoite täsmää järjestelmäämme tallennetun kanssa, saat sähköpostin jossa on linkki asiakastilisi poistamista varten. Linkkiä käyttämällä poistamme kaikki henkilötietosi järjestelmästämme. Otathan huomioon että tämä linkki on toiminnassa vain seuraavat %s% tuntia.';
+
+$_['text_delete_data_subject'] = 'Oikeus tulla unohdetuksi - Tietojen poistopyyntö';
+$_['text_button_delete_data'] = 'Poista henkilötietoni';
+$_['text_greeting_delete'] = 'Hei! Olet pyytänyt henkilötietojesi poistamista kaupastamme. Otathan huomioon että tämä toiminto poistaa asiakastilisi, joten et voi enää päästä käsiksi tai käyttää tietojasi.<br /><br />Jos et tehnyt tietojen poistopyyntöä itse, voit jättää tämän viestin huomioimatta. Muussa tapauksessa, klikkaa allaolevaa linkkiä jatkaaksesi tietojesi poistamista:';
+
+$_['text_personal_data_deletion'] = 'Henkilötietojen poistaminen';
+$_['text_hash_error_delete'] = 'Toimintoa ei voitu suorittaa. Linkki on epäkelpo tai se on erääntynyt. Jos haluat suorittaa tämän toiminnon loppuun, sinun täytyy tehdä uusi tietojen poistopyyntö.';
+$_['text_confirm_the_action'] = 'Asiakkaan vahvistus vaaditaan';
+$_['text_confirm_the_action_helper'] = '<strong>HUOMIO! Tätä toimintoa ei voi peruuttaa!</strong> Mikäli vahvistat tietojesi poistamisen, asiakastilisi poistetaan verkkokaupastamme pysyvästi, etkä voi enää kirjautua sisään tai saada pääsyä tietoihisi kuten tilaushistoria, osoitteet, yms. Ole hyvä ja tee päätöksesi:';
+$_['text_button_confirm_deletion'] = 'Kyllä, Haluan poistaa kaikki henkilötietoni';
+$_['text_button_cancel_deletion'] = 'Ei, Haluan jättää henkilötietoni ennalleen.';
+
+// GDPR Compliance 1.1/2.1./3.1
+$_['text_order'] = 'Tilaus';
+$_['text_personal_data'] = 'Rekisteröitymättömän asiakkaan henkilötiedot';
+$_['text_personal_data_helper'] = 'Mikäli asiakas ei ole rekisteröinyt asiakastiliä kauppaamme, voi meillä silti olla joitain tietoja kyseiseen sähköpostiosoitteeseen liittyen. Näet täältä tiedot, jotka saamme haettua tilausjärjestelmästämme.';
+$_['text_payment_view_data'] = 'Maksutiedot';
+$_['text_shipping_view_data'] = 'Toimitustiedot';
+$_['text_order_view_data'] = 'Tilauksen tiedot';
+$_['text_user_agent'] = 'Selaintiedot';
+$_['text_accept_language'] = 'Käytetty kieli';
