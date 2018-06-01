bae4cf7715d08a1e522437e1e5a0394aa84f99f1
Aleksandar Banderov <banderov@isenselabs.com>
2018-06-01 11:39:09 +0300

    Add admin Polish translation

---
+++ polish/src_2.3/upload/admin/language/pl-pl/extension/module/isenselabs_gdpr.php
@@ -0,0 +1,192 @@
+<?php
+// Heading
+$_['heading_title']    			= 'Zgodność z RODO';
+
+// Text
+$_['text_extension']   			= 'Rozszerzenia';
+$_['text_success']     			= 'Sukces: Ustawienia zostały zmodyfikowane!';
+$_['text_edit']        			= 'Edytuj moduł';
+$_['text_module']      			= 'Moduły';
+$_['text_complete']    			= 'Kompletny';
+$_['text_error']       			= 'Błąd';
+
+// Entry
+$_['entry_status']     			= 'Status';
+$_['text_gdpr_status']			= 'Status rozszerzenia';
+$_['text_gdpr_status_helper']   = 'Włącz lub wyłącz rozszerzenie';
+
+// Errors
+$_['error_permission'] 			= 'Ostrzeżenie: nie masz uprawnień do modyfikowania modułu!';
+$_['error_unexpected'] 			= 'Wystąpił nieoczekiwany błąd. Spróbuj ponownie później lub skontaktuj się z programistami modułu z następującym identyfikatorem błędu: ';
+
+// Licensing
+$_['text_your_license']         = 'Twoja licencja';
+$_['text_please_enter_the_code']= 'Wprowadź kod licencji zakupu produktu:';
+$_['text_activate_license']     = 'Aktywuj licencję';
+$_['text_not_having_a_license'] = 'Nie masz kodu? Uzyskaj go stąd.';
+$_['text_license_holder']       = 'Posiadacz licencji';
+$_['text_registered_domains']   = 'Zarejestrowane domeny';
+$_['text_expires_on']           = 'Licencja wygasa:';
+$_['text_valid_license']        = 'WAŻNA LICENCJA';
+$_['text_manage']               = 'zarządzaj';
+$_['text_get_support']          = 'Wsparcie';
+$_['text_community']            = 'Społeczność';
+$_['text_ask_our_community']    = 'Tworzymy dużą społeczność. Możesz przedstawić swój problem na forum.';
+$_['text_browse_forums']        = 'Przeglądaj fora';
+$_['text_tickets']              = 'Tickety';
+$_['text_open_a_ticket']        = 'Chcesz komunikować się indywidualnie z naszymi technikami? Utwórz zgłoszenie do pomocy technicznej.';
+$_['text_open_ticket_for_real'] = 'Otwórz ticket';
+$_['text_pre_sale']             = 'Przedsprzedaż';
+$_['text_pre_sale_text']        = 'Masz świetny pomysł na swój sklep internetowy? Nasz zespół programistów może uczynić go rzeczywistym.';
+$_['text_bump_the_sales']       = 'Podnieś swoją sprzedaż';
+
+$_['tab_home']       			= 'Lista kontrolna';
+$_['tab_controlpanel'] 			= 'Ustawienia';
+$_['tab_support']       		= 'Wsparcie';
+$_['tab_requests']				= 'Zapytania';
+$_['tab_acceptances']			= 'Udzielone zgody';
+$_['tab_personaldata']			= 'Dostawcy zewnętrzni';
+$_['tab_datasecurity']			= 'Bezpieczeństwo danych';
+
+$_['text_loading_data']		 	= 'Wczytywanie, proszę czekać...';
+$_['text_no_results']			= 'Brak wyników w bieżącym widoku.';
+
+$_['column_request_id']			= 'ID Zapytania';
+$_['column_type']				= 'Typ';
+$_['column_email']				= 'Email';
+$_['column_accept_language']	= 'Język';
+$_['column_user_agent']			= 'User Agent';
+$_['column_server_ip']			= 'IP serwera';
+$_['column_client_ip']			= 'IP klienta';
+$_['column_date']				= 'Data';
+$_['column_acceptance_id']		= 'ID zgody';
+$_['column_name']				= 'Nazwa';
+$_['column_content']			= 'Zawartość';
+$_['column_actions']			= 'Akcje';
+
+$_['text_date_start']			= 'Data startu';
+$_['text_date_end']				= 'Data końca';
+$_['text_filter']				= 'Filtr';
+$_['text_download']				= 'Pobierz';
+$_['text_view_current']			= 'Wyświetl bieżącą politykę';
+
+$_['text_life_of_links']		= 'Wygaśnięcie wniosków RODO';
+$_['text_access_to_personal_data_link'] = 'Dostęp do danych personalnych';
+$_['text_access_to_personal_data_link_helper'] = 'Czas wygaśnięcia (w godzinach) każdego wniosku o uzyskanie danych osobowych. Jeśli klient nie potwierdzi w tym czasie, będzie musiał złożyć kolejną prośbę.';
+$_['text_access_default_hours'] = 'Domyślnie: 5 godzin';
+
+$_['text_remove_personal_data_link'] = 'Usuwanie danych osobowych';
+$_['text_remove_personal_data_link_helper'] = 'Czas wygaśnięcia (w godzinach) w celu usunięcia / anonimizacji danych osobowych klienta. Jeśli klient nie potwierdzi w tym czasie, będzie musiał złożyć kolejną prośbę.';
+$_['text_delete_default_hours'] = 'Domyślnie: 2 godziny';
+
+$_['text_access_to_personal_data'] = 'Dostęp do danych osobowych';
+$_['text_access_to_personal_data_helper'] = 'Musisz określić, do których usług wysyłasz prywatne dane klientów (np. Imię i nazwisko, adres e-mail, identyfikator klienta, adres IP itp.) W poniższych polach.';
+$_['text_third_party_services'] = 'Usługi innych firm';
+$_['text_third_party_services_helper'] = 'Określ tutaj usługi, których używasz do śledzenia, analizowania lub ulepszania doświadczeń swoich klientów. Przykład: reklamy na Facebooku, Facebook Pixel, Google Analytics, Google AdWords, ponowne kierowanie Google, MailChimp itp.';
+$_['text_other_services'] = 'Inne usługi';
+$_['text_other_services_helper'] = 'Określ tutaj usługi, które nie są publiczne, lub coś, co jest używane wyłącznie do użytku wewnętrznego. Przykład: integracja ERP, PIWIK, Matomo itp';
+
+$_['text_general'] = 'Ustawienia główne';
+$_['text_show_gdpr_menu_left'] = 'Pokaż link do rozszerzenia w głównym menu administracyjnym';
+$_['text_show_gdpr_menu_left_helper'] = 'Włącz tę opcję, jeśli chcesz zobaczyć link do modułu RODO w głównym menu administratora.';
+$_['text_enabled'] = 'Włączone';
+$_['text_disabled'] = 'Wyłączone';
+$_['text_show_gdpr_menu_left_default'] = 'Wartość domyślna: wyłączona';
+
+$_['tab_cookieconsent'] = 'Zgoda na pliki cookie';
+$_['text_cc_enable'] = 'Pokaż pasek zgody na pliki cookie';
+$_['text_cc_enable_helper'] = 'Wybierz, czy chcesz włączyć lub wyłączyć pasek zgody na pliki cookie.';
+$_['text_cc_positioning'] = 'Pozycje';
+$_['text_cc_positioning_helper'] = 'Wybierz pozycję paska zgody.';
+$_['text_cc_banner_bottom'] = 'Banner dół';
+$_['text_cc_banner_top'] = 'Banner góra';
+$_['text_cc_banner_top_pushdown'] = 'Banner góra (wysuwany)';
+$_['text_cc_banner_float_left'] = 'Lewo';
+$_['text_cc_banner_float_right'] = 'Prawo';
+$_['text_cc_link_to_pp'] = 'Pokaż Link do Polityki prywatności w pasku';
+$_['text_cc_link_to_pp_helper'] = 'Jeśli jest włączona, wyświetli link do polityki prywatności na pasku.';
+$_['text_cc_texts'] = 'Własne teksty';
+$_['text_cc_texts_helper'] = 'Zdefiniuj teksty, które chcesz wyświetlić w pasku zgody na pliki cookie.';
+$_['text_cc_main_message'] = 'Główna wiadomość';
+$_['text_cc_main_message_helper'] = 'Tutaj wpisz główną wiadomość na pasku. Przykład jest już pokazany.';
+$_['text_cc_pp_message'] = 'Tekst linku Polityka prywatności';
+$_['text_cc_pp_message_helper'] = 'Jeśli zdecydujesz się wyświetlić link do polityki na pasku, podaj tutaj tekst linku.';
+$_['text_cc_accept_text'] = 'Tekst przycisku akceptacji';
+$_['text_cc_accept_text_helper'] = 'Zdefiniuj tekst na przycisku akceptowania.';
+$_['text_cc_dismiss_text'] = 'Tekst przycisku odrzucenia';
+$_['text_cc_dismiss_text_helper'] = 'Zdefiniuj tekst na przycisku odrzucania.';
+$_['text_cc_track_pp_clicks'] = 'Śledź kliknięcia jako akceptacje Polityki prywatności';
+$_['text_cc_track_pp_clicks_helper'] = 'Jeśli ta opcja jest włączona, kliknięcia przycisku "Zgadzam się" będą traktowane jako akceptacja zasad prywatności i będą one logowane w module.';
+$_['text_cc_bar_bg'] = 'Tło banera';
+$_['text_cc_bar_text'] = 'Treść banera';
+$_['text_cc_button_bg'] = 'Tło przycisku';
+$_['text_cc_button_text'] = 'Tekst przycisku';
+$_['text_cc_palette'] = 'Paleta kolorów';
+$_['text_cc_palette_styling'] = 'Wybierz stylizację paska zgody na pliki cookie.';
+
+// Dashboard
+$_['text_steps_gdpr_compliant'] = 'Lista kontrolna RODO';
+$_['text_install_the_extension'] = 'Zainstaluj rozszerzenie';
+$_['text_install_the_extension_helper'] = 'Upewnij się, że moduł jest zainstalowany &amp; wszystkie pliki są przesłane do Twojego sklepu.';
+$_['text_enable_the_extension'] = 'Włącz rozszerzenie';
+$_['text_enable_the_extension_helper'] = 'Przejdź do karty Panel sterowania i włącz moduł.';
+$_['text_track_policy_acceptances'] = 'Rozpocznij śledzenie akceptacji zasad';
+$_['text_track_policy_acceptances_helper'] = 'Jest to część ustawy o RWP, która wymaga, abyś dokładnie wiedział, kiedy i na jakie zasady zgodzili się Twoi klienci.';
+$_['text_respont_gdpr_requests'] = 'Rozpocznij przyjmowanie / odpowiadanie na żądania RODO';
+$_['text_respont_gdpr_requests_helper'] = 'Zgodnie z ustawą o RODO musisz odpowiedzieć na wszystkie żądania w ciągu 72 godzin.';
+$_['text_update_pp_helper'] = 'To jest coś, co musisz zrobić ręcznie. Pamiętaj, aby zaktualizować swoją politykę prywatności i dodać, że twój sklep jest teraz zgodny z prawem RODO. <br/> <br /> RODO mówi, że informacje, które przekazujesz ludziom na temat tego, jak przetwarzasz ich dane osobowe, muszą być następujące: <ul><li> zwięzłe, przejrzyste, zrozumiałe i łatwo dostępne; </li><li> napisane jasnym i prostym językiem, </li><li>bezpłatnie.</li> </ul>';
+$_['text_enable_cookie_consent_bar'] = 'Włącz pasek zgody na pliki cookie (opcjonalnie)';
+$_['text_enable_cookie_consent_bar_helper'] = 'Jeśli nie masz paska zgody na pliki cookie, możesz wypróbować własny. Jest wysoce konfigurowalny i kiedy ktoś kliknie przycisk "Zgadzam się", dodajemy w naszej bazie danych zapis, że ten konkretny klient zgodził się na twoją politykę prywatności. <br /> <br /> Możesz włączyć nasz pasek zgody od Zakładka "Cookie Consent" w ustawieniach modułu.';
+
+$_['text_consent_bar_copyright'] = '* Pasek zgody na pliki cookie został utworzony przy użyciu kodu źródłowego pliku cookieconsent.insites.com, który został pierwotnie opracowany przez firmę SilkTide Ltd.';
+$_['text_show_gdpr_footer'] = 'Pokaż link do rozszerzenia w stopce';
+$_['text_show_gdpr_footer_helper'] = 'Włącz tę opcję, jeśli chcesz zobaczyć link do modułu RODO w stopce Twojego sklepu. Pamiętaj, że jeśli Twój motyw jest wysoce zmodyfikowany, link może się nie pojawić i musisz go dodać ręcznie.';
+
+$_['text_data_security_breach'] = 'Naruszenie bezpieczeństwa danych';
+$_['text_notify_customers'] = 'Powiadomienie o naruszeniu bezpieczeństwa danych';
+$_['text_data_security_breach_helper'] = 'Użyj tej opcji, aby powiadomić klienta, gdy nastąpiło naruszenie bezpieczeństwa danych.';
+$_['text_data_security_breach_helper_additional'] = 'Musisz powiadomić swoich klientów dokładnie, kiedy doszło do naruszenia, jaka jest liczba dotkniętych kont i jakie działania zostały podjęte po ich odkryciu.';
+
+$_['text_gdpr_checklist_helper'] = 'Użyj poniższej osobistej listy kontrolnej RODO, aby oznaczyć każde wykonane działanie. Kliknij, aby sprawdzić każdy punkt.';
+$_['text_gdpr_check_1'] = 'Czy rozszerzenie zgodności z RODO jest włączone';
+$_['text_gdpr_check_1_helper'] = 'Jest to pierwszy prosty krok do osiągnięcia zgodności z RODO.';
+$_['text_gdpr_check_2'] = 'Utwórz nowe łącze RODO widoczne w stopce';
+$_['text_gdpr_check_2_helper'] = 'WMamy tę funkcję. Możesz ją włączyć na karcie <strong> Ustawienia </strong>.';
+$_['text_gdpr_check_3'] = 'Przetestuj rozszerzenie zgodności z prawem RODO na stronie sklepu';
+$_['text_gdpr_check_3_helper'] = 'Dodajemy Ci nową sekcję na froncie sklepu. Po włączeniu rozszerzenia klienci i użytkownicy będą mieli dostęp do nowego zestawu funkcji związanych z RODO. Sprawdź, jak interfejs frontowy rozszerzenia RODO jest wyświetlany na twoim interfejsie - [link] - i czy wygląda dobrze na twoim motywie. Możesz również przetestować funkcje użytkownika.';
+$_['text_gdpr_check_4'] = 'Zaktualizuj swoją Politykę prywatności za pomocą informacji o RODO';
+$_['text_gdpr_check_4_helper'] = 'Będziemy śledzić akceptację Twojej polityki prywatności. Trzeba tylko upewnić się, że polityka prywatności sklepu internetowego zawiera sekcję RODO. Tutaj znajdziesz swoją <strong> Politykę prywatności </strong> - [link]. Jeśli nie masz takiej strony, utwórz ją stąd - [link2]. Możesz znaleźć dobre przykłady tego, co musisz uwzględnić w polityce prywatności [linkstart] znajdziesz w tym poście na blogu [linkend].';
+$_['text_gdpr_check_5'] = 'Zaktualizuj Warunki korzystania z swojej strony w oparciu o RODO.';
+$_['text_gdpr_check_5_helper'] = 'Będziemy śledzić akceptację strony Warunki. Musisz tylko upewnić się, że na stronie Warunki świadczenia usług znajduje się sekcja RODO. Tutaj znajdziesz swoją stronę <strong> Warunki i postanowienia </ strong> - [link]. Jeśli nie masz takiej strony, utwórz ją stąd - [link2]. Możesz znaleźć dobre przykłady tego, co musisz uwzględnić w swoich warunkach & amp; warunki [linkstart] w tym poście na blogu [linkend].';
+$_['text_gdpr_check_6'] = 'Pozwól swoim klientom mieć dostęp do danych osobowych';
+$_['text_gdpr_check_6_helper'] = 'Mamy tę funkcję. Twoi klienci mogą żądać i pobierać dane osobowe. Możesz przetestować ją tutaj, za pomocą profilu klienta - [link]';
+$_['text_gdpr_check_7'] = 'Pozwól swoim klientom zostać zapomnianymi';
+$_['text_gdpr_check_7_helper'] = 'Mamy tę funkcję. Możesz przetestować ją na froncie sklepu tutaj - [link]';
+$_['text_gdpr_check_8'] = 'Pozwól swoim klientom na edycję swoich danych';
+$_['text_gdpr_check_8_helper'] = 'Mamy tę funkcję. Możesz przetestować ją na froncie sklepu tutaj - [link]';
+$_['text_gdpr_check_9'] = 'Rozpocznij korzystanie z zgody na pliki cookie, jeśli nadal jej nie używasz (opcjonalnie)';
+$_['text_gdpr_check_9_helper'] = 'Mamy tę funkcję. Możesz ją włączyć w karcie <strong> Polityka Cookie </strong> i skonfigurować zgodnie z wybranym stylem.';
+$_['text_gdpr_check_10'] = 'Zarządzaj Żądaniami i Akceptacjami RODO';
+$_['text_gdpr_check_10_helper'] = 'Mamy tę funkcję. Możesz to zrobić na kartach <strong> Żądania </strong> i <strong> Akceptacja zasad </strong>.';
+$_['text_link'] = '[LINK]';
+$_['text_link_test'] = 'KLIKNIJ BY PRZETESTOWAĆ';
+$_['text_link_view'] = 'KLIKNIJ BY ZOBACZYĆ';
+$_['text_link_add'] = 'KLIKNIJ BY DODAĆ';
+
+// GDPR 1.2/2.2/3.2
+$_['text_cc_cookies_change'] = 'Zmień tekst przycisku Cookies';
+$_['text_cc_cookies_change_helper'] = 'Zdefiniuj tekst na przycisku, który zapewni klientom opcję włączania / wyłączania plików cookie.';
+$_['text_cookie_management'] = 'Zarządzanie plikami cookies';
+$_['text_cc_customize'] = 'Modyfikuj';
+$_['text_ccc_functional'] = 'Ściśle wymagane pliki cookie';
+$_['text_ccc_functional_helper'] = 'Pliki cookie są wymagane do uruchomienia witryny i nie można ich wyłączyć. Takie pliki cookie są ustawiane tylko w odpowiedzi na działania użytkownika, takie jak język, waluta, sesja logowania, preferencje prywatności. Możesz ustawić przeglądarkę tak, aby blokowała te pliki cookie, ale nasza strona może wtedy nie działać.';
+$_['text_ccc_analytics'] = 'Analityka i statystyki';
+$_['text_ccc_analytics_helper'] = 'Te pliki cookie pozwalają nam mierzyć ruch odwiedzających i wyświetlać źródła ruchu, zbierając informacje w zestawach danych. Pomagają nam również zrozumieć, które produkty i działania są bardziej popularne niż inne.';
+$_['text_ccc_marketing'] = 'Marketing i retargeting';
+$_['text_ccc_marketing_helper'] = 'Te pliki cookie są zwykle ustawiane przez naszych partnerów marketingowych i reklamowych. Mogą być przez nich wykorzystywane do budowania profilu zainteresowania, a następnie wyświetlać trafne reklamy. Jeśli nie zezwolisz na te pliki cookie, nie będziesz mieć do czynienia z reklamami ukierunkowanymi na Twoje zainteresowania.';
+$_['text_cc_close_action'] = 'Działanie po kliknięciu przycisku "Zamknij"';
+$_['text_cc_close_action_helper'] = 'Wybierz, jakie działania należy podjąć, gdy klienci klikną przycisk "Zamknij" na pasku zgody na pliki cookie.';
+$_['text_cc_stop_nothing'] = 'Nic nie rób';
+$_['text_cc_stop_marketing'] = 'Zatrzymaj marketingowe pliki cookie';
+$_['text_cc_stop_analytics'] = 'Zatrzymaj analytics cookies';
+$_['text_cc_stop_both'] = 'Zatrzymaj ciasteczka marketingowe i analityczne';
