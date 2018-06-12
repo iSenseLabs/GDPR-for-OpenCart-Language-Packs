88eb7915892869fe183f8558398bd3e7a21deeee
Aleksandar Banderov <banderov@isenselabs.com>
2018-06-12 15:36:08 +0300

    Update English

--- english/src_2.0.x-2.2/upload/catalog/language/english/module/isenselabs_gdpr.php
+++ english/src_2.0.x-2.2/upload/catalog/language/english/module/isenselabs_gdpr.php
@@ -68,7 +68,7 @@ $_['text_right_to_be_forgotten'] = 'Right to be Forgotten';
 $_['text_right_to_be_forgotten_helper'] = 'Use this option if you want to remove your personal and other data from our store. Keep in mind that <strong>this process will delete your account, so you will no longer be able to access or use it anymore</strong>.';
 $_['text_request_personal_data_deletion'] = 'Request personal data deletion';
 $_['text_enter_your_email_data_deletion_helper'] = 'You will receive an email with a link, which upon clicking will delete all data that we store for you. If something cannot be deleted, it will be anonymized. The link will be active for %s% hours.';
-$_['text_successful_data_deletion_helper'] = 'Thank you for your submission! If the email address is correct and it is present in the database, you will receive an email with a link. Using the link will delete all personal data that we store for you. Keep in mind that the link will be active for %s% hours.';
+$_['text_successful_data_deletion_helper'] = 'Thank you for your submission! If the email address is correct and it is present in the database, you will receive an email with a link. Using the link will create a deletion request for all personal data that we store for you. Keep in mind that the link will be active for %s% hours.';
 
 $_['text_delete_data_subject'] = 'Personal data deletion request';
 $_['text_button_delete_data'] = 'Delete My Personal Data';
@@ -108,3 +108,10 @@ $_['text_my_gdpr_tools_header'] = 'My GDPR Tools';
 // GDPR Compliance 1.4/2.4/3.4
 $_['text_optin_checkbox'] = 'I have read and agree to the <a href="%s" class="agree"><b>%s</b></a>';
 $_['text_optin_error'] = 'You must agree to the %s in order to continue.';
+
+// GDPR Compliance 1.5/2.5/3.5
+$_['successful_deletion_request'] = 'Successful Request!';
+$_['successful_deletion_request_helper'] = 'Your request was sent to the store owners. They will review it and will decide whether to honor it or deny it. Here are the possible outcomes:<br /><br />- If they decide to honor your request, you will be notified what actions will be taken in order to remove your data from the store.<br />- If your request is denied, you will receive an email explaining what were the reasons for this decision.';
+$_['text_right_to_be_forgotten_request'] = 'Right to be Forgotten Request';
+$_['text_greeting_rtb'] = 'The customer %s% wants to have his/hers data removed from your store. Open your store\'s admin panel to honor or deny the request.';
+$_['successful_deletion_request_helper_automatically'] = 'Your request was sent to the store owners and it was automatically approved. All your data is now removed from the store.<br /><br />We are sorry to see you go :(';
