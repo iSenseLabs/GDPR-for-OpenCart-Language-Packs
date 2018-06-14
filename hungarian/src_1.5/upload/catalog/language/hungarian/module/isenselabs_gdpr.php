1d55b358af9917fed37d06a489bf8a8fa25662a5
Aleksandar Banderov <banderov@isenselabs.com>
2018-06-14 09:20:50 +0300

    Add Hungarian

---
+++ hungarian/src_1.5/upload/catalog/language/hungarian/module/isenselabs_gdpr.php
@@ -0,0 +1,110 @@
+<?php
+$_['text_account'] = 'Fiók';
+$_['text_gdpr'] = 'GDPR Eszközök';
+
+$_['button_back'] = 'Vissza';
+$_['button_continue'] = 'Folytatás';
+$_['entry_email'] = 'E-mail cím';
+$_['text_your_email'] = 'E-mail címe';
+
+$_['text_data_rectification'] = 'Személyes adatok helyesbítése';
+$_['text_data_rectification_helper'] = 'Az alábbi linkek segítségével módosíthatja a nálunk tárolt adatait:';
+$_['text_edit'] = 'Név, e-mail cím és telefonszám megváltoztatása';
+$_['text_password'] = 'Jelszó megváltoztatása';
+$_['text_address'] = 'Címek szerkesztése';
+$_['text_newsletter'] = 'Hírlevél feliratkozás megváltoztatása';
+
+$_['text_data_portability'] = 'Személyes adatok hordozása';
+$_['text_data_portability_helper'] = 'Az alábbi linkek segítségével letöltheti a személyes adatait:';
+$_['text_personal_information'] = 'Személyes adatok';
+$_['text_personal_addresses'] = 'Címek';
+$_['text_orders'] = 'Megrendelések';
+$_['text_gdpr_requests'] = 'GDPR igénylések';
+
+$_['text_access_to_personal_data'] = 'Személyes adatokhoz való hozzáférés';
+$_['text_access_to_personal_data_helper'] = 'Minden nálunk tárolt személyes adatának kivonatát lekérheti az alábbi linken:';
+$_['text_personal_data_helper'] = 'Az alábbi űrlap kitöltésével kérvényezheti tárolt személyes adatainak kivonatát tőlünk.';
+$_['text_request_a_report'] = 'Jelentés kérése';
+$_['text_enter_your_email'] = 'E-mail címe';
+$_['text_enter_your_email_data_request_helper'] = 'Egy e-mailt küldünk, amiben található linkre kattintás után hozzáférhet a kért adatokhoz. A kiküldött link %s% órán át érvényes.';
+$_['text_how_this_works_data_request'] = 'Hogyan működik?';
+$_['text_successful_request'] = 'Sikeres!';
+$_['text_successful_data_request_helper'] = 'Köszönjük a kérését! Ha az e-mail cím egyezik a nálunk tárolt e-mail címével, akkor egy e-email üzenetben kapni fog tőlünk egy linket. Ezen linkre kattintva hozzá férhet a személyes adataihoz, amit oldalunkon tárolunk. A kiküldött link biztonsági okokból %s% órán át érvényes.';
+
+$_['text_personal_data_subject'] = 'GDPR személyes adat jelentés';
+$_['text_greeting'] = 'Tisztelt látogatónk! Kérvényezte tőlünk a tárolt személyes adataihoz való hozzáférést. Az alábbi linkre kattintva megtekintheti ezeket:';
+$_['text_button_view_data'] = 'Személyes adatok megjelenítése';
+$_['text_actions'] = 'Műveletek';
+$_['text_active_link_helper'] = '* A kiküldött link %s% órán át érvényes.';
+
+$_['text_view_personal_data'] = 'Személyes adatok megtekintése';
+$_['text_firstname'] = 'Keresztnév';
+$_['text_lastname'] = 'Vezetéknév';
+$_['text_telephone'] = 'Telefon';
+$_['text_email'] = 'E-mail';
+$_['text_fax'] = 'Fax';
+$_['text_newsletter_subscription'] = 'Hírlevél feliratkozás';
+$_['text_ip'] = 'IP';
+$_['text_customer_information'] = 'Ügyfél adatok';
+$_['text_saved_addresses'] = 'Címek';
+$_['text_company'] = 'Cég';
+$_['text_address_1'] = 'Címsor 1';
+$_['text_address_2'] = 'Címsor 2';
+$_['text_country'] = 'Ország';
+$_['text_city'] = 'Város';
+$_['text_postcode'] = 'Irányítószám';
+$_['text_zone'] = 'Zóna';
+$_['text_address'] = 'Cím';
+$_['text_data_third_party'] = 'Harmadik fél által nyújtott szolgáltatás felé továbbított adatok';
+$_['text_data_third_party_helper'] = 'Esetenként továbbíthatjuk személyes adatai egy részét az alábbi szolgáltatók felé. Ez annak érdekében történik, hogy a lehető legjobb felhasználói élményt nyújthassuk oldalunkon.';
+$_['text_third_party_services'] = 'Szolgáltatások';
+$_['text_other'] = 'Egyéb';
+
+$_['text_hash_error'] = 'Nincs elérhető adat. A link érvénytelen vagy lejárt. Amennyiben hozzá szeretne férni adataihoz, nyújtson be új kérvényt.';
+$_['text_no_data_third_party'] = 'Nem továbbítjuk adatait harmadik fél felé.';
+$_['text_no_data'] = 'Nincs elérhető adat ehhez az ügyfélhez!';
+
+$_['text_right_to_be_forgotten'] = 'Fiók törlés kezdeményezés';
+$_['text_right_to_be_forgotten_helper'] = 'Ezzel a funkcióval törölheti összes nálunk tárolt személyes adatát. Felhívjuk a figyelmét, hogy <strong>ez a folyamat visszafordíthatatlanul törli a fiókját összes adatával együtt</strong>.';
+$_['text_request_personal_data_deletion'] = 'Személyes adatok törlésének kérése';
+$_['text_enter_your_email_data_deletion_helper'] = 'Egy e-mailt fog kapni tőlünk, amiben található linkre kattintva az összes tárolt adatát töröljük. Amely adatok nem törölhetőek, azokat anonimizáljuk. Az e-mailben található link %s% órán át érvényes.';
+$_['text_successful_data_deletion_helper'] = 'Fiók törlés kezdeményezését rögzítettük! Amennyiben valós e-mail címet adott meg elküldjük rá a törléshez szükséges linket. Erre a linkre kattintva az összes tárolt adatát töröljük. Az e-mailben található link %s% órán át érvényes.';
+
+$_['text_delete_data_subject'] = 'Személyes adatok törlésének kérése';
+$_['text_button_delete_data'] = 'Törlöm a személyes adataimat';
+$_['text_greeting_delete'] = 'Tisztelt látogatónk! Személyes adatainak törlését kérvényezte tőlünk. Felhívjuk figyelmét, hogy ez a folyamat azonnal végrehajtódik és nem visszafordítható, nem fog tudni többet belépni oldalunkra.<br /><br />Amennyiben meggondolta magát tekintse levelünket tárgytalannak, egyéb esetben kattintson az alábbi linkre a folytatáshoz:';
+
+$_['text_personal_data_deletion'] = 'Személyes adatok törlése';
+$_['text_hash_error_delete'] = 'A műveletet nem tudtuk végrehajtani. A link érvénytelen vagy lejárt. Amennyiben szeretné ezt a műveletet végrehajtani kérjük nyújtson be egy új GDPR igénylést.';
+$_['text_confirm_the_action'] = 'Felhasználói megerősítés szükséges';
+$_['text_confirm_the_action_helper'] = '<strong>Ez a művelet nem visszafordítható!</strong> Ha megerősíti a törlési kérelmét nem fog tudni többet belépni oldalunkra és nem férhet hozzá korábbi rendeléseihez, kívánságlistájához sem. Döntését itt véglegesítheti:';
+$_['text_button_confirm_deletion'] = 'Igen, kérem adataim törlését';
+$_['text_button_cancel_deletion'] = 'Nem, adataim ne kerüljenek törlésre';
+
+// GDPR Compliance 1.1/2.1./3.1
+$_['text_order'] = 'Megrendelés';
+$_['text_personal_data'] = 'Vendég ügyfél adatok';
+$_['text_personal_data_helper'] = 'Ha valaki regisztráció nélkül vásárol oldalunkon, néhány adatot eltárolunk róla adatbázisunkban. Alábbiakban láthatja milyen adatokat tároltunk el megrendelésekhez köthetően.';
+$_['text_payment_view_data'] = 'Fizetési adatok';
+$_['text_shipping_view_data'] = 'Szállítási adatok';
+$_['text_order_view_data'] = 'Megrendelés adatok';
+$_['text_user_agent'] = 'User Agent';
+$_['text_accept_language'] = 'Elfogadás nyelve';
+
+// GDPR Compliance 1.2/2.2./3.2
+$_['text_cookie_settings_header'] = 'Válassza ki mely cookie-kat (sütiket) tárolhatja el oldalunk az Ön gépén';
+$_['text_btn_close'] = 'Bezárás';
+$_['text_btn_save'] = 'Változások mentése';
+$_['text_general_cookies'] = 'Alapműködéshez szükséges cookie-k';
+$_['text_general_cookies_helper'] = 'Ezeket a cookie-kat nem lehet letiltani, mert a weboldal működéséhez elengedhetetlenek. Ezek a cookie-k csak olyan adatokat tárolnak, mint nyelv, pénznem, munkamenet azonosító, adatvédelmi beállítások. Böngészőjében letilthatja ezeket a cookie-kat, de ez az oldal alapműködését befolyásolhatja.';
+$_['text_general_analytics'] = 'Statisztikai és elemző cookie-k';
+$_['text_general_analytics_helper'] = 'Ezek a cokkie-k segítenek nekünk abban, hogy mérhessük a látogatók által meglátogatott oldalainkat és az akuális látogatók számát. Abban is segítenek, hogy megtudjuk melyik termék a legnépszerűbb.';
+$_['text_general_marketing'] = 'Marketing és remarketing cookie-k';
+$_['text_general_marketing_helper'] = 'Ezeket a cookie-kat leginkább a marketing vagy hirdetési partnereink helyezik el a gépén. Arra használhatják fel, hogy megismerjék melyik termékeket kedveli és ezáltal személyre szabott hirdetéseket jelenítsen meg a keresőt használva. Amennyiben letiltja ezeket a cookie-kat, nem fog személyre szabott hirdetésekel találkozni.';
+
+// GDPR Compliance 1.2.1/2.2.1/3.2.1
+$_['text_my_gdpr_tools_header'] = 'GDPR eszköztáram';
+
+// GDPR Compliance 1.4/2.4/3.4
+$_['text_optin_checkbox'] = 'Elolvastam és elfogadom a(z) <a href="%s" class="agree"><b>%s</b></a>';
+$_['text_optin_error'] = 'El kell fogadni a(z) %s !';
---
+++ hungarian/src_2.3/upload/admin/language/hu-hu/extension/module/isenselabs_gdpr.php
@@ -0,0 +1,222 @@
+<?php
+// Heading
+$_['heading_title']    			= 'GDPR megfelelés';
+
+// Text
+$_['text_extension']   			= 'Bővítmények';
+$_['text_success']     			= 'Siker: Módosítottad a GDPR modult!';
+$_['text_edit']        			= 'Modul szerkesztése';
+$_['text_module']      			= 'Modulok';
+$_['text_complete']    			= 'Kész';
+$_['text_error']       			= 'Hiba';
+
+// Entry
+$_['entry_status']     			= 'Állapot';
+$_['text_gdpr_status']			= 'Bővítmény állapota';
+$_['text_gdpr_status_helper']   = 'Bővítmény engedélyezése vagy letiltása.';
+
+// Errors
+$_['error_permission'] 			= 'Figyelmeztetés: Nincs jogosultságod módosítani a bővítményt!';
+$_['error_unexpected'] 			= 'Váratlan hiba lépett fel. Próbáld meg ismét vagy vedd fel a kapcsolatot a fejlesztőkkel! Hibakód: ';
+
+// Licensing
+$_['text_your_license']         = 'Licensz';
+$_['text_please_enter_the_code']= 'Adja meg a termék vásárlásakor kapott licensz kódot:';
+$_['text_activate_license']     = 'Licensz aktiválása';
+$_['text_not_having_a_license'] = 'Nincs licenszkódja? Itt beszerezheti.';
+$_['text_license_holder']       = 'Licensz birtokosa';
+$_['text_registered_domains']   = 'Regisztrált domainek';
+$_['text_expires_on']           = 'Licensz lejárati ideje';
+$_['text_valid_license']        = 'ÉRVÉNYES LICENSZ';
+$_['text_manage']               = 'Kezelés';
+$_['text_get_support']          = 'Támogatás kérése';
+$_['text_community']            = 'Közösség';
+$_['text_ask_our_community']    = 'Nagy közösséggel rendelkezünk. Tegye fel kérdését a közösségi fórumon.';
+$_['text_browse_forums']        = 'Fórumok keresése';
+$_['text_tickets']              = 'Hibajegyek';
+$_['text_open_a_ticket']        = 'Fel kívánja velünk venni a kapcsolatot, hogy megoldjuk a felmerült hibáját? Nyisson hibajegyet nálunk.';
+$_['text_open_ticket_for_real'] = 'Hibajegy létrehozása';
+$_['text_pre_sale']             = 'Ajánlatkérés';
+$_['text_pre_sale_text']        = 'Van egy kiváló ötlete OpenCart-hoz? A fejlesztő csapatunk segít, hogy ötlete valósággá válhasson.';
+$_['text_bump_the_sales']       = 'Még több leárazás';
+
+$_['tab_home']       			= 'Ellenőrző lista';
+$_['tab_controlpanel'] 			= 'Beállítások';
+$_['tab_support']       		= 'Támogatás';
+$_['tab_requests']				= 'Igénylések';
+$_['tab_acceptances']			= 'Szabályzat elfogadások';
+$_['tab_personaldata']			= 'Harmadik fél';
+$_['tab_datasecurity']			= 'Adat biztonság';
+
+$_['text_loading_data']		 	= 'Adatok betöltése, kérem várjon...';
+$_['text_no_results']			= 'Nem található adat ebben a nézetben.';
+
+$_['column_request_id']			= 'Kérés ID';
+$_['column_type']				= 'Típus';
+$_['column_email']				= 'E-mail';
+$_['column_accept_language']	= 'Elfogadás nyelve';
+$_['column_user_agent']			= 'User Agent';
+$_['column_server_ip']			= 'Szerver IP';
+$_['column_client_ip']			= 'Kliens IP';
+$_['column_date']				= 'Dátum';
+$_['column_acceptance_id']		= 'Elfogadás ID';
+$_['column_name']				= 'Név';
+$_['column_content']			= 'Szöveges tartalom';
+$_['column_actions']			= 'Műveletek';
+
+$_['text_date_start']			= 'Kezdő dátum';
+$_['text_date_end']				= 'Végzés dátum';
+$_['text_filter']				= 'Szűrő';
+$_['text_download']				= 'Letöltés';
+$_['text_view_current']			= 'Jelenlegi szabályzat megtekintése';
+
+$_['text_life_of_links']		= 'GDPR igénylések érvényessége';
+$_['text_access_to_personal_data_link'] = 'Személyes adatokhoz való hozzáférés joga';
+$_['text_access_to_personal_data_link_helper'] = 'Adja meg az érvényességi idejét (órákban) minden egyes személyes adat kérésnek. Ha a kérvényező nem erősíti meg szándékát ezen idő alatt, újból le kell adnia az igénylést.';
+$_['text_access_default_hours'] = 'Alapértelmezett érték: 5 óra';
+
+$_['text_remove_personal_data_link'] = 'Személyes adatok törléséhez való jog';
+$_['text_remove_personal_data_link_helper'] = 'Adja meg az érvényességi idejét (órákban) a személyes adat törlésének kéréséhez. Ha a kérvényező nem erősíti meg szándékát ezen idő alatt, újból le kell adnia az igénylést.';
+$_['text_delete_default_hours'] = 'Alapértelmezett érték: 2 óra';
+
+$_['text_access_to_personal_data'] = 'Személyes adatokhoz hozzáférők listája';
+$_['text_access_to_personal_data_helper'] = 'Adja meg a szolgáltatások nevét, amelyek hozzáférhetnek az ügyfelek személyes adataihoz (pl.: név, e-mail cím, ügyfél azonosító, IP, stb.).';
+$_['text_third_party_services'] = 'Harmadik fél általi szolgáltatások';
+$_['text_third_party_services_helper'] = 'Adja meg a szolgáltatások nevét, amelyek nyomon követik, elemzik az ügyfelek tevékenységét. (pl.: Google Analytics, Google AdWords, Google Retargeting, Facebook Ads, stb.).';
+$_['text_other_services'] = 'Egyéb szolgáltatások';
+$_['text_other_services_helper'] = 'Adja meg a többi nem nyilvános szolgáltatás nevét, amelyek hozzáférnek az ügyfelek személyes adataihoz. Például: ERP integration, Matomo, stb.';
+
+$_['text_general'] = 'Általános beállítások';
+$_['text_show_gdpr_menu_left'] = 'GDPR megfelelés hozzáadása az Admin főmenühöz';
+$_['text_show_gdpr_menu_left_helper'] = 'Ha engedélyezi, akkor ez a beállítás egy admin panel linket ad hozzá a GDPR megfelelés bővítményhez Admin főmenüben.';
+$_['text_enabled'] = 'Engedélyezve';
+$_['text_disabled'] = 'Letiltva';
+$_['text_show_gdpr_menu_left_default'] = 'Alapértelmezett érték: Letiltva';
+
+$_['tab_cookieconsent'] = 'Cookie (süti) hozzájárulás';
+$_['text_cc_enable'] = 'Cookie (süti) hozzájárulás sáv bekapcsolása';
+$_['text_cc_enable_helper'] = 'Engedélyezi vagy letiltja a Cookie (süti) hozzájárulást.';
+$_['text_cc_positioning'] = 'Elhelyezés';
+$_['text_cc_positioning_helper'] = 'Cookie hozzájárulási sáv helye.';
+$_['text_cc_banner_bottom'] = 'Alsó szalagcím';
+$_['text_cc_banner_top'] = 'Felső szalagcím';
+$_['text_cc_banner_top_pushdown'] = 'Felső szalagcím (pushdown)';
+$_['text_cc_banner_float_left'] = 'Balról beúszó';
+$_['text_cc_banner_float_right'] = 'Jobbról beúszó';
+$_['text_cc_link_to_pp'] = 'Adatkezelési tájékoztató linkjének hozzáadása a cookie sávhoz.';
+$_['text_cc_link_to_pp_helper'] = 'Hozzá adja az üzlet adatkezelési tájékoztatójának linkjét a cookie sávhoz.';
+$_['text_cc_texts'] = 'Egyéni szöveg';
+$_['text_cc_texts_helper'] = 'Adjon hozzá egyéni szöveget a cookie sávhoz.';
+$_['text_cc_main_message'] = 'Fő szöveg';
+$_['text_cc_main_message_helper'] = 'Adja meg a fő szövegét a cookie sávnak. Egy példát itt talál:';
+$_['text_cc_pp_message'] = 'Adatkezelési tájékoztató linkje';
+$_['text_cc_pp_message_helper'] = 'Adja meg az adatkezelési tájékoztató linkjét itt.';
+$_['text_cc_accept_text'] = 'Elfogadás gomb szöveg';
+$_['text_cc_accept_text_helper'] = 'Adja meg az elfogadás gomb szövegét.';
+$_['text_cc_dismiss_text'] = 'Elvetés gomb';
+$_['text_cc_dismiss_text_helper'] = 'Adja meg az elvetés gomb szövegét.';
+$_['text_cc_track_pp_clicks'] = 'Cookie sáv elfogadásának nyomokövetése';
+$_['text_cc_track_pp_clicks_helper'] = 'Nyomon követi a cookie sáv "Elfogadás" gombra kattintását. Ez egyenértékű az adatkezelési tájékoztató elfogadásával.';
+$_['text_cc_bar_bg'] = 'Szalagcím háttere';
+$_['text_cc_bar_text'] = 'Szalagcím szöveg';
+$_['text_cc_button_bg'] = 'Gomb háttere';
+$_['text_cc_button_text'] = 'Gomb szöveg';
+$_['text_cc_palette'] = 'Színpaletta';
+$_['text_cc_palette_styling'] = 'Válassza ki a cookie sáv színpalettáját.';
+
+// Dashboard
+$_['text_steps_gdpr_compliant'] = 'GDPR ellenőrző lista';
+$_['text_install_the_extension'] = 'Telepítse a GDPR megfelelés bővítményt';
+$_['text_install_the_extension_helper'] = 'Ellenőrizze a telepített bővítményt és, hogy minden fájl fel lett-e töltve.';
+$_['text_enable_the_extension'] = 'Engedélyezze a bővítményt';
+$_['text_enable_the_extension_helper'] = 'A vezérlőpulton engedélyezze a modult.';
+$_['text_track_policy_acceptances'] = 'Kezdje el naplózni az elfogadott szabályzatokat';
+$_['text_track_policy_acceptances_helper'] = 'Ez egy GDPR követelmény. Értesítést kap arról, hogy melyik szabályzatot fogadták el az ügyfelek.';
+$_['text_respont_gdpr_requests'] = 'Kezelje a beérkezett GDPR igényléseket';
+$_['text_respont_gdpr_requests_helper'] = 'Ez a tulajdonság automatikusan kezelve van a bővítmény által. GDPR törvény értelmében 72 órán belül reagálni kell a beérkezett igénylésekre. GDPR megfelelés bővítmény ezeket a igényléseket azonnal feldolgozza.';
+$_['text_update_pp'] = 'Frissítse az üzlet adatkezelési tájékoztatóját';
+$_['text_update_pp_helper'] = 'Az adatkezelési tájékoztató frissítése az Ön feladata. Gondoskodjon róla, hogy minden GDPR szabálynak megfelel a tartalma.<br /><br />A tájékoztatónak, amiben a személyes adatok kezelését írja le: <ul><li>tömörnek, átláthatónak és könnyen hozzáférhetőnek kell lennie;</li><li>mindenki által érthető nyelvezettel, kifejezetten akkor, ha gyermekek is érintettek;</li><li>ingyenesen elérhető.</li></ul>';
+$_['text_enable_cookie_consent_bar'] = 'Cookie (süti) hozzájárulás sáv bekapcsolása (opcionális)';
+$_['text_enable_cookie_consent_bar_helper'] = 'Cookie (süti) hozzájárulás sáv a GDPR megfelelés bővítményben széles körűen testreszabható és naplóz minden cookie valamint adatkezelési tájékoztató elfogadást az adatbázisba.<br /><br />Cookie (süti) hozzájárulás sávot engedélyezheti a GDPR megfelelés beállításainak "Cookie hozzájárulás" fülén.';
+
+$_['text_consent_bar_copyright'] = '* Cookie (süti) hozzájárulás sáv cookieconsent.insites.com forráskódja alapján készült, amit SilkTide Ltd. fejlesztett.';
+$_['text_show_gdpr_footer'] = 'GDPR eszközök linkjének elhelyezése az oldal láblécében';
+$_['text_show_gdpr_footer_helper'] = 'GDPR eszközök linkjének engedélyezése, hogy az ügyfelek könnyen hozzáférhessenek. Egyedi témák esetén a link nem minden esetben jelenik meg, hanem kézzel kell hozzáadni a megfelelő helyen.';
+
+$_['text_data_security_breach'] = 'Adatvédelmi incidens';
+$_['text_notify_customers'] = 'Adatvédelmi incidens értesítés';
+$_['text_data_security_breach_helper'] = 'Ügyfelek értesítése egy adatvédelmi incidens esetén.';
+$_['text_data_security_breach_helper_additional'] = 'Az ügyfeleket értesíteni kell, ha illetéktelenek hozzáférhettek az adataikról. Tájékoztatni kell őket mikor történt pontosan az incidens, hány fiók volt érintett és, összefoglalni hogy milyen lépéseket tett meg az incidens óta.';
+
+$_['text_gdpr_checklist_helper'] = 'Jelölje meg az elvégzett feladatokat a következő GDPR ellenőrző listán.';
+$_['text_gdpr_check_1'] = 'Engedélyezze a GDPR megfelelés bővítményt';
+$_['text_gdpr_check_1_helper'] = 'Ez az első könnyű lépés, hogy megfeleljen GDPR-nek.';
+$_['text_gdpr_check_2'] = 'GDPR eszközök linkjének elhelyezése az oldal láblécében';
+$_['text_gdpr_check_2_helper'] = 'GDPR eszközök lábléc linkjét <strong>Beállítások</strong> fülön engedélyezheti.';
+$_['text_gdpr_check_3'] = 'Ellenőrizze ügyfél-oldalról a GDPR eszközöket';
+$_['text_gdpr_check_3_helper'] = 'GDPR megfelelés modul egy új oldalt hoz létre az áruházban, ahol az ügyfelek és a látogatók új GDPR-hez kapcsolódó funkciót érhetnek el. Ellenőrizze az oldal megjelenését - [link]. A felhasználói funkciókat is kipróbálhatja.';
+$_['text_gdpr_check_4'] = 'Frissítse az üzlet adatkezelési tájékoztatóját GDPR-nek megfelelő adatokkal';
+$_['text_gdpr_check_4_helper'] = 'Naplózhatja az elfogadott szabályzatokat. Csak annyi a dolga, hogy az adatkezelési tájékoztató tartalmazzon GDPR-hez kapcsolódó fejezeteket. Az <strong>adatkezelési tájékoztató</strong> itt található - [link]. Ha nem elérhető az oldal, akkor hozzon létre egyet - [link2]. Példákat találhat [linkstart]ebben a blogposztban[linkend] angol nyelven.';
+$_['text_gdpr_check_5'] = 'Adja hozzá az általános szerződés feltételekhez GPDR szabályzatot';
+$_['text_gdpr_check_5_helper'] = 'Naplózhatja az elfogadott szabályzatokat. Csak annyi a dolga, hogy az általános szerződési feltételek tartalmazzon GDPR-hez kapcsolódó fejezeteket. Az <strong>ÁSZF</strong> oldalát itt találja - [link]. Ha nem elérhető az oldal, akkor hozzon létre egyet - [link2]. Példákat találhat [linkstart]ebben a blogposztban[linkend] angol nyelven.';
+$_['text_gdpr_check_6'] = 'Engedélyezze az ügyfeleknek, hogy hozzáférjenek tárolt személyes adataikhoz';
+$_['text_gdpr_check_6_helper'] = 'Ügyfelei kérhetik vagy letölthetik személyes adataikat. Ezen az oldalon ellenőrizheti a működését - [link]';
+$_['text_gdpr_check_7'] = 'Elfeledtetéshez való jog engedélyezése';
+$_['text_gdpr_check_7_helper'] = 'Ezen az oldalon ellenőrizheti a működését - [link]';
+$_['text_gdpr_check_8'] = 'Személyes adatok helyesbítéshez való jog engedélyezése';
+$_['text_gdpr_check_8_helper'] = 'Ezen az oldalon ellenőrizheti a működését - [link]';
+$_['text_gdpr_check_9'] = 'Cookie (süti) hozzájárulás sáv bekapcsolása (opcionális)';
+$_['text_gdpr_check_9_helper'] = 'Cookie (süti) hozzájárulás sávot bekapcsolhatja <strong>Cookie hozzájárulás</strong> fülön és tetszés szerinti stílust állíthat be neki.';
+$_['text_gdpr_check_10'] = 'Kövesse nyomon a beérkezett és feldolgozott GPDR igényléseket és szabályzat elfogadásokat';
+$_['text_gdpr_check_10_helper'] = 'Nyomon követheti a GDPR <strong>igénylések</strong> és <strong>szabályzat elfogadások</strong>-at.';
+$_['text_link'] = '[LINK]';
+$_['text_link_test'] = 'KATTINTSON A TESZTELÉSHEZ';
+$_['text_link_view'] = 'KATTINTSON A MEGJELENÍTÉSHEZ';
+$_['text_link_add'] = 'KATTINTSON A HOZZÁADÁSHOZ';
+
+// GDPR 1.2/2.2/3.2
+$_['text_cc_cookies_change'] = 'Cookie gombok feliratának módosítása';
+$_['text_cc_cookies_change_helper'] = 'Adja meg a cookie-k engedélyezés/letiltás gomb feliratát.';
+$_['text_cookie_management'] = 'Cookie kezelés';
+$_['text_cc_customize'] = 'Testreszabás';
+$_['text_ccc_functional'] = 'Alapműködéshez szükséges cookie-k';
+$_['text_ccc_functional_helper'] = 'Ezeket a cookie-kat nem lehet letiltani, mert a weboldal működéséhez elengedhetetlenek. Ezek a cookie-k csak olyan adatokat tárolnak, mint nyelv, pénznem, munkamenet azonosító, adatvédelmi beállítások. Böngészőjében letilthatja ezeket a cookie-kat, de ez az oldal alapműködését befolyásolhatja.';
+$_['text_ccc_analytics'] = 'Statisztikai és elemző cookie-k';
+$_['text_ccc_analytics_helper'] = 'Ezek a cokkie-k segítenek nekünk abban, hogy mérhessük a látogatók által meglátogatott oldalainkat és az akuális látogatók számát. Abban is segítenek, hogy megtudjuk melyik termék a legnépszerűbb.';
+$_['text_ccc_marketing'] = 'Marketing és remarketing cookie-k';
+$_['text_ccc_marketing_helper'] = 'Ezeket a cookie-kat leginkább a marketing vagy hirdetési partnereink helyezik el a gépén. Arra használhatják fel, hogy megismerjék melyik termékeket kedveli és ezáltal személyre szabott hirdetéseket jelenítsen meg a keresőt használva. Amennyiben letiltja ezeket a cookie-kat, nem fog személyre szabott hirdetésekel találkozni.';
+$_['text_cc_close_action'] = '"Bezárás" gomb megnyomásakor ez történjen';
+$_['text_cc_close_action_helper'] = 'Mit hajtson végre az oldal, amikor a "Bezárás" gombot megnyomják.';
+$_['text_cc_stop_nothing'] = 'Tartsa meg az összes cookie-t, amíg ezt kifejezetten nem tiltja le a látogató';
+$_['text_cc_stop_marketing'] = 'Tiltsa le a marketing cookie-kat, ameddig a látogató nem fogadja el azokat';
+$_['text_cc_stop_analytics'] = 'Tiltsa le a statisztikai és elemző cookie-kat, ameddig a látogató nem fogadja el azokat';
+$_['text_cc_stop_both'] = 'Tiltsa le mind a marketing, mind a statisztikai és elemző cookie-kat, ameddig a látogató nem fogadja el azokat';
+
+// GDPR 1.3/2.3/3.3
+$_['text_gdpr_seo_link'] = 'SEO URL';
+$_['text_gdpr_seo_link_helper'] = 'Ezzel engedélyezheti a SEO-barát URL-t az oldal GDPR eszközök oldalához. Megjegyzés: Ehhez engedélyezni kell "<em>SEO URL-eket</em>" az áruház beállításainál.<br /><br />Adja meg a szöveget, amit az URL-ben legyen.';
+$_['text_cc_cookie_button'] = 'Cookie (süti) hozzájárulás sáv előhívásának felirata';
+$_['text_cc_cookie_button_helper'] = 'Adja meg a szöveget a cookie hozzájárulás sáv előhívásához.';
+$_['text_cc_always_show'] = 'Cookie (süti) hozzájárulás sáv előhívó gomb megjelenítése';
+$_['text_cc_always_show_helper'] = 'Egy kis gombot jelenít meg az oldal tetején vagy alján (attól függően, hogy a cookie sáv hol található) ami előhívja ismét a cookie sávot, hogy a látogatók módosítani szeretnék a korábbi beállításukat.';
+
+// GDPR 1.4/2.4/3.4
+$_['text_gdpr_cc_styles'] = 'Egyedi CSS stílus';
+$_['text_gdpr_cc_styles_helper'] = 'Egyedi stílus beállítása cookie hozzájárulási sávhoz.';
+$_['text_cc_default_action'] = 'Alapállapota a cookie hozzájárulási sávnak';
+$_['text_cc_default_action_helper'] = 'Adja meg az alapbeállításait a cookie hozzájárulási sávnak, ami új látogatók esetén beállításra kerül.';
+$_['text_optins'] = 'Elfogadás beállítások';
+$_['text_contact_form_optin_link'] = 'Kapcsolat oldali szabályzat elfogadás bekapcsolása';
+$_['text_contact_form_optin_link_helper'] = 'Ezzel bekapcsolhatja, hogy a látogatóknak el kelljen fogadnia az adatkezelési tájékoztatót a kapcsolatfelvétel előtt.';
+$_['tab_optins'] = 'Elfogadva/nem elfogadva';
+$_['column_optin_id'] = 'Elfogadás ID';
+$_['column_action'] = 'Művelet';
+
+// GDPR 1.4.1/2.4.1/3.4.1
+$_['text_contact_form_newsletter_link'] = 'Hírlevél elfogadás beállítása';
+$_['text_contact_form_newsletter_link_helper'] = 'Ezzel a funkcióval naplózhatja a hírlevél feliratkozásokat és leiratkozásokat.';
+$_['text_filter_optin'] = 'Feliratkozás';
+$_['text_filter_optout'] = 'Leiratkozás';
+$_['text_filter_acceptance'] = 'Elfogadás';
+$_['text_filter_custom'] = 'Egyedi';
+$_['text_filter_all'] = 'Mind';
