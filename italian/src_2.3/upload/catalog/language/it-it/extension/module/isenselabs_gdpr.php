7cfef63c1c8f7358e79dd1bab1845fd26e7f551d
Aleksandar Banderov <banderov@isenselabs.com>
2018-06-18 09:30:46 +0300

    Update Italian

--- italian/src_2.3/upload/catalog/language/it-it/extension/module/isenselabs_gdpr.php
+++ italian/src_2.3/upload/catalog/language/it-it/extension/module/isenselabs_gdpr.php
@@ -1,49 +1,43 @@
 <?php
 $_['text_account'] = 'Account';
-$_['text_gdpr'] = 'GDPR Strumenti';
-
+$_['text_gdpr'] = 'Strumenti GDPR';
 $_['button_back'] = 'Indietro';
 $_['button_continue'] = 'Avanti';
-$_['entry_email'] = 'Indirizzo Email';
-$_['text_your_email'] = 'Il tuo indirizzo Email';
-
+$_['entry_email'] = 'Indirizzo email';
+$_['text_your_email'] = 'Il tuo indirizzo email';
 $_['text_data_rectification'] = 'Rettifica dati';
-$_['text_data_rectification_helper'] = 'Puoi utilizzare i link sottostanti per aggiornare i dati dell\'account se non è accurato.';
+$_['text_data_rectification_helper'] = 'Puoi utilizzare i link sottostanti per aggiornare o modificare i dati dell&rsquo;account.';
 $_['text_edit'] = 'Modifica le informazioni del tuo account';
 $_['text_password'] = 'Cambia la tua password';
 $_['text_address'] = 'Modifica le voci della tua rubrica';
-$_['text_newsletter'] = 'Iscriviti / disiscriviti dalla newsletter';
-
-$_['text_data_portability'] = 'Portabilità dei dati';
+$_['text_newsletter'] = 'Iscriviti/disiscriviti dalla newsletter';
+$_['text_data_portability'] = 'Portabilit&agrave; dei dati';
 $_['text_data_portability_helper'] = 'Puoi utilizzare i link sottostanti per scaricare tutti i dati che conserviamo e utilizziamo per una migliore esperienza nel nostro negozio.';
-$_['text_personal_information'] = 'Personal information';
+$_['text_personal_information'] = 'Informazioni personali';
 $_['text_personal_addresses'] = 'Indirizzi salvati';
 $_['text_orders'] = 'Ordini';
-$_['text_gdpr_requests'] = 'GDPR richieste';
-
+$_['text_gdpr_requests'] = 'Richieste GDPR';
 $_['text_access_to_personal_data'] = 'Accesso ai dati personali';
-$_['text_access_to_personal_data_helper'] = 'Puoi utilizzare il link sottostante per richiedere un rapporto che conterrà tutte le informazioni personali che conserviamo per te.';
-$_['text_personal_data_helper'] = 'Utilizza la forma sottostante per richiedere un rapporto che conterrà tutte le informazioni personali che conserviamo per te.';
-$_['text_request_a_report'] = 'Richiedi un rapporto';
-$_['text_enter_your_email'] = 'Entra il tuo indirizzo email';
-$_['text_enter_your_email_data_request_helper'] = 'Riceverai un\'email con un link che, una volta cliccato, ti mostrerà tutte le informazioni che memorizziamo per te. Il link sarà attivo per% s% ore.';
+$_['text_access_to_personal_data_helper'] = 'Puoi utilizzare il link sottostante per richiedere un rapporto che conterr&agrave; tutte le informazioni personali che conserviamo per te.';
+$_['text_personal_data_helper'] = 'Utilizza il form sottostante per richiedere un rapporto che conterr&agrave; tutte le informazioni personali che conserviamo per te.';
+$_['text_request_a_report'] = 'Richiedi un rapporto dei tuoi dati personali';
+$_['text_enter_your_email'] = 'Inserisci il tuo indirizzo email';
+$_['text_enter_your_email_data_request_helper'] = 'Riceverai un&rsquo;email con un link che, una volta cliccato, ti mostrer&agrave; tutte le informazioni che memorizziamo per te. Il link sar&agrave; attivo per %s% ore.';
 $_['text_how_this_works_data_request'] = 'Come funziona?';
-$_['text_successful_request'] = 'Successo!';
-$_['text_successful_data_request_helper'] = 'Grazie per la vostra sottoscrizione! Se l\'indirizzo e-mail è corretto ed è presente nel database, riceverai un\'email con un link. L\'uso del link ti aiuterà a vedere tutti i dati personali che abbiamo per te. Tieni presente che il link sarà attivo per% s% ore.';
-
+$_['text_successful_request'] = 'Richiesta inoltrata con successo!';
+$_['text_successful_data_request_helper'] = 'Grazie per la tua sottoscrizione! Se l&rsquo;indirizzo e-mail &egrave; corretto ed &egrave; presente nel nostro database, riceverai un&rsquo;email con un link. L&rsquo;uso del link ti aiuter&agrave; a vedere tutti i tioi dati personali che possediamo. Tieni presente che il link sar&agrave; attivo per %s% ore.';
 $_['text_personal_data_subject'] = 'Clicca qua per vedere i tuoi dati personali';
-$_['text_greeting'] = 'Ciao! Hai richiesto di vedere i tuoi dati personali nel nostro negozio. Clicca sul link qui sotto per verificare:';
-$_['text_button_view_data'] = 'Guarda i tuoi dati personali';
+$_['text_greeting'] = 'Salve! Hai richiesto di visualizzare i tuoi dati personali. Clicca sul link qui sotto per verificare:';
+$_['text_button_view_data'] = 'Visualizza i tuoi dati personali';
 $_['text_actions'] = 'Azioni';
-$_['text_active_link_helper'] = '* Tieni presente che il link sarà attivo per% s% ore.';
-
-$_['text_view_personal_data'] = 'Visualizza dati personali';
+$_['text_active_link_helper'] = 'Il link sar&agrave; attivo per %s% ore.';
+$_['text_view_personal_data'] = 'Visualizza i dati personali';
 $_['text_firstname'] = 'Nome';
-$_['text_lastname'] = 'Cogniome';
+$_['text_lastname'] = 'Cognome';
 $_['text_telephone'] = 'Telefono';
 $_['text_email'] = 'Email';
 $_['text_fax'] = 'Fax';
-$_['text_newsletter_subscription'] = 'Sottoscrizione alla Newsletter';
+$_['text_newsletter_subscription'] = 'Sottoscrizione alla newsletter';
 $_['text_ip'] = 'IP';
 $_['text_customer_information'] = 'Informazioni per il cliente';
 $_['text_saved_addresses'] = 'Indirizzi salvati';
@@ -51,55 +45,66 @@ $_['text_company'] = 'Ditta';
 $_['text_address_1'] = 'Indirizzo 1';
 $_['text_address_2'] = 'Indirizzo 2';
 $_['text_country'] = 'Nazione';
-$_['text_city'] = 'Citta';
+$_['text_city'] = 'Citt&agrave;';
 $_['text_postcode'] = 'Codice postale';
 $_['text_zone'] = 'Zona';
 $_['text_address'] = 'Indirizzo';
 $_['text_data_third_party'] = 'Dati inviati a servizi di terze parti';
-$_['text_data_third_party_helper'] = 'A volte possiamo inviare una parte dalle tue informazioni personali ai servizi di seguito. Questo è per garantire che tu abbia la migliore esperienza possibile sul nostro negozio.';
+$_['text_data_third_party_helper'] = 'A volte possiamo inviare una parte dalle tue informazioni personali ai servizi di seguito. Questo &egrave; per garantire che tu abbia la migliore esperienza possibile sul nostro sito.';
 $_['text_third_party_services'] = 'Servizi';
 $_['text_other'] = 'Altro';
-$_['text_hash_error'] = 'Non ci sono dati da mostrare. Il link non è valido o è scaduto. Se vuoi vedere i dati richiesti, devi fare un\'altra richiesta GDPR.';
+$_['text_hash_error'] = 'Non ci sono dati da mostrare. Il link non &egrave; valido o &egrave; scaduto. Se vuoi vedere i dati richiesti, devi fare un&rsquo;altra richiesta GDPR.';
 $_['text_no_data_third_party'] = 'Noi non inviamo i tuoi dati personali a servizi di terze parti.';
 $_['text_no_data'] = 'Non ci sono dati per questo cliente!';
-
 $_['text_right_to_be_forgotten'] = 'Diritto di essere dimenticato';
-$_['text_right_to_be_forgotten_helper'] = 'Usa questa opzione se desideri rimuovere i tuoi dati personali e di altro tipo dal nostro negozio. Tieni presente che <strong> questo processo eliminerà il tuo account, quindi non potrai più accedervi o utilizzarlo</strong>.';
-$_['text_request_personal_data_deletion'] = 'Richiedi la cancellazione dei dati personali';
-$_['text_enter_your_email_data_deletion_helper'] = 'Riceverai un\'email con un link, che dopo aver cliccato saranno cancellati tutti i dati che memorizziamo per te. Se qualcosa non può essere cancellato, sarà reso anonimo. Il link sarà attivo per %s% ore.';
-$_['text_successful_data_deletion_helper'] = 'Grazie per la vostra sottoscrizione! Se l\'indirizzo e-mail è corretto ed è presente nel database, riceverai un\'email con un link. L\'uso del link cancellerà tutti i dati personali che abbiamo per te. Tieni presente che il link sarà attivo per %s% ore.';
-
+$_['text_right_to_be_forgotten_helper'] = 'Usa questa opzione se desideri rimuovere i tuoi dati personali e di altro tipo dal nostro negozio. Tieni presente che <strong> questo processo eliminer&agrave; il tuo account, quindi non potrai pi&ugrave; accedervi o utilizzarlo</ strong>.';
+$_['text_request_personal_data_deletion'] = 'Richiedi la cancellazione dei tuoi dati personali (diritto all&rsquo;oblio)';
+$_['text_enter_your_email_data_deletion_helper'] = 'Riceverai un&rsquo;email con un link che, dopo aver cliccato, provveder&agrave; a cancellare tutti i dati che memorizziamo per te. Se qualcosa non pu&ograve; essere cancellato, sar&agrave; reso anonimo. Il link sar&agrave; attivo per %s% ore.';
+$_['text_successful_data_deletion_helper'] = 'Grazie per la tua sottoscrizione! Se l&rsquo;indirizzo e-mail &egrave; corretto ed &egrave; presente nel nostro database, riceverai un&rsquo;email con un link. L&rsquo;uso del link canceller&agrave; tutti i dati personali che abbiamo per te. Tieni presente che il link sar&agrave; attivo per %s% ore.';
 $_['text_delete_data_subject'] = 'Richiesta di cancellazione dei dati personali';
 $_['text_button_delete_data'] = 'Elimina i miei dati personali';
-$_['text_greeting_delete'] = 'Ciao! Hai richiesto di cancellare i tuoi dati personali dal nostro negozio. Tieni presente che questo processo eliminerà il tuo account, quindi non potrai più accedervi o utilizzarlo più. <br /> <br /> Se non sei stato tu, ti preghiamo di ignorare questo messaggio. Altrimenti, clicca sul link sottostante per procedere:';
-
+$_['text_greeting_delete'] = 'Salve! Hai richiesto di cancellare i tuoi dati personali dal nostro negozio. Tieni presente che questo processo eliminer&agrave; il tuo account, quindi non potrai pi&ugrave; accedervi o utilizzarlo pi&ugrave;. <br /> <br /> Se non sei stato tu, ti preghiamo di ignorare questo messaggio. Altrimenti clicca sul link sottostante per procedere:';
 $_['text_personal_data_deletion'] = 'Cancellazione dei dati personali';
-$_['text_hash_error_delete'] = 'L\'azione non può essere completata. Il link non è valido o è scaduto. Se si desidera eseguire questa azione, è necessario effettuare un\'altra richiesta GDPR.';
-$_['text_confirm_the_action'] = 'Conferma la richiesta dal cliente';
-$_['text_confirm_the_action_helper'] = '<strong> Questa azione è irreversibile! </strong> Se confermi l\'azione, il tuo account verrà eliminato nel nostro sito web e non potrai più accedere con esso. Non avrai inoltre accesso ai tuoi ordini, indirizzi, ecc. Per favore, prendi la tua decisione qui sotto:';
-$_['text_button_confirm_deletion'] = 'Sì, elimina tutti i miei dati';
+$_['text_hash_error_delete'] = 'L&rsquo;azione non pu&ograve; essere completata. Il link non &egrave; valido o &egrave; scaduto. Se si desidera eseguire questa azione, &egrave; necessario effettuare un&rsquo;altra richiesta GDPR.';
+$_['text_confirm_the_action'] = 'Si prega di confermare la richiesta';
+$_['text_confirm_the_action_helper'] = '<strong>Attenzione, questa azione &egrave; irreversibile! </ strong> Se confermi, il tuo account verr&agrave; eliminato definitivamente e non potrai pi&ugrave; accedere con esso. Inoltre non potrai pi&ugrave; avere accesso ai tuoi ordini, indirizzi, ecc. Confermi?';
+$_['text_button_confirm_deletion'] = 'S&igrave;, elimina tutti i miei dati';
 $_['text_button_cancel_deletion'] = 'No, lascia intatti i miei dati';
-
 // GDPR Compliance 1.1/2.1./3.1
 $_['text_order'] = 'Ordine';
 $_['text_personal_data'] = 'Dati personali come ospiti';
-$_['text_personal_data_helper'] = 'Se il cliente non è registrato, potrebbero ancora esserci alcuni dati per questa particolare e-mail nel nostro database. Vedrai qui le informazioni che siamo stati in grado di estrarre dalla tabella degli ordini.';
-$_['text_payment_view_data'] = 'Informazione di pagamento';
+$_['text_personal_data_helper'] = 'Se il cliente non &egrave; registrato, potrebbero ancora esserci alcuni dati per questa particolare e-mail nel nostro database. Vedrai qui le informazioni che siamo stati in grado di estrarre dalla tabella degli ordini.';
+$_['text_payment_view_data'] = 'Informazioni di pagamento';
 $_['text_shipping_view_data'] = 'Informazioni sulla spedizione';
-$_['text_order_view_data'] = 'Informazioni per l\'ordine';
-$_['text_user_agent'] = 'Utenza Agente';
-$_['text_accept_language'] = 'Accetta la lingua';
-
+$_['text_order_view_data'] = 'Informazioni per l&rsquo;ordine';
+$_['text_user_agent'] = 'Agente utente';
+$_['text_accept_language'] = 'Lingua';
 // GDPR Compliance 1.2/2.2./3.2
-$_['text_cookie_settings_header'] = 'Scegli il tipo di Cookie che accetti di usare';
-$_['text_btn_close'] = 'Chiudi';
+$_['text_cookie_settings_header'] = 'Seleziona il tipo di cookies che accetti';
+$_['text_btn_close'] = 'X chiudi';
 $_['text_btn_save'] = 'Salva';
-$_['text_general_cookies'] = 'Cookies richiesti obbligatoriamente';
-$_['text_general_cookies_helper'] = 'Questi cookie sono obbligatori affinchè il sito funzioni e non possono essere disattivati. Tali cookie sono configurati a valle di azioni da te intraprese come lingua, valuta, sessioni di login, preferenze di privacy. Puoi configurare il tuo browser per bloccare questi cookie ma il sito potrebbe non funzionare.';
-$_['text_general_analytics'] = 'Statistiche';
-$_['text_general_analytics_helper'] = 'Questi cookie ci permettono di monitorare il traffico dei visitatori e visualizzare le sorgenti di traffico. Servono anche a capire quali prodotti e azioni sono maggiormente referenziati.';
-$_['text_general_marketing'] = 'Marketing e Retargeting';
-$_['text_general_marketing_helper'] = 'Questi cookie sono normalmente configurati dai nostri partner di marketing e pubblicità. Possono essere usati per profilare i tuoi interessi e mostrare pubblicità rilevanti. Se bloccherai questi cookie non vedrai pubblicità rilevanti per i tuoi interessi.';
+$_['text_general_cookies'] = 'Cookies strettamente necessari';
+$_['text_general_cookies_helper'] = 'Cookies indispensabili al funzionamento del sito. Tali cookies vengono impostati solo in risposta alle azioni eseguite come lingua, valuta, sessione di accesso, preferenze. Puoi impostare il tuo browser per bloccare questi cookies ma il nostro sito potrebbe non funzionare.';
+$_['text_general_analytics'] = 'Analitici e statistici';
+$_['text_general_analytics_helper'] = 'Questi cookies ci permettono di misurare il traffico dei visitatori e vedere le sorgenti di traffico raccogliendo informazioni in insiemi di dati. Inoltre ci aiutano a capire quali prodotti e azioni sono pi&ugrave; popolari di altri.';
+//$_['text_general_analytics_helper'] = 'Questi cookies ci permettono di misurare il traffico dei visitatori raccogliendo informazioni in insiemi di dati. Gli indirizzi IP forniti a Google Analytics sono stati da noi resi anonimi, pertanto sono da considerare alla stregua dei cookies tecnici.';
+$_['text_general_marketing'] = 'Marketing e retargeting (profilazione)';
+$_['text_general_marketing_helper'] = 'Questi cookies sono solitamente impostati da partner di marketing e pubblicitari per creare un profilo di tuo interesse per mostrare annunci pertinenti. Disabilitando questi cookies non si verificheranno annunci mirati per i propri interessi.';
 
 // GDPR Compliance 1.2.1/2.2.1/3.2.1
-$_['text_my_gdpr_tools_header'] = 'I Miei Strumenti GDPR';
+$_['text_my_gdpr_tools_header'] = 'Il mio GDPR Tools';
+
+// GDPR Compliance 1.4/2.4/3.4
+$_['text_optin_checkbox'] = 'Ho letto e accetto <a href="%s" target="_blank" class="agree"><b>%s</b></a>';
+$_['text_optin_error'] = 'È necessario accettare% s per continuare.';
+
+// GDPR Compliance 1.5/2.5/3.5
+$_['successful_deletion_request'] = 'Richiesta inoltrata con successo!';
+$_['successful_deletion_request_helper'] = 'La tua richiesta stata inviata e, dopo essere stata esaminata, sar&agrave; evasa o respinta.<br />Se verr&agrave; evasa ti saranno notificate le azioni intraprese per rimuovere i tuoi dati.<br />Se sar&agrave; respinta riceverai un&rsquo;email che spiegher&agrave; quali sono state le motivazioni.';
+$_['text_right_to_be_forgotten_request'] = 'Richiesta di "diritto d&rsquo;oblio"';
+$_['text_greeting_rtb'] = 'Il cliente %s% ha richiesto la cancellazione dei suoi dati. Apri il pannello di amministrazione per eseguire o respingere la richiesta.';
+$_['successful_deletion_request_helper_automatically'] = 'La tua richiesta di cancellazione &egrave; stata inoltrata ed &egrave; stata approvata automaticamente. Tutti i tuoi dati sono stati cancellati.<br /> <br />Ci dispiace che tu sia andato via :(';
+
+// GDPR Compliance 1.5.1/2.5.1/3.5.1
+$_['text_subscribed'] = 'Iscriviti';
+$_['text_unsubscribed'] = 'Disiscriviti';
