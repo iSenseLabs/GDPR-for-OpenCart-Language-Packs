81ff66c3051db342e65c72a90d58d32cebea3838
Aleksandar Banderov <banderov@isenselabs.com>
2018-09-25 11:50:17 +0300

    Add Catalan language

---
+++ catalan/src_1.5/upload/catalog/language/catalan/module/isenselabs_gdpr.php
@@ -0,0 +1,130 @@
+<?php
+$_['text_account'] = 'Compte';
+$_['text_gdpr'] = 'Eines GDPR';
+
+$_['button_back'] = 'Tornar';
+$_['button_continue'] = 'Continuar';
+$_['entry_email'] = 'Correu Electrònic';
+$_['text_your_email'] = 'El Seu Correu Electrònic';
+
+$_['text_data_rectification'] = 'Rectificació de Dades';
+$_['text_data_rectification_helper'] = 'Podeu utilitzar els enllaços següents per actualitzar les dades del vostre compte si no són precisos.';
+$_['text_edit'] = 'Editeu la informació del vostre compte';
+$_['text_password'] = 'Canvieu la vostra contrasenya';
+$_['text_address'] = 'Modifiqueu les entrades de la llibreta d&#39;adreces';
+$_['text_newsletter'] = 'Subscrigui/Cancel·li la subscripció al butlletí';
+
+$_['text_data_portability'] = 'Portabilitat de Dades';
+$_['text_data_portability_helper'] = 'Podeu utilitzar els següents enllaços per descarregar totes les dades que emmagatzemem i que fem servir per a que obtingui una millor experiència a la nostra botiga.';
+$_['text_personal_information'] = 'Informació Personal';
+$_['text_personal_addresses'] = 'Adreces guardades';
+$_['text_orders'] = 'Comandes';
+$_['text_gdpr_requests'] = 'Sol·licituds GDPR';
+
+$_['text_access_to_personal_data'] = 'Accés a Dades Personals';
+$_['text_access_to_personal_data_helper'] = 'Podeu utilitzar l&#39;enllaç següent per sol·licitar un informe amb tota la informació personal que tenim per a vostè.';
+$_['text_personal_data_helper'] = 'Utilitzeu el següent formulari per sol·licitar un informe amb tota la informació personal que guardem per a vostè.';
+$_['text_request_a_report'] = 'Sol·liciteu un informe';
+$_['text_enter_your_email'] = 'Introdueixi el seu correu electrònic';
+$_['text_enter_your_email_data_request_helper'] = ' Rebrà un correu electrònic amb un enllaç que us portarà a tota la informació que guardem per a vostè. L&#39;enllaç estarà actiu durant %s% hores.';
+$_['text_how_this_works_data_request'] = 'Com funciona?';
+$_['text_successful_request'] = 'Èxit!';
+$_['text_successful_data_request_helper'] = 'Gràcies pel vostre enviament. Si l&#39;adreça de correu electrònic és correcta i està present a la base de dades, rebrà un correu electrònic amb un enllaç. Feu clic a l&#39;enllaç per veure totes les dades personals que emmagatzemem per a vostè. Tingueu en compte que l&#39;enllaç estarà actiu durant %s% hores.';
+
+$_['text_personal_data_subject'] = 'Marqueu això per veure les vostres dades personals';
+$_['text_greeting'] = 'Hola! Heu sol·licitat veure les vostres dades personals a la nostra botiga. Feu clic a l&#39;enllaç següent:';
+$_['text_button_view_data'] = 'Consulteu les Vostres Dades Personals';
+$_['text_actions'] = 'Accions';
+$_['text_active_link_helper'] = '* Tingueu en compte que l&#39;enllaç estarà actiu durant %s% hores.';
+
+$_['text_view_personal_data'] = 'Veure Dades Personals';
+$_['text_firstname'] = 'Nom';
+$_['text_lastname'] = 'Cognom';
+$_['text_telephone'] = 'Telèfon';
+$_['text_email'] = 'Email';
+$_['text_fax'] = 'Fax';
+$_['text_newsletter_subscription'] = 'Subscripció Newsletter';
+$_['text_ip'] = 'IP';
+$_['text_customer_information'] = 'Informació Client';
+$_['text_saved_addresses'] = 'Adreces Guardades';
+$_['text_company'] = 'Empresa';
+$_['text_address_1'] = 'Adreça Línia 1';
+$_['text_address_2'] = 'Adreça Línia 2';
+$_['text_country'] = 'País';
+$_['text_city'] = 'Ciutat';
+$_['text_postcode'] = 'Codi Postal';
+$_['text_zone'] = 'Província';
+$_['text_address'] = 'Adreça';
+$_['text_data_third_party'] = 'Dades Enviades a Serveis de Tercers';
+$_['text_data_third_party_helper'] = 'De vegades, podem enviar una part de la vostra informació personal als serveis següents. Això és per garantir que tingueu la millor experiència possible al nostre lloc web.';
+$_['text_third_party_services'] = 'Serveis';
+$_['text_other'] = 'Altres';
+
+$_['text_hash_error'] = 'No hi ha cap dada que es mostri. L&#39;enllaç no és vàlid o caducat. Si voleu veure les dades sol·licitades, heu de fer una sol·licitud nova.';
+$_['text_no_data_third_party'] = 'No enviem les seves dades personals a serveis de tercers.';
+$_['text_no_data'] = 'No hi ha dades per a aquest client!';
+
+$_['text_right_to_be_forgotten'] = 'Dret a Ser Oblidat';
+$_['text_right_to_be_forgotten_helper'] = 'Utilitzeu aquesta opció si voleu eliminar les dades personals i d&#39;altres de la nostra botiga. Tingueu en compte que <strong>aquest procés suprimirà el vostre compte, de manera que ja no podreu accedir-hi ni utilitzar-lo</strong>.';
+$_['text_request_personal_data_deletion'] = 'Sol·liciteu la supressió de dades personals';
+$_['text_enter_your_email_data_deletion_helper'] = 'Rebrà un correu electrònic amb un enllaç que, al fer clic, eliminarà totes les dades que guardem per a vostè. Si alguna cosa no es pot eliminar, serà anonimitzada. L&#39;enllaç estarà actiu durant %s% hores.';
+$_['text_successful_data_deletion_helper'] = 'Gràcies pel vostre enviament. Si l&#39;adreça de correu electrònic és correcta i està present a la base de dades, rebrà un correu electrònic amb un enllaç. L&#39;ús de l&#39;enllaç crearà una sol·licitud de supressió de totes les dades personals que emmagatzemem per a vostè. Tingueu en compte que l&#39;enllaç estarà actiu durant %s% hores.';
+
+$_['text_delete_data_subject'] = 'Sol·licitud de supressió de dades personals';
+$_['text_button_delete_data'] = 'Suprimir les meves dades personals';
+$_['text_greeting_delete'] = 'Hola! Heu sol·licitat suprimir les dades personals de la nostra botiga. Tingueu en compte que aquest procés suprimirà el vostre compte, de manera que ja no podreu accedir-hi ni utilitzar-lo.<br /><br />Si no era així, ignoreu aquest missatge. En cas contrari, feu clic a l&#39;enllaç següent per a continuar:';
+
+$_['text_personal_data_deletion'] = 'Esborrament de Dades Personals';
+$_['text_hash_error_delete'] = 'L&#39;acció no es pot completar. L&#39;enllaç no és vàlid o ha caducat. Si voleu fer aquesta acció, haureu de fer una altra sol·licitud GDPR.';
+$_['text_confirm_the_action'] = 'Cal Confirmació del Client';
+$_['text_confirm_the_action_helper'] = '<strong>Aquesta acció és irreversible!</strong> Si confirma l&#39;acció, el vostre compte al nostre lloc web se suprimirà i ja no podreu iniciar-hi la sessió. Tampoc tindreu accés a les vostres comandes, adreces, etc. Realitzeu la vostra decisió a continuació:';
+$_['text_button_confirm_deletion'] = 'Sí, elimineu totes les meves dades';
+$_['text_button_cancel_deletion'] = 'No, deixeu les meves dades intactes';
+
+// GDPR Compliance 1.1/2.1./3.1
+$_['text_order'] = 'Comanda';
+$_['text_personal_data'] = 'Dades Personals de Convidats';
+$_['text_personal_data_helper'] = 'Si el client no està registrat, pot ser que hi hagi algunes dades d&#39;aquest correu electrònic en particular a la nostra base de dades. Veureu aquí la informació que vam poder treure de la taula de comandes.';
+$_['text_payment_view_data'] = 'Informació Pagament';
+$_['text_shipping_view_data'] = 'Informació Enviament';
+$_['text_order_view_data'] = 'Informació Comanda';
+$_['text_user_agent'] = 'User Agent';
+$_['text_accept_language'] = 'Idioma Acceptació';
+
+// GDPR Compliance 1.2/2.2./3.2
+$_['text_cookie_settings_header'] = 'Trieu el tipus de cookies que accepteu utilitzar';
+$_['text_btn_close'] = 'Tancar';
+$_['text_btn_save'] = 'Guardar Canvis';
+$_['text_general_cookies'] = 'Galetes Estrictament Necessàries';
+$_['text_general_cookies_helper'] = 'Aquestes cookies són necessàries per executar el lloc web i no es pòden desactivar. Aquestes cookies només s&#39;estableixen en resposta a les accions que heu realitzat, com ara idioma, moneda, inici de sessió, preferències de privadesa. Podeu configurar el vostre navegador per bloquejar aquestes cookies però és possible que el nostre lloc no funcioni.';
+$_['text_general_analytics'] = 'Analytics i Estadístiques';
+$_['text_general_analytics_helper'] = 'Aquestes cookies ens permeten mesurar el trànsit de visitants i veure les fonts de trànsit recopilant informació en conjunts de dades. També ens ajuden a comprendre quins productes i accions són més populars que els altres.';
+$_['text_general_marketing'] = 'Màrqueting i Retargeting';
+$_['text_general_marketing_helper'] = 'Normalment, aquestes cookies són establertes pels nostres socis de màrqueting i publicitat. Poden utilitzar-los per crear un perfil del vostre interès i, posteriorment, mostrar-vos anuncis rellevants. Si no permeteu aquestes galetes, no experimentareu anuncis orientats per als vostres interessos.';
+
+// GDPR Compliance 1.2.1/2.2.1/3.2.1
+$_['text_my_gdpr_tools_header'] = 'Les Meves Eines GDPR';
+
+// GDPR Compliance 1.4/2.4/3.4
+$_['text_optin_checkbox'] = 'He llegit i accepto el <a href="%s" class="agree"><b>%s</b></a>';
+$_['text_optin_error'] = 'Heu d&#39;acceptar el %s per continuar';
+
+// GDPR Compliance 1.5/2.5/3.5
+$_['successful_deletion_request'] = 'Sol·licitud Enviada!';
+$_['successful_deletion_request_helper'] = 'La vostra sol·licitud s&#39;ha enviat als propietaris de la botiga. Ho revisaran i decidiran si volen complir-la o denegar-la. Aquests són els possibles resultats:<br /><br />- Si decideixen complir la vostra sol·licitud, se us notificarà quines accions es prendran per eliminar les vostres dades de la botiga.<br />- Si la vostra sol·licitud se us denega, rebrà un correu electrònic explicant quins són els motius d&#39;aquesta decisió.';
+$_['text_right_to_be_forgotten_request'] = 'Sol·licitud Dret a Ser Oblidat';
+$_['text_greeting_rtb'] = 'El client %s% vol que les seves dades siguin eliminades de la vostra botiga. Obriu el tauler d&#39;administració de la vostra botiga per complir o denegar la sol·licitud.';
+$_['successful_deletion_request_helper_automatically'] = 'La vostra sol·licitud s&#39;ha enviat als propietaris de la botiga i s&#39;ha aprovat automàticament. Ara, totes les vostres dades s&#39;eliminen de la botiga.<br /><br />Lamentem veure-ho :(';
+
+// GDPR Compliance 1.5.1/2.5.1/3.5.1
+$_['text_subscribed'] = 'Subscrit';
+$_['text_unsubscribed'] = 'No Subscrit';
+
+// GDPR Compliance 1.8/2.8/3.8
+$_['text_button_confirm_subscription'] = 'Confirmar Subscripció';
+$_['text_confirm_subscription_subject'] = 'Confirmar Subscripció Newsletter';
+$_['text_confirm_subscription'] = 'Hola! Confirmeu la subscripció a la newsletter, de manera que puguem començar a enviar-vos correus electrònics de butlletins, on podreu trobar moltes coses interessants:';
+$_['text_newsletter_subscription_success'] = 'La vostra subscripció a la newsletter s&#39;ha actualitzat correctament!';
+$_['text_newsletter_subscription_success_message'] = 'La vostra subscripció s&#39;ha actualitzat correctament. Ara està subscrit a la nostra botiga.';
+$_['text_thank_you'] = 'Gràcies!';
+$_['text_succes_message_after_edit_newsletter'] = 'Gràcies per la vostra selecció. Aviat rebrà un missatge a la safata d&#39;entrada del seu correu electrònic, que ha d&#39;obrir per confirmar la seva selecció.';
